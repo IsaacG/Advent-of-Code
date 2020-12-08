df0f0c239623c72ca04f330a891a0832731c2b4a
Isaac Good <nuc-git@isaacgood.com>
2020-12-07 21:57:52 -0800

    Improve loader util

--- py/util.py
+++ py/util.py
@@ -2,13 +2,12 @@
 
 import pathlib
 import sys
-from typing import List
+from typing import List, Optional
 
 
-def load_data(sep='\n') -> List[str]:
-  datafile = pathlib.Path(sys.argv[1])
-  data = datafile.read_text().split(sep)
-  if data[-1] == "":
-    del data[-1]
-  return data
+def load_data(sep: str = '\n', text: Optional[str] = None) -> List[str]:
+  if text is None:
+    datafile = pathlib.Path(sys.argv[1])
+    text = datafile.read_text()
+  return text.strip().split(sep)
 
