43ed3db578cc77d7fa65670973de29893dbec370
Isaac Good <nuc-git@isaacgood.com>
2020-12-08 22:19:58 -0800

    util: add a mapping function

--- py/util.py
+++ py/util.py
@@ -5,9 +5,10 @@ import sys
 from typing import List, Optional
 
 
-def load_data(sep: str = '\n', text: Optional[str] = None) -> List[str]:
+def load_data(sep: str = '\n', text: Optional[str] = None, func=lambda x: x) -> List[str]:
   if text is None:
     datafile = pathlib.Path(sys.argv[1])
     text = datafile.read_text()
-  return text.strip().split(sep)
+  data = text.strip().split(sep)
+  return [func(i) for i in data]
 
