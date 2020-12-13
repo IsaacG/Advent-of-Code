09ae8fad2538b2b35ca7573712a402d1f028b3ae
Isaac Good <github@isaacgood.com>
2020-12-12 23:23:05 -0800

    Day 13

---
+++ py/13.py
@@ -0,0 +1,72 @@
+#!/bin/python
+"""Day 13. Bus terminal departure schedules."""
+
+import aoc
+from typing import List
+
+
+SAMPLE = ["""\
+939
+7,13,x,x,59,x,31,19
+"""]
+
+
+class Day13(aoc.Challenge):
+  """Figure out when busses depart."""
+
+  TRANSFORM = str
+
+  TESTS = (
+    aoc.TestCase(inputs=SAMPLE[0], part=1, want=295),
+    aoc.TestCase(inputs=SAMPLE[0], part=2, want=1068781),
+  )
+
+  def part1(self, lines: List[str]) -> int:
+    """Find which bus will first arrive once we are at the bus terminal."""
+    earliest = int(lines[0])
+    busses = [int(i) for i in lines[1].split(',') if i != 'x']
+    departs_at = {}
+    for bus in busses:
+      depart_time = (int(earliest / bus) + 1) * bus
+      departs_at[depart_time] = bus
+    pick = min(departs_at)
+    return (pick - earliest) * departs_at[pick]
+
+  def part2(self, lines: List[str]) -> int:
+    """Find out a time at which the busses will all arrive one minute apart.
+
+    The bus numbers are all prime which is handy.
+    Start with start_time=1 and step=1.
+    Loop with start_time+=step until we find a time as which a bus will depart
+    at that time (plus bus offset). Call that bus number, N.
+    Now we want times that are start_time + multiples of N so update step *= N.
+    The step accumulates all the busses we "found" so we can drop the bus from
+    the pending list once it is found.
+    Continue stepping through time, updating step *= N with each bus we find,
+    until we found all busses.
+    """
+    # Build a list of offset, bus for all busses we care about.
+    busses = [int(i) if i != 'x' else 0 for i in lines[1].split(',')]
+    busses = [(a, b) for a, b in enumerate(busses) if b]
+
+    start_time = 0
+    step = 1
+    while busses:
+      start_time += step
+
+      # See if any new busses align with this timestamp.
+      newfound = [b for (k, b) in busses if not (start_time + k) % b]
+      if newfound:
+        newfound = newfound[0]
+        # Remove the bus from the unfound bus list and update the step
+        # so future iterations would include this bus, too.
+        busses = [(a, b) for a, b in busses if b != newfound]
+        step *= newfound
+    else:
+      return start_time
+
+
+if __name__ == '__main__':
+  Day13().run()
+
+# vim:ts=2:sw=2:expandtab
