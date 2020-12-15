316a8ef3ba705dfe0bdc8eb19995b2237b9754d8
Isaac Good <github@isaacgood.com>
2020-12-14 23:29:41 -0800

    Clean up Day 15

--- py/15.py
+++ py/15.py
@@ -1,4 +1,5 @@
 #!/bin/python
+"""Day 15. Van Eck sequence. https://www.youtube.com/watch?v=etMJxB-igrc - Numberphile."""
 
 import aoc
 import collections
@@ -7,79 +8,40 @@ import math
 import re
 from typing import Any, Callable, Dict, List
 
-SAMPLE = ["""\
-0,3,6
-""", """\
-"""]
-
 
 class Day15(aoc.Challenge):
+  """Solve for the n'th value in a Van Eck sequence."""
 
   TRANSFORM = lambda _, s: [int(i) for i in s.split(',')]
-  DEBUG = True
+  SLOW = True, True
 
   TESTS = (
-    aoc.TestCase(inputs=SAMPLE[0], part=1, want=436),
-    aoc.TestCase(inputs=SAMPLE[0], part=2, want=175594),
+    aoc.TestCase(inputs='0,3,6', part=1, want=436),
+    aoc.TestCase(inputs='1,3,2', part=1, want=1),
+    aoc.TestCase(inputs='2,1,3', part=1, want=10),
   )
 
-  def part1(self, lines: List[str]) -> int:
-    starting = lines[0]
-    ages = collections.defaultdict(list)
-    counter = 1
-    last = None
-    is_new = False
-    for s in starting:
-      last = s
-      is_new = last not in ages
-      ages[s].insert(0, counter)
-      counter += 1
-
-    while True:
-      if is_new:
-        s = 0
-      else:
-        s = ages[last][0] - ages[last][1]
-      last = s
-      is_new = last not in ages
-      ages[s].insert(0, counter)
-      ages[s] = ages[s][0:2]
-      if counter == 2020: return last
-      counter += 1
+  def part1(self, lines: List[List[int]]) -> int:
+    return self.solve(lines[0], 2020)
         
-  def part2(self, lines: List[str]) -> int:
-    starting = lines[0]
-    ages = collections.defaultdict(list)
-    counter = 1
-    last = None
-    is_new = False
-    for s in starting:
-      last = s
-      is_new = last not in ages
-      ages[s].insert(0, counter)
-      counter += 1
+  def part2(self, lines: List[List[int]]) -> int:
+    return self.solve(lines[0], 30000000)
 
-    while True:
-      if is_new:
-        s = 0
+  def solve(self, starting: List[int], end: int) -> int:
+    time_said = {j: i for i, j in enumerate(starting[:-1])}
+    last_spoken = starting[-1]
+
+    for counter in range(len(starting), end):
+      if last_spoken not in time_said:
+        say = 0
       else:
-        s = ages[last][0] - ages[last][1]
-      last = s
-      is_new = last not in ages
-      ages[s].insert(0, counter)
-      ages[s] = ages[s][0:2]
-      if counter == 30000000: return last
-      counter += 1
-        
-
-
+        say = counter - 1 - time_said[last_spoken]
+      time_said[last_spoken] = counter - 1
+      last_spoken = say
+    return last_spoken
 
 
 if __name__ == '__main__':
   Day15().run()
 
-# vim:ts=2:sw=2:expandtab
-SAMPLE = ["""\
-
-"""]
 # vim:ts=2:sw=2:expandtab
