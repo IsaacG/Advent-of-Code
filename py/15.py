92b1293dacd449832cd492e0bd1b8bc70edc2f18
Isaac Good <github@isaacgood.com>
2020-12-14 23:58:31 -0800

    Day 15: switch dict to list, runtime 15->8.4s

--- py/15.py
+++ py/15.py
@@ -28,15 +28,23 @@ class Day15(aoc.Challenge):
     return self.solve(lines[0], 30000000)
 
   def solve(self, starting: List[int], end: int) -> int:
-    time_said = {j: i for i, j in enumerate(starting[:-1])}
+    """Solve Van Eck's n'th digit.
+
+    1st pass: store the last two timestamps. 30s.
+    2nd pass: store the last timestamp in a map. 15s.
+    3rd pass: store the last timestamp in a list. 8.5s.
+    """
+    time_said = [0] * end
+    for i, j in enumerate(starting[:-1]):
+      time_said[j] = i + 1
     last_spoken = starting[-1]
 
     for counter in range(len(starting), end):
-      if last_spoken not in time_said:
+      if not time_said[last_spoken]:
         say = 0
       else:
-        say = counter - 1 - time_said[last_spoken]
-      time_said[last_spoken] = counter - 1
+        say = counter - time_said[last_spoken]
+      time_said[last_spoken] = counter
       last_spoken = say
     return last_spoken
 
