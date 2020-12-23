ea7b8c973b34813410b1a06e5dd3c837834d6d8d
Isaac Good <github@isaacgood.com>
2020-12-22 22:58:56 -0800

    Day 27, half cleaned

--- py/23.py
+++ py/23.py
@@ -8,7 +8,7 @@ import re
 from typing import Any, Callable, Dict, List
 
 
-class Node:
+class Cup:
 
   def __init__(self, val, prev):
     self.prev = prev
@@ -53,76 +53,63 @@ class Node:
 
 class Day23(aoc.Challenge):
 
-  TRANSFORM = str
-  DEBUG = True
-  SEP = '\n'
-
   TESTS = (
-    aoc.TestCase(inputs='389125467', part=1, want='67384529'),
+    aoc.TestCase(inputs='389125467', part=1, want=67384529),
     aoc.TestCase(inputs='389125467', part=2, want=149245887792),
   )
 
-  def part2(self, lines: List[str]) -> int:
-    cups = [int(i) for i in lines[0]]
-    cups.extend(range(10,1000001))
-    first = Node(cups.pop(0), None)
-    cur = first
+  def solve(self, cups: List[int], rounds: int) -> Cup:
+    """Solve the cups game.
+
+    Use a list of labels as a starting point.
+    Run for `rounds` iteration.
+    Return the cup labeled "1".
+    """
+    # Build a linked list of Cups with a map from label to Cup.
+    mapping = {}
+    cur = None
     for i in cups:
-      cur = Node(i, cur)
-    cur.next = first
-    first.prev = cur
-    mapping = {n.val: n for n in first.all()}
+      cur = Cup(i, cur)
+      mapping[i] = cur
 
-    cur = first
-    for rounds in range(10000000):
-      take = cur.next3()
-      dest = (cur.val - 1) or 1000000
-      while dest in take:
-        dest = (dest - 1) or 1000000
+    # Link the last and first cups, making the list circular.
+    cur.next = mapping[cups[0]]
+    mapping[cups[0]].prev = cur
 
-      dest = mapping[dest]
-      cur.move_next_3(dest)
+    
+    # Start the game with the "first" Cup.
+    cur = mapping[cups[0]]
+    highest = max(cups)
+    for rounds in range(rounds):
+      # Destination label cannot be current or next three labels.
+      not_valid = cur.next3() + [cur.val]
+      dest = cur.val
+      while dest in not_valid:
+        # Try one lower value - with circular counting.
+        dest = (dest - 1) or highest
+
+      # Move 3 Cups from cur to dest.
+      cur.move_next_3(mapping[dest])
+      # Move to the next Cup.
       cur = cur.next
-    one = mapping[1]
+    return mapping[1]
+
+  def part2(self, cups: List[int]) -> int:
+    """Extend the cups through 1M. Play for 10M rounds."""
+    cups = list(cups)
+    cups.extend(range(10, int(1e6)+1))
+    one = self.solve(cups, int(1e7))
     return one.next.val * one.next.next.val
 
+  def part1(self, cups: List[int]) -> int:
+    """Play for 100 rounds, return the list of Cups."""
+    cups = list(cups)
+    one = self.solve(cups, 100)
+    vals = [str(i.val) for i in one.all()][1:]
+    return int("".join(vals))
 
-  def part1(self, lines: List[str]) -> int:
-    cups = [int(i) for i in lines[0]]
-    l = len(cups)
-    cur = 0
-    for rounds in range(100):
-      print(f'-- move {rounds+1} --')
-      print(f'cups: {cups}')
-      current = cups[cur % l]
-      print('current:', current)
-      take = [cups[(cur + j) % l] for j in (1,2,3)]
-      print(f'pick up: {take}')
-      for i in range(3):
-        try:
-          del cups[(cur+1)]
-        except:
-          del cups[0]
-      print(cups)
-      smaller = [i for i in cups if i < current]
-      if rounds == 8:
-        print('smaller', smaller)
-      if smaller:
-        dest_v = max(smaller)
-      else:
-        dest_v = max(cups)
-      print(f'destination: {dest_v}')
-      dest = (cups.index(dest_v) + 1) % l
-      cups.insert(dest, take[2])
-      cups.insert(dest, take[1])
-      cups.insert(dest, take[0])
-      cur = (cups.index(current) + 1) % l
-      print('1', current, cur, cups[cur])
-    print(cups)
-    return "".join(str(i) for i in (cups[1+cups.index(1):] + cups[:cups.index(1)]))
-
-  def preparse_input(self, x):
-    return x
+  def preparse_input(self, lines):
+    return [int(i) for i in lines[0]]
 
 
 if __name__ == '__main__':
