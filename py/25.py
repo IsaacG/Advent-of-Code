fa7b30d50830497c073c9bc33958406e125048e9
Isaac Good <github@isaacgood.com>
2020-12-24 22:13:16 -0800

    Day 25: wrap up

--- py/25.py
+++ py/25.py
@@ -8,6 +8,8 @@ import math
 import re
 from typing import Any, Callable, Dict, List
 
+MODULO = 20201227
+
 
 class Day25(aoc.Challenge):
   """Day 25. Brute force solve Diffie Hellman key exchange."""
@@ -22,18 +24,17 @@ class Day25(aoc.Challenge):
     """Apply the transform function."""
     num = 1
     for i in range(loop_counter):
-      num = (num * subject) % 20201227
+      num = (num * subject) % MODULO
     return num
     
-  def force(self, want):
+  def force(self, w0, w1):
     """Brute force the loop_counter to "solve" Diffie Hellman."""
     attempt = 0
     num = 1
-    w0, w1 = want
     while num != w0 and num != w1:
       attempt += 1
-      num = (num * 7) % 20201227
-    return attempt, want.index(num)
+      num = (num * 7) % MODULO
+    return attempt, 0 if num == w0 else 1
 
   def part1(self, pubkeys: List[str]) -> int:
     """Brute force the key.
@@ -41,10 +42,11 @@ class Day25(aoc.Challenge):
     Brute force both public keys to find the smaller loop counter.
     Use the loop counter to transform the *other* public key.
     """
-    loop_counter, key_index = self.force(pubkeys)
+    loop_counter, key_index = self.force(*pubkeys)
     return self.transform(pubkeys[1 - key_index], loop_counter)
   
   def part2(self, pubkeys: List[str]) -> int:
+    """No part 2."""
     return 0
 
 
