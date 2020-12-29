2bce29f1cb394c4b6372db949e13f0abfb06cee7
Isaac Good <nuc-git@isaacgood.com>
2020-12-28 23:34:20 -0800

    2019 Day 5 part 1

---
+++ 2019/05.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env pypy
+
+import collections
+import functools
+import math
+import re
+import typer
+from typing import Any, Callable, Dict, List
+
+import intcode
+from lib import aoc
+
+SAMPLE = [""]
+
+class Day05(aoc.Challenge):
+
+  TESTS = (
+    aoc.TestCase(inputs='4,0,99', part=1, want=4),
+    aoc.TestCase(inputs='04,2,99', part=1, want=99),
+    aoc.TestCase(inputs='104,6,99', part=1, want=6),
+    aoc.TestCase(inputs='1002,7,3,5,104,5,99,33', part=1, want=99),
+  )
+
+  def run_computer(self, memory: List[int]) -> int:
+    """Run the computer with a given noun, verb."""
+    computer = intcode.Intcode(memory, debug=False)
+    # First input: 1
+    computer.inputs.append(1)
+    computer.run()
+    # Read the last output.
+    return computer.outputs.pop()
+
+  def part1(self, memory: List[int]) -> int:
+    """Run the computer with fixed noun, verb."""
+    return self.run_computer(memory)
+
+  def preparse_input(self, x) -> List[int]:
+    """Return the first row as a list of ints."""
+    return [int(num) for num in x[0].split(',')]
+
+
+if __name__ == '__main__':
+  typer.run(Day05().run)
+
+# vim:ts=2:sw=2:expandtab
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,5 +1,6 @@
 """Intcode computer."""
 
+import collections
 from typing import List
 
 
@@ -22,21 +23,48 @@ class Operation:
     self.comp = comp
     self.ptr = comp.ptr
 
+    param_count = self.len - 1
+    instruction = self.comp.read_n(comp.ptr, param_count + 1)
+    self.op = instruction[0]
+    params = instruction[1:]
+    self._params = params
+
+  def read_from(self, param: int) -> int:
+    mask = 10 ** (param + 1)
+    param_mode = (self.op // mask) % 10
+    if param_mode == 0:  # position mode
+      return self.comp.read(self._params[param - 1])
+    else:  # 1 == immediate mode
+      return self._params[param - 1]
+
+  def write_to(self, param: int, val: int):
+    self.comp.write(self._params[param - 1], val)
+
   @property
   def len(self):
     return self._LEN
 
-  def params(self):
-    return self.readN(self.ptr, self.len)[1:]
-
   def write(self, *args, **kwargs):
     return self.comp.write(*args, **kwargs)
 
-  def read1(self, *args, **kwargs):
-    return self.comp.read1(*args, **kwargs)
+  def read(self, *args, **kwargs):
+    return self.comp.read(*args, **kwargs)
 
-  def readN(self, *args, **kwargs):
-    return self.comp.readN(*args, **kwargs)
+  def read_n(self, *args, **kwargs):
+    return self.comp.read_n(*args, **kwargs)
+
+  def pop_input(self) -> int:
+    return self.comp.inputs.popleft()
+
+  def push_output(self, val: int):
+    return self.comp.outputs.append(val)
+
+  def __str__(self):
+    return '<OP[%s] %s: %s>' % (
+      self.ptr,
+      type(self).__name__,
+      ', '.join(str(i) for i in self._params)
+    )
 
 
 @register_op(1, 4)
@@ -44,8 +72,8 @@ class Add(Operation):
   """Add operation."""
 
   def run(self):
-    addrs = self.params()
-    self.write(addrs[2], self.read1(addrs[0]) + self.read1(addrs[1]))
+    val = self.read_from(1) + self.read_from(2)
+    self.write_to(3, val)
 
 
 @register_op(2, 4)
@@ -53,8 +81,24 @@ class Mult(Operation):
   """Mult operation."""
 
   def run(self):
-    addrs = self.params()
-    self.write(addrs[2], self.read1(addrs[0]) * self.read1(addrs[1]))
+    val = self.read_from(1) * self.read_from(2)
+    self.write_to(3, val)
+
+
+@register_op(3, 2)
+class Input(Operation):
+  """Input operation."""
+
+  def run(self):
+    self.write_to(1, self.pop_input())
+
+
+@register_op(4, 2)
+class Output(Operation):
+  """Input operation."""
+
+  def run(self):
+    self.push_output(self.read_from(1))
 
 
 @register_op(99, 1)
@@ -65,21 +109,27 @@ class Halt(Operation):
 class Intcode:
   """Intcode computer."""
 
-  def __init__(self, memory: List[int]):
+  def __init__(self, memory: List[int], debug: bool = False):
     self.memory = list(memory)
     self.ptr = 0
+    self.inputs = collections.deque()
+    self.outputs = collections.deque()
+    self.debug = debug
 
   def run(self):
     """Run the program until Half and return mem[0]."""
     op = self.next_op()
     while not isinstance(op, Halt):
+      if self.debug:
+        print(op)
+        print(self.memory)
       op.run()
       op = self.next_op()
-    return self.read1(0)
+    return self.read(0)
 
   def next_op(self) -> Operation:
     """Return the next Operation."""
-    instruction = self.memory[self.ptr]
+    instruction = self.memory[self.ptr] % 100
     op = _OPS[instruction](self)
     self.ptr += op.len
     return op
@@ -88,10 +138,10 @@ class Intcode:
     """Write one value to mem[addr]."""
     self.memory[addr] = val
 
-  def read1(self, addr: int) -> int:
+  def read(self, addr: int) -> int:
     """Read one value from mem[addr]."""
-    return self.readN(addr, 1)[0]
+    return self.read_n(addr, 1)[0]
 
-  def readN(self, addr: int, count: int) -> List[int]:
+  def read_n(self, addr: int, count: int) -> List[int]:
     """Read N values from mem[addr]."""
     return self.memory[addr:addr + count]
