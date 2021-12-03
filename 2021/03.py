41bcc76c523504967cad87ab3d3694132b865772
Isaac Good <nuc-git@isaacgood.com>
2021-12-02 21:50:32 -0800

    Tidy up 2021-03 more

--- 2021/03.py
+++ 2021/03.py
@@ -28,49 +28,40 @@ SAMPLE = ["""\
 
 
 class Day03(aoc.Challenge):
-
     """Submarine Diagnostics Analysis."""
 
     TESTS = (
         aoc.TestCase(inputs=SAMPLE[0], part=1, want=198),
         aoc.TestCase(inputs=SAMPLE[0], part=2, want=230),
     )
-
     INPUT_TYPES = str
 
     def common(self, lines: list[str], position: int) -> tuple[str, str]:
         """Return the most and least common values found at a given position."""
         count = collections.Counter(l[position] for l in lines)
-        if count["1"] >= count["0"]:
-            return ("1", "0")
-        else:
-            return ("0", "1")
+        return ("1", "0") if count["1"] >= count["0"] else ("0", "1")
 
     def part1(self, lines: list[str]) -> int:
         """Find the most and least common value across all numbers."""
-        most = "".join(self.common(lines, i)[0] for i in range(len(lines[0])))
-        least = "".join(self.common(lines, i)[1] for i in range(len(lines[0])))
-        return int(most, base=2) * int(least, base=2)
+        result = 1
+        for most_least in range(2):
+            num = "".join(self.common(lines, i)[most_least] for i in range(len(lines[0])))
+            result *= int(num, base=2)
+        return result
 
     def part2(self, lines: list[str]) -> int:
         """Use the most/least common values to filter the numbers there is just one."""
-        nums = lines.copy()
-        for i in range(len(lines[0])):
-            want = self.common(nums, i)[0]
-            nums = [l for l in nums if l[i] == want]
-            if len(nums) == 1:
-                num_one = nums[0]
-                break
+        result = 1
+        for most_least in range(2):
+            nums = lines.copy()
+            for i in range(len(lines[0])):
+                want = self.common(nums, i)[most_least]
+                nums = [l for l in nums if l[i] == want]
+                if len(nums) == 1:
+                    result *= int(nums[0], base=2)
+                    break
 
-        nums = lines.copy()
-        for i in range(len(lines[0])):
-            want = self.common(nums, i)[1]
-            nums = [l for l in nums if l[i] == want]
-            if len(nums) == 1:
-                num_two = nums[0]
-                break
-
-        return int(num_one, base=2) * int(num_two, base=2)
+        return result
 
 
 if __name__ == '__main__':
