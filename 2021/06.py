da4828e9463f8b88a06065ed1e420efdd4f92f66
Isaac Good <nuc-git@isaacgood.com>
2021-12-05 21:45:36 -0800

    Cleanup 2021-06

--- 2021/06.py
+++ 2021/06.py
@@ -11,47 +11,44 @@ import typer
 
 from lib import aoc
 
-SAMPLE = ["""\
-3,4,3,1,2
-"""]
+SAMPLE = "3,4,3,1,2"
 
 InputType = list[int]
 
 
 class Day06(aoc.Challenge):
+    """Compute how many lantern fish there are, after they breed."""
 
     DEBUG = True
 
     TESTS = (
-        aoc.TestCase(inputs=SAMPLE[0], part=1, want=5934),
-        aoc.TestCase(inputs=SAMPLE[0], part=2, want=26984457539),
+        aoc.TestCase(inputs=SAMPLE, part=1, want=5934),
+        aoc.TestCase(inputs=SAMPLE, part=2, want=26984457539),
     )
 
-    # Convert lines to type:
-    INPUT_TYPES = int
-    # Split on whitespace and coerce types:
-    # INPUT_TYPES = [str, int]
-    # Apply a transform function
-    # TRANSFORM = lambda _, l: (l[0], int(l[1:]))
-
     def part1(self, lines: InputType) -> int:
-        fish = lines
-        for i in range(80):
-            new = sum(1 for i in fish if i == 0)
-            fish = [i - 1 for i in fish if i] + [8] * new + [6] * new
-        return len(fish)
+        """Compute fish population after 80 days."""
+        return self.solve(lines, 80)
 
     def part2(self, lines: InputType) -> int:
+        """Compute fish population after 256 days."""
+        return self.solve(lines, 256)
+
+    def solve(self, lines: InputType, days: int) -> int:
+        """Compute fish population after N days."""
+        # Construct the initial population map. Age -> count.
         counter = dict(collections.Counter(lines))
-        for i in range(256):
+        for i in range(days):
+            # Reduce day counter for all fish.
             new = {k - 1: v for k, v in counter.items() if k}
+            # Add new baby fish and reset the counter on fish that spawned.
             new[6] = counter.get(0, 0) + counter.get(7, 0)
             new[8] = counter.get(0, 0)
             counter = new
         return sum(v for v in counter.values())
 
     def parse_input(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
+        """Parse the input data. Comma separated ints."""
         return [int(i) for i in puzzle_input.split(",")]
 
 
