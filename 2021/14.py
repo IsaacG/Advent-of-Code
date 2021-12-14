b515f758c9d3449708b53a62247a86ffa76fcdb1
Isaac Good <nuc-git@isaacgood.com>
2021-12-13 22:40:12 -0800

    Tidy up 2021/14

--- 2021/14.py
+++ 2021/14.py
@@ -2,15 +2,10 @@
 """Advent of Code: Day 14."""
 
 import collections
-import functools
-import math
-import re
-
 import typer
-
 from lib import aoc
 
-InputType = list[int]
+InputType = tuple[str, dict[str, str]]
 SAMPLE = ["""\
 NNCB
 
@@ -34,8 +29,7 @@ CN -> C
 
 
 class Day14(aoc.Challenge):
-
-    DEBUG = True
+    """Compute a polymer after it transforms a bunch of times."""
 
     TESTS = (
         aoc.TestCase(inputs=SAMPLE[0], part=1, want=1588),
@@ -43,47 +37,66 @@ class Day14(aoc.Challenge):
     )
 
     def part1(self, lines: InputType) -> int:
-        start, formulas = lines
-        for _ in range(10):
-            out = []
-            for a, b in zip(start, start[1:]):
-                pair = a + b
-                out.append(a)
-                out.append(formulas[pair])
-            out.append(start[-1])
-            start = out
-        count = collections.Counter(start)
-        return count.most_common()[0][1] - count.most_common()[-1][1]
+        """Transform the polymer 10 times."""
+        return self.solve(lines, 10)
 
     def part2(self, lines: InputType) -> int:
+        """Transform the polymer 10 times."""
+        return self.solve(lines, 40)
+
+    @staticmethod
+    def solve(lines, count) -> int:
+        """Transform the polymer a bunch of times and count.
+
+        To avoid requiring more RAM than I can fit in a computer, do not actually expand
+        the string for each step. Instead, store the number of times each pair occurs.
+
+        ABCBCD => AB, BC, CB, BC, CD => AB: 1, BC: 2, CB: 1, CD: 1
+
+        Then the transforms can be applied pair-wise.
+        Assume formulas AB -> X, BC -> Y
+        AB: 1 becomes AX: 1, XB: 1
+        BC: 2 becomes BY: 2, YC: 2
+        etc
+
+        At the end we can simply count up the letters from each pair-count.
+        AX: 1, XB: 1, BY: 2, YC: 2 => 1*(A, X, X, B), 2*(B, Y, Y, C)
+
+        Note, however, that ABCD turns into AB BC CD. The middle letters are all doubled.
+        The first and last are not doubled. They remain the same throughout all steps.
+        To balance that out, add one for the first and last then divide it all by two.
+        """
         start, formulas = lines
-        original = start[0] + start[-1]
-        pairs = collections.Counter([a + b for a, b in zip(start, start[1:])])
-        for _ in range(40):
-            out = collections.defaultdict(int)
-            for pair, count in pairs.items():
+        # The first and last char need an extra count at the end as they are not duplicated.
+        first_last = start[0], start[-1]
+        # Build a counter of how many times each pair shows up.
+        pairs = dict(collections.Counter([a + b for a, b in zip(start, start[1:])]))
+        for _ in range(count):
+            out: dict[str, int] = collections.defaultdict(int)
+            # Assume AB -> C. If AB appears N times, add AC and CB both N times.
+            for pair, occurances in pairs.items():
                 new_char = formulas[pair]
-                pair1 = pair[0] + new_char
-                pair2 = new_char + pair[1]
-                out[pair1] += count
-                out[pair2] += count
+                new_pairs = (pair[0] + new_char, new_char + pair[1])
+                for new_pair in new_pairs:
+                    out[new_pair] += occurances
             pairs = out
-        counter = collections.defaultdict(int)
-        for pair, count in pairs.items():
-            counter[pair[0]] += count
-            counter[pair[1]] += count
-        counter[original[0]] += 1
-        counter[original[1]] += 1
-        counter = {k: v//2 for k, v in counter.items()}
+        # Count the number of occurances of each letter.
+        counter: dict[str, int] = collections.defaultdict(int)
+        for pair, occurances in pairs.items():
+            counter[pair[0]] += occurances
+            counter[pair[1]] += occurances
+        # Add one more for the first-last which didn't get doubled.
+        for letter in first_last:
+            counter[letter] += 1
+        counter = {k: v // 2 for k, v in counter.items()}
         return max(counter.values()) - min(counter.values())
 
     def parse_input(self, puzzle_input: str) -> InputType:
         """Parse the input data."""
+        # Start formula and a list of formulas.
         start, formula_block = puzzle_input.split("\n\n")
-        formulas = {}
-        for line in formula_block.splitlines():
-            pair, insert = line.split(" -> ")
-            formulas[pair] = insert
+        # The formulas are pairs joined on " -> ".
+        formulas: dict[str, str] = dict(line.split(" -> ") for line in formula_block.splitlines())
         return (start, formulas)
 
 
