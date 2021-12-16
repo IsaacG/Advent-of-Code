901c6e3ea1578f47c71b775e7c1095e38265e1e8
Isaac Good <nuc-git@isaacgood.com>
2021-12-15 23:25:06 -0800

    Partial cleanup 2021/16

--- 2021/16.py
+++ 2021/16.py
@@ -1,101 +1,137 @@
 #!/bin/python
 """Advent of Code: Day 16."""
 
-import collections
 import dataclasses
-import functools
-import math
-import re
+import io
+import operator
+from typing import Optional
 
 import typer
 from lib import aoc
 
-SAMPLE = ["EE00D40C823060", "8A004A801A8002F478", "620080001611562C8802118E34", "C0015000016115A2E0802F182340", "A0016C880162017C3686B18A3D4780"]
 InputType = str
 
+
+
+def hex_to_bits(string):
+    return "".join(bin(int(char, 16))[2:].zfill(4) for char in string)
+
+
 @dataclasses.dataclass
-class Packet:
+class BasePacket:
+    """A transmission packet."""
     version: int
     packet_type: int
-    num: int = -1
-    length_type: int = -1
-    length: int = -1
-    packet_count: int = -1
-    sub_packets: list["Packet"] = None
 
-    def version_sum(self):
-        if self.sub_packets:
-            ts = sum(p.version_sum() for p in self.sub_packets)
+    def __post_init__(self):
+        pass
+
+@dataclasses.dataclass
+class Literal(BasePacket):
+    value: int
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.packet_type != 4:
+            raise ValueError("invalid packet")
+
+    def eval(self):
+        return self.value
+
+    def sum_versions(self):
+        return self.version
+
+
+@dataclasses.dataclass
+class Operator(BasePacket):
+    sub_packets: list["Packet"]
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.packet_type not in (0, 1, 2, 3, 5, 7, 6):
+            raise ValueError("invalid packet")
+        if self.packet_type in (5, 7, 6) and len(self.sub_packets) != 2:
+            raise ValueError("invalid number of subpackets")
+        if self.packet_type > 4:
+            self.binary_operator = True
         else:
-            ts = 0
+            self.binary_operator = False
+
+    def sum_versions(self):
+        ts = sum(p.sum_versions() for p in self.sub_packets)
         return self.version + ts
 
     def eval(self):
-        if self.packet_type == 0: # sum
-            res = 0
-            for p in self.sub_packets:
-                res += p.eval()
-            return res
-        if self.packet_type == 1: # product
-            res = 1
-            for p in self.sub_packets:
-                res *= p.eval()
-            return res
-        if self.packet_type == 2: # min
-            return min(p.eval() for p in self.sub_packets)
-        if self.packet_type == 3: # max
-            return max(p.eval() for p in self.sub_packets)
-        if self.packet_type == 4: # literal
-            return self.num
-        if self.packet_type == 5: # gt
+        op = {
+            0: sum,
+            1: aoc.Helpers().mult,
+            2: min,
+            3: max,
+            5: operator.gt,
+            6: operator.lt,
+            7: operator.eq,
+        }[self.packet_type]
+        if self.binary_operator:
             a, b = [p.eval() for p in self.sub_packets]
-            return 1 if a > b else 0
-        if self.packet_type == 6: # lt
-            a, b = [p.eval() for p in self.sub_packets]
-            return 1 if a < b else 0
-        if self.packet_type == 7: # eq
-            a, b = [p.eval() for p in self.sub_packets]
-            return 1 if a == b else 0
+            return 1 if op(a, b) else 0
+        else:
+            values = (p.eval() for p in self.sub_packets)
+            return op(values)
 
 
-def packet_parser(take_n):
-    version = int(take_n(3), 2)
-    ptype = int(take_n(3), 2)
-    num = None
-    length_type = None
-    length = None
-    packet_count = None
-    if ptype == 4:  # number
+class BitStream(io.StringIO):
+
+    def __init__(self, string: str):
+        if any(s not in ("0", "1") for s in string):
+            raise ValueError("invalid binary stream")
+        super().__init__(string)
+        self.length = len(string)
+
+    @property
+    def done(self) -> bool:
+        return self.tell() == self.length
+
+    def read_int(self, count: int) -> int:
+        return int(self.read(count), 2)
+
+
+def packet_parser(bits_io):
+    version = bits_io.read_int(3)
+    packet_type = bits_io.read_int(3)
+
+    if packet_type == 4:  # number
         packet = []
         more = True
         while more:
-            group = take_n(5)
+            group = bits_io.read(5)
             more, num = group[:1] == "1", group[1:]
             packet.append(num)
         num = int("".join(packet), 2)
         # print(f"Packet Literal {version=} {num=}")
-        return Packet(version, ptype, num=num)
+        return Literal(version, packet_type, num)
 
-    length_type = int(take_n(1))
+    length_type = bits_io.read_int(1)
+    sub_packets = []
+
     if length_type == 0: # 15 bits => body len
-        length = int(take_n(15), 2)
-        sub_packets = []
-        subpackets_str = (take_n(length))
-        subpackets_iter = iter(subpackets_str)
-        # print(f"{version=} Type 0 - Subpacket length = {length}: {subpackets_str=}")
-        while True:
-            subpackets_str = "".join(subpackets_iter)
-            if not subpackets_str:
-                break
-            subpackets_iter = iter(subpackets_str)
-            take_f = lambda n: "".join(next(subpackets_iter) for _ in range(n))
-            sub_packets.append(packet_parser(take_f))
-        return Packet(version, ptype, length_type=length_type, length=length, sub_packets=sub_packets)
+        length = bits_io.read_int(15)
+        sub_io = BitStream(bits_io.read(length))
+
+        # Solving this properly would involve:
+        # sub_end = sub_io.seek(0, io.SEEK_END)
+        # sub_io.seek(0, io.SEEK_SET)
+        # while sub_io.tell() != sub_end:
+        # But in the interest of simple code, just loop until tell == length
+
+        while sub_io.tell() != length:
+            sub_packets.append(packet_parser(sub_io))
+
     elif length_type == 1: # 11 bits => sub-packet count
-        packet_count = int(take_n(11), 2)
-        # print(f"{version=} Type 1 - Subpacket count = {packet_count}")
-        sub_packets = [packet_parser(take_n) for _ in range(packet_count)]
-        return Packet(version, ptype, length_type=length_type, packet_count=packet_count, sub_packets=sub_packets)
+        packet_count = bits_io.read_int(11)
+        for _ in range(packet_count):
+            sub_packets.append(packet_parser(bits_io))
+
+    return Operator(version, packet_type, sub_packets)
 
 
 class Day16(aoc.Challenge):
@@ -103,7 +139,7 @@ class Day16(aoc.Challenge):
     DEBUG = True
 
     TESTS = (
-        # aoc.TestCase(inputs=SAMPLE[0], part=1, want=4),
+        aoc.TestCase(inputs="8A004A801A8002F478", part=1, want=16),
         aoc.TestCase(inputs="8A004A801A8002F478", part=1, want=16),
         aoc.TestCase(inputs="620080001611562C8802118E34", part=1, want=12),
         aoc.TestCase(inputs="C0015000016115A2E0802F182340", part=1, want=23),
@@ -118,53 +154,17 @@ class Day16(aoc.Challenge):
         aoc.TestCase(inputs="9C0141080250320F1802104A08", part=2, want=1),
     )
 
-    # Convert lines to type:
-    INPUT_TYPES = str
-    # Split on whitespace and coerce types:
-    # INPUT_TYPES = [str, int]
-    # Apply a transform function
-    # TRANSFORM = lambda _, l: (l[0], int(l[1:]))
+    def part1(self, parsed_input: str) -> int:
+        bits_io = BitStream(hex_to_bits(parsed_input))
+        return packet_parser(bits_io).sum_versions()
 
-    def part1(self, parsed_input: InputType) -> int:
-        bits = ''
-        for char in parsed_input:
-            byte = bin(int(char, 16))[2:].zfill(4)
-            bits += byte
-        bits_iter = iter(bits)
+    def part2(self, parsed_input: str) -> int:
+        bits_io = BitStream(hex_to_bits(parsed_input))
+        return packet_parser(bits_io).eval()
 
-        def take_n(n):
-            return "".join(next(bits_iter) for _ in range(n))
-
-        packet = packet_parser(take_n)
-        # print(packet)
-        ts = packet.version_sum()
-        print(ts)
-        return ts
-
-    def part2(self, parsed_input: InputType) -> int:
-        bits = ''
-        for char in parsed_input:
-            byte = bin(int(char, 16))[2:].zfill(4)
-            bits += byte
-        bits_iter = iter(bits)
-
-        def take_n(n):
-            return "".join(next(bits_iter) for _ in range(n))
-
-        packet = packet_parser(take_n)
-        return packet.eval()
-
-
-    def parse_input(self, puzzle_input: str) -> InputType:
+    def parse_input(self, puzzle_input: str) -> str:
         """Parse the input data."""
-        return super().parse_input(puzzle_input)[0]
-
-        return puzzle_input.splitlines()
         return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
 
 
 if __name__ == "__main__":
