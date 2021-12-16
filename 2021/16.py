f1d94b6441848c5ac8729fed33238e7558684b2f
Isaac Good <nuc-git@isaacgood.com>
2021-12-15 23:57:46 -0800

    Cleanup 2021/16

--- 2021/16.py
+++ 2021/16.py
@@ -4,15 +4,28 @@
 from __future__ import annotations
 
 import dataclasses
+import enum
 import io
 import math
 import operator
-from typing import Optional
 
 import typer
 from lib import aoc
 
-InputType = str
+
+class PacketType(enum.IntEnum):
+    """Various Packet types."""
+    SUM = 0
+    PROD = 1
+    MIN = 2
+    MAX = 3
+    LITERAL = 4
+    GREATER_THAN = 5
+    LESS_THAN = 6
+    EQUAL = 7
+
+
+BINARY_OPERATORS = (PacketType.GREATER_THAN, PacketType.LESS_THAN, PacketType.EQUAL)
 
 
 @dataclasses.dataclass
@@ -20,12 +33,18 @@ class BasePacket:
     """A base transmission packet."""
 
     version: int
-    packet_type: int
+    packet_type: PacketType
 
     def __post_init__(self) -> None:
         """Validate inputs."""
-        pass
 
+    def eval(self) -> int:
+        """Evaluate the packet to an int."""
+        raise NotImplementedError
+
+    def sum_versions(self) -> int:
+        """Return the version sum value."""
+        return self.version
 
 @dataclasses.dataclass
 class Literal(BasePacket):
@@ -36,44 +55,36 @@ class Literal(BasePacket):
     def __post_init__(self) -> None:
         """Validate inputs."""
         super().__post_init__()
-        if self.packet_type != 4:
+        if self.packet_type != PacketType.LITERAL:
             raise ValueError("invalid packet")
 
     def eval(self) -> int:
         """Evaluate the packet to an int."""
         return self.value
 
-    def sum_versions(self) -> int:
-        """Return the version sum value."""
-        return self.version
-
 
 @dataclasses.dataclass
 class Operator(BasePacket):
     """An Operator transmission packet. This contains an operator and operands."""
 
-    sub_packets: list[BasePacket]
+    operands: list[BasePacket]
 
     def __post_init__(self) -> None:
         """Validate inputs and initialize."""
         super().__post_init__()
-        if self.packet_type not in (0, 1, 2, 3, 5, 7, 6):
+        if self.packet_type == PacketType.LITERAL:
             raise ValueError("invalid packet")
-        if self.packet_type in (5, 7, 6) and len(self.sub_packets) != 2:
+        if self.packet_type in BINARY_OPERATORS and len(self.operands) != 2:
             raise ValueError("invalid number of subpackets")
-        if self.packet_type > 4:
-            self.binary_operator = True
-        else:
-            self.binary_operator = False
 
     def sum_versions(self) -> int:
         """Return the version sum value."""
-        ts = sum(p.sum_versions() for p in self.sub_packets)
-        return self.version + ts
+        operands_versions = sum(p.sum_versions() for p in self.operands)
+        return self.version + operands_versions
 
     def eval(self) -> int:
         """Evaluate the packet to an int."""
-        op = {
+        operation = {
             0: sum,
             1: math.prod,
             2: min,
@@ -82,12 +93,11 @@ class Operator(BasePacket):
             6: operator.lt,
             7: operator.eq,
         }[self.packet_type]
-        if self.binary_operator:
-            a, b = [p.eval() for p in self.sub_packets]
-            return 1 if op(a, b) else 0
+        if self.packet_type in BINARY_OPERATORS:
+            a, b = [p.eval() for p in self.operands]
+            return 1 if operation(a, b) else 0
         else:
-            values = (p.eval() for p in self.sub_packets)
-            return op(values)
+            return operation(p.eval() for p in self.operands)
 
 
 class BitStream(io.StringIO):
@@ -116,33 +126,43 @@ class BitStream(io.StringIO):
         """Read n bits and convert from binary to an int."""
         return int(self.read(count), 2)
 
+    def read_literal(self) -> int:
+        """Read a literal value.
+
+        Read groups of 5 until the first bit is a 0.
+        """
+        literal = ""
+        more = True
+        while more:
+            group = self.read(5)
+            more = group[:1] == "1"
+            literal += group[1:]
+        return int(literal, 2)
+
 
 def packet_parser(bits_io: BitStream) -> BasePacket:
     """Parse a BitStream to a Packet."""
+    # Every packet starts with a version and packet_type.
     version = bits_io.read_int(3)
-    packet_type = bits_io.read_int(3)
+    packet_type = PacketType(bits_io.read_int(3))
 
-    if packet_type == 4:  # number
-        packet = []
-        more = True
-        while more:
-            group = bits_io.read(5)
-            more, num = group[:1] == "1", group[1:]
-            packet.append(num)
-        num = int("".join(packet), 2)
-        # print(f"Packet Literal {version=} {num=}")
-        return Literal(version, packet_type, num)
+    # Literals are easy. Read the literal value and we are done.
+    if packet_type == PacketType.LITERAL:
+        return Literal(version, packet_type, bits_io.read_literal())
 
+    # Other operators have sub_packets.
     length_type = bits_io.read_int(1)
     sub_packets = []
 
-    if length_type == 0: # 15 bits => body len
+    # Type 0: 15 bits indicating the body length
+    if length_type == 0:
         length = bits_io.read_int(15)
         sub_io = BitStream(bits_io.read(length))
         while not sub_io.done:
             sub_packets.append(packet_parser(sub_io))
 
-    elif length_type == 1: # 11 bits => sub-packet count
+    # Type 0: 11 bits indicating a sub_packet count.
+    else:
         packet_count = bits_io.read_int(11)
         for _ in range(packet_count):
             sub_packets.append(packet_parser(bits_io))
@@ -151,6 +171,7 @@ def packet_parser(bits_io: BitStream) -> BasePacket:
 
 
 class Day16(aoc.Challenge):
+    """Parse and evaluate Packet data."""
 
     TESTS = (
         aoc.TestCase(inputs="8A004A801A8002F478", part=1, want=16),
@@ -169,10 +190,12 @@ class Day16(aoc.Challenge):
     )
 
     def part1(self, parsed_input: str) -> int:
+        """Parse the packets and sum up all the versions."""
         bits_io = BitStream.from_hex(parsed_input)
         return packet_parser(bits_io).sum_versions()
 
     def part2(self, parsed_input: str) -> int:
+        """Parse the packets and evaluate."""
         bits_io = BitStream.from_hex(parsed_input)
         return packet_parser(bits_io).eval()
 
