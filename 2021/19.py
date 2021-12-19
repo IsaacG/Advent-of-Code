1e75ede949a3e82121310ab7d6c6b38027ac31f7
Isaac Good <nuc-git@isaacgood.com>
2021-12-19 00:24:27 -0800

    Clean up 2021/09

--- 2021/19.py
+++ 2021/19.py
@@ -6,145 +6,9 @@ import time
 
 import typer
 from lib import aoc
+import input_data
 
-SAMPLE = """\
---- scanner 0 ---
-404,-588,-901
-528,-643,409
--838,591,734
-390,-675,-793
--537,-823,-458
--485,-357,347
--345,-311,381
--661,-816,-575
--876,649,763
--618,-824,-621
-553,345,-567
-474,580,667
--447,-329,318
--584,868,-557
-544,-627,-890
-564,392,-477
-455,729,728
--892,524,684
--689,845,-530
-423,-701,434
-7,-33,-71
-630,319,-379
-443,580,662
--789,900,-551
-459,-707,401
-
---- scanner 1 ---
-686,422,578
-605,423,415
-515,917,-361
--336,658,858
-95,138,22
--476,619,847
--340,-569,-846
-567,-361,727
--460,603,-452
-669,-402,600
-729,430,532
--500,-761,534
--322,571,750
--466,-666,-811
--429,-592,574
--355,545,-477
-703,-491,-529
--328,-685,520
-413,935,-424
--391,539,-444
-586,-435,557
--364,-763,-893
-807,-499,-711
-755,-354,-619
-553,889,-390
-
---- scanner 2 ---
-649,640,665
-682,-795,504
--784,533,-524
--644,584,-595
--588,-843,648
--30,6,44
--674,560,763
-500,723,-460
-609,671,-379
--555,-800,653
--675,-892,-343
-697,-426,-610
-578,704,681
-493,664,-388
--671,-858,530
--667,343,800
-571,-461,-707
--138,-166,112
--889,563,-600
-646,-828,498
-640,759,510
--630,509,768
--681,-892,-333
-673,-379,-804
--742,-814,-386
-577,-820,562
-
---- scanner 3 ---
--589,542,597
-605,-692,669
--500,565,-823
--660,373,557
--458,-679,-417
--488,449,543
--626,468,-788
-338,-750,-386
-528,-832,-391
-562,-778,733
--938,-730,414
-543,643,-506
--524,371,-870
-407,773,750
--104,29,83
-378,-903,-323
--778,-728,485
-426,699,580
--438,-605,-362
--469,-447,-387
-509,732,623
-647,635,-688
--868,-804,481
-614,-800,639
-595,780,-596
-
---- scanner 4 ---
-727,592,562
--293,-554,779
-441,611,-461
--714,465,-776
--743,427,-804
--660,-479,-426
-832,-632,460
-927,-485,-438
-408,393,-506
-466,436,-512
-110,16,151
--258,-428,682
--393,719,612
--211,-452,876
-808,-476,-593
--575,615,604
--485,667,467
--680,325,-822
--627,-443,-432
-872,-547,-609
-833,512,582
-807,604,487
-839,-516,451
-891,-625,532
--652,-548,-490
-30,-46,-14
-"""
+SAMPLE = input_data.D19_SAMPLE
 InputType = list[int]
 
 
@@ -156,6 +20,7 @@ class Day19(aoc.Challenge):
         aoc.TestCase(inputs=SAMPLE, part=2, want=3621),
     )
 
+    @functools.cache
     def orientations(self, scanner):
         """Return all 24 orientations of a beacon map.
 
@@ -203,44 +68,86 @@ class Day19(aoc.Challenge):
         #             continue
         #         assert options[i] != options[j], f"{i}, {j}"
         
-        # Freeze these sets so they are hashable and can be placed into other sets.
-        return frozenset([frozenset(o) for o in options])
+        return options
 
+    def test_orientations(self) -> int:
+        """Test the orientations work."""
+        beacon_maps = self.parse_input(input_data.D19_ORIENTATIONS)
+        initial = beacon_maps[0]
+        options = self.orientations(initial)
+        for scanner in beacon_maps:
+            assert scanner in options
+
+    # This is slow. Cache results so part2 can reuse part1.
     @functools.cache
-    def merge(self, parsed_input):
-        to_merge = set(parsed_input)
+    def merge(self, beacon_maps):
+        """Merge all the sensor maps into a unified map of the beacons.
+
+        Return the unified map as well as the sensor locations.
+
+        Very similar to a jigsaw puzzle, start with a single piece and "fix" it in place.
+        Look through all the "loose" pieces.
+        With every piece, try to connect it to the "fixed" pieces, attempting every
+        possible orientation until something fits. Once a piece fits, "fix" it in place.
+        Repeat until all the loose pieces have been fixed.
+        """
+        # All the beacon_maps need merging.
+        to_merge = set(beacon_maps)
+        # Pick out one piece to start the "fixed" map.
         merged_map = to_merge.pop()
+        # That fixed piece has the scanner at the origin.
         scanners = [(0,0,0)]
-        assert len(merged_map) > 12, merged_map
+        # Expand all the other pieces to the set of all their possible orientations.
+        # to_merge = set(self.orientations(p) for p in to_merge)
+
         start = time.clock_gettime(time.CLOCK_MONOTONIC)
-        to_merge = set(self.orientations(p) for p in to_merge)
+        # Merge beacon maps until all are used.
         while to_merge:
             matched = None
-            for options in list(to_merge):
-                for option in options:
-                    assert isinstance(option, frozenset), options
-                    sample_set = list(option)[:-10]
+            # For every loose piece, try to fit it in.
+            for beacon_map in list(to_merge):
+                # Try to fit the loose piece in every possible orientation.
+                for orientation in self.orientations(beacon_map):
+                    orientation = list(orientation)
+                    # Rather than working with the entire set of beacons in the map,
+                    # ignore 10 at first. Try to the other beacons match.
+                    # If at least two beacons match, then maybe there are 12 that match!
+                    # At that point, add back those 10 points and see if there is a full 12.
+                    sample_set, rest_of_points = orientation[:-10], orientation[10:]
+                    # When checking to see if a piece "fits" into the map, move the beacon map
+                    # such that a point on the beacon map aligns with a point on the fixed map.
+                    # If 12 points align at any point, this is a "match".
+                    # We need to try aligning every point on the beacon map with every point on
+                    # the fixed map. Since at least 12 points should line up, there are 12 points
+                    # on the beacon map that line up in the same way on the fixed map.
+                    # That is 12 translations that are identical. As such, we can trying to translate
+                    # 11 points on the beacon map as redundant.
                     for mp in merged_map:
-                        for point in list(option)[:-11]:
+                        for point in orientation[:-11]:
+                            # How much this beacon map needs shifting to align.
                             translation = tuple(a - b for a, b in zip(point, mp))
+                            # Translate the sample points.
                             translated = set(tuple(p - t for p, t in zip(x, translation)) for x in sample_set)
-                            if len(merged_map.intersection(translated)) < 2:
+                            # If only one point lines up, move on.
+                            if len(merged_map.intersection(translated)) == 2:
                                 continue
 
-                            translated = set(tuple(p - t for p, t in zip(x, translation)) for x in option)
+                            # Otherwise, go for a full match.
+                            translated.update(tuple(p - t for p, t in zip(x, translation)) for x in rest_of_points)
                             overlap = len(merged_map.intersection(translated))
                             if overlap >= 12:
-                                matched = options
+                                matched = True
+                                # Fix this loose piece to the overall map.
                                 merged_map = merged_map.union(translated)
                                 scanners.append(translation)
-                                to_merge.remove(matched)
+                                # Remove this piece from the loose pieces.
+                                to_merge.remove(beacon_map)
                                 end = time.clock_gettime(time.CLOCK_MONOTONIC)
                                 delta = int(end - start)
                                 self.debug(f"{len(to_merge)=} {len(merged_map)=} time: {delta}")
                                 break
                         if matched: break
                     if matched: break
-            assert matched
         return merged_map, scanners
 
     def part1(self, parsed_input: InputType) -> int:
@@ -254,20 +161,13 @@ class Day19(aoc.Challenge):
         distances = [sum(abs(i - j) for i, j in zip(a, b)) for a in scanners for b in scanners]
         return max(distances)
 
-    def test_orientations(self, parsed_input: InputType) -> int:
-        initial = parsed_input[0]
-        options = self.orientations(initial)
-        for scanner in parsed_input:
-            assert scanner in options
-        return 5
-
     def parse_input(self, puzzle_input: str) -> InputType:
         """Parse the input data."""
         scans = []
         for block in puzzle_input.split("\n\n"):
             points = set()
             for line in block.splitlines()[1:]:
-                x, y, z = line.split(",")
+                x, y, z = line.strip().split(",")
                 points.add(tuple([int(x), int(y), int(z)]))
             scans.append(frozenset(points))
         return frozenset(scans)
