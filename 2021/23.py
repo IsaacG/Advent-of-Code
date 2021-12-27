7f7530f73710045d2b3889b6a581324cd9527c60
Isaac Good <github@isaacgood.com>
2021-12-27 12:11:16 -0800

    2021/23 Cleanup and solve part 2

--- 2021/23.py
+++ 2021/23.py
@@ -2,8 +2,6 @@
 """Advent of Code: Day 23.
 
 Optimizations needed:
-* Never move C/D pieces in an x-direction away from final room.
-* When possible, move piece direct to home-room and skip hallways.
 * Use A* and h(): x-distance
 
 The A* should help when you have:
@@ -40,144 +38,180 @@ import re
 import typer
 from lib import aoc
 
-SAMPLE = ["""\
+SAMPLE = """\
 #############
 #...........#
 ###B#C#B#D###
   #A#D#C#A#
   #########
-"""]
 """
-           01234567890
-          #############
-        0 #...........#
-        1 ###C#C#B#D###
-        2   #D#A#B#A#
-            #########
-             2 4 6 8 
-"""
-InputType = list[int]
+InputType = tuple[tuple[str, int, int], ...]
 
 STEP_COST = {"A": 1, "B": 10, "C": 100, "D": 1000}
-ROOM_X = (2,4,6,8)
-VALID_HALLWAY = {(x, 0) for x in range(11) if x not in ROOM_X}
+ROOM_X = dict(zip("ABCD", (2, 4, 6, 8)))
+VALID_HALLWAY = {(x, 0) for x in range(11) if x not in ROOM_X.values()}
 ALL_HALLWAY = {(x, 0) for x in range(11)}
-ROOM_DEPTH = 2
-ROOM = {
-    name: {(ROOM_X[i], y) for y in range(1, ROOM_DEPTH+1)}
-    for i, name in enumerate("ABCD")
-}
-ALL_ROOMS = set(itertools.chain.from_iterable(ROOM.values()))
-ALL_SPOTS = ALL_HALLWAY | ALL_ROOMS
-VALID_SPOTS = VALID_HALLWAY | ALL_ROOMS
-VALID_FOR_TYPE = {n: VALID_HALLWAY | ROOM[n] for n in "ABCD"}
 
 
-@functools.cache
-def can_reach(start, occupied):
-    to_explore = set([start])
-    explored = set()
-    steps = {start: 0}
-    while to_explore:
-        cur = sorted(to_explore, key=lambda x: steps[x])[0]
-        to_explore.remove(cur)
-        explored.add(cur)
-        for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            maybe = (cur[0] + x, cur[1] + y)
-            if maybe in explored or maybe in occupied:
-                continue
-            if maybe in ALL_SPOTS:
-                to_explore.add(maybe)
-                steps[maybe] = steps[cur] + 1
-    del steps[start]
-    return {pos: cost for pos, cost in steps.items() if pos in VALID_SPOTS}
+class AmphipodGame:
 
+    def __init__(self, room_size, iter_limit):
+        self.room_size = room_size
+        self.iter_limit = iter_limit
 
-@functools.cache
-def next_board(positions):
-    next_positions = {}
-    for piece in positions:
-        for n, cost in valid_moves(piece, positions).items():
-            assert n not in next_positions
-            next_positions[n] = cost
-    return next_positions
+        self.room = {
+            name: {(x, y) for y in range(1, room_size+1)}
+            for name, x in ROOM_X.items()
+        }
+        all_rooms = set(itertools.chain.from_iterable(self.room.values()))
+        self.all_spots = ALL_HALLWAY | all_rooms
+        self.valid_spots = VALID_HALLWAY | all_rooms
 
+    def solve(self, pieces):
+        for assumption in ["ABCD"[i:] for i in range(1, 5)]:
+            if solution := self.solve_with_assumption(pieces, assumption):
+                return solution
 
-@functools.cache
-def valid_moves(start, positions):
-    amphi, loc = start[0], start[1:]
-    locations = just_positions(positions)
-    reachable = can_reach(loc, locations)
-    room = ROOM[amphi]
-    only_right = not any(
-        (x, y) in room and piece != amphi
-        for piece, x, y in positions
-    )
-    step_cost = STEP_COST[amphi]
+    def solve_with_assumption(self, pieces, assumption):
+        self.no_extra_move = assumption
+        to_explore = set([pieces])
+        cost = {pieces: 0}
+        f_cost = {pieces: 0}
 
-    valid = reachable.keys()
-    
-    # If the amphipod is in the hallway, it will only move to its room, and only
-    # if no other amphipod types are in that room.
-    if loc in VALID_HALLWAY:
-        if only_right and (vr := valid & room):
-            valid = set([max(vr)])
-        else:
+        count = 0
+        while to_explore and count < self.iter_limit:
+            count += 1
+            current = sorted(to_explore, key=lambda x: f_cost[x])[0]
+            to_explore.remove(current)
+
+            for board, move_cost in self.next_board(current).items():
+                combined_cost = cost[current] + move_cost
+                if board in cost and cost[board] <= combined_cost:
+                    continue
+                cost[board] = combined_cost
+                f_cost[board] = combined_cost + self.heuristic(board)
+                to_explore.add(board)
+
+        final_pos = tuple(sorted((amphi, x, y) for amphi, room in self.room.items() for x, y in room))
+        return cost.get(final_pos)
+
+    @functools.cache
+    def heuristic(self, pieces):
+        cost = 0
+        for piece in pieces:
+            amphi, x, y = piece
+            want_x = ROOM_X[amphi]
+            cost += STEP_COST[amphi] * abs(x - want_x)
+        return cost
+
+    @functools.cache
+    def can_reach(self, start, occupied):
+        to_explore = set([start])
+        explored = set()
+        steps = {start: 0}
+        while to_explore:
+            cur = sorted(to_explore, key=lambda x: steps[x])[0]
+            to_explore.remove(cur)
+            explored.add(cur)
+            for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                maybe = (cur[0] + x, cur[1] + y)
+                if maybe in explored or maybe in occupied:
+                    continue
+                if maybe in self.all_spots:
+                    to_explore.add(maybe)
+                    steps[maybe] = steps[cur] + 1
+        del steps[start]
+        return {pos: cost for pos, cost in steps.items() if pos in self.valid_spots}
+
+    def next_board(self, positions):
+        next_positions = {}
+        for piece in positions:
+            for n, cost in self.valid_moves(piece, positions).items():
+                next_positions[n] = cost
+        return next_positions
+
+    def valid_moves(self, start, positions):
+        amphi, loc = start[0], start[1:]
+        locations = tuple(sorted(i[1:] for i in positions))
+        room = self.room[amphi]
+        only_right = not any(
+            (x, y) in room and piece != amphi
+            for piece, x, y in positions
+        )
+        step_cost = STEP_COST[amphi]
+
+        # If the amphipod is in its room and there are no other types of amphipods in that
+        # room, it will not move.
+        if loc in room and only_right:
             return {}
-    # If the amphipod is in its room and there are no other types of amphipods in that
-    # room, it will not move.
-    elif loc in room and only_right:
-        return {}
-    # Otherwise, the amphipod will only move to the hallway..
-    else:
-        valid &= VALID_HALLWAY
+        # If the amphipod is in the hallway, it will only move to the home room.
+        # If the home room has other amphipods, it will not move.
+        if loc in VALID_HALLWAY and not only_right:
+            return {}
 
-    next_steps = {}
-    other_pieces = list(positions)
-    other_pieces.remove(start)
-    for p, c in reachable.items():
-        if p not in valid:
-            continue
-        energy = c * step_cost
-        new_positions = tuple(sorted(other_pieces + [(amphi,) + p]))
-        next_steps[new_positions] = energy
-    return next_steps
+        reachable = self.can_reach(loc, locations)
+        valid = reachable.keys()
+        valid_to_home = valid & room
 
-
-def just_positions(pieces):
-    return tuple(sorted(i[1:] for i in pieces))
-
-
-def show(pieces):
-    locations = {}
-    for piece in pieces:
-        if len(piece) == 2:
-            n = "X"
-            x, y = piece
-        elif len(piece) == 3:
-            n, x, y = piece
+        # If the amphipod can move to the home room, always do that.
+        if only_right and valid_to_home:
+            valid = set([max(valid_to_home)])
+        # If the amphipod is in the hallway, it will only move to the home room.
+        # That is checked in the prior clause. If that is not an option,
+        # the amphipod will stay in the hallway without moving.
+        elif loc in VALID_HALLWAY:
+            return {}
+        # Otherwise, the amphipod will only move to the hallway..
         else:
-            raise ValueError(repr(piece))
-        locations[(x, y)] = n
-    print("#" * 13)
-    for y in range(3):
-        if y == 0:
-            line = "#"
-            for x in range(11):
-                line += locations.get((x, y), " ")
-            line += "#"
-            print(line)
-        else:
-            line = "#"
-            for x in range(11):
-                if x in (2, 4, 6, 8):
-                    line += locations.get((x, y), " ")
+            valid &= VALID_HALLWAY
+            # Attempt to solve assuming no_extra_move pieces never move in the direction away from their home room.
+            if amphi in self.no_extra_move and loc not in room:
+                want_x = ROOM_X[amphi]
+                if loc[0] > want_x:
+                    valid_x = range(want_x, loc[0] + 1)
                 else:
-                    line += "#"
-            line += "#"
-            print(line)
-    print("#" * 13)
-    print()
+                    valid_x = range(loc[0], want_x + 1)
+                valid = {(x, y) for x, y in valid if x in valid_x}
+
+        next_steps = {}
+        other_pieces = list(positions)
+        other_pieces.remove(start)
+        for p in valid:
+            energy = reachable[p] * step_cost
+            new_positions = tuple(sorted(other_pieces + [(amphi,) + p]))
+            next_steps[new_positions] = energy
+        return next_steps
+
+    def show(self, pieces):
+        locations = {}
+        for piece in pieces:
+            if len(piece) == 2:
+                n = "X"
+                x, y = piece
+            elif len(piece) == 3:
+                n, x, y = piece
+            else:
+                raise ValueError(repr(piece))
+            locations[(x, y)] = n
+        print("#" * 13)
+        for y in range(3):
+            if y == 0:
+                line = "#"
+                for x in range(11):
+                    line += locations.get((x, y), " ")
+                line += "#"
+                print(line)
+            else:
+                line = "#"
+                for x in range(11):
+                    if x in (2, 4, 6, 8):
+                        line += locations.get((x, y), " ")
+                    else:
+                        line += "#"
+                line += "#"
+                print(line)
+        print("#" * 13)
+        print()
 
 
 class Day23(aoc.Challenge):
@@ -186,49 +220,31 @@ class Day23(aoc.Challenge):
     SUBMIT = {1: False, 2: False}
 
     TESTS = (
-        aoc.TestCase(inputs=SAMPLE[0], part=1, want=12521),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=0),
+        aoc.TestCase(inputs=SAMPLE, part=1, want=12521),
+        aoc.TestCase(inputs=SAMPLE, part=2, want=44169),
     )
+
+    def part2(self, parsed_input: InputType) -> int:
+        pieces = parsed_input
+        updated = []
+        # Shift the bottom row down by two.
+        for amphipod, x, y in pieces:
+            if y == 2:
+                y = 4
+            updated.append((amphipod, x, y))
+        # Add the extra two rows.
+        updated.extend([
+            ("D", 2, 2), ("C", 4, 2), ("B", 6, 2), ("A", 8, 2),
+            ("D", 2, 3), ("B", 4, 3), ("A", 6, 3), ("C", 8, 3),
+        ])
+        pieces = tuple(sorted(updated))
+        game = AmphipodGame(4, 10000000)
+        return game.solve(pieces)
 
     def part1(self, parsed_input: InputType) -> int:
         pieces = parsed_input
-        show(pieces)
-        print(1)
-        a = ("A", 2, 2)
-        b = ("B", 9, 0)
-        c = ("C", 2, 1)
-        print(valid_moves(a, (a, b, c)))
-        print(2)
-        show((a, b, c))
-        for board, cost in next_board((a,b,c)).items():
-            print(cost)
-            show(board)
-        # return 0
-
-        to_explore = set([pieces])
-        cost = {pieces: 0}
-
-        count = 0
-        while to_explore and count < 100000:
-            count += 1
-
-            current = sorted(to_explore, key=lambda x: cost[x])[0]
-            if count % 100 == 0:
-                print(len(cost))
-                print(cost[current])
-                show(current)
-            to_explore.remove(current)
-
-            for board, move_cost in next_board(current).items():
-                combined_cost = cost[current] + move_cost
-                if board in cost and cost[board] <= combined_cost:
-                    continue
-                cost[board] = combined_cost
-                to_explore.add(board)
-
-        final_pos = tuple(sorted((amphi, x, y) for amphi, room in ROOM.items() for x, y in room))
-        show(final_pos)
-        return cost[final_pos]
+        game = AmphipodGame(2, 100000)
+        return game.solve(parsed_input)
 
     def parse_input(self, puzzle_input: str) -> InputType:
         """Parse the input data.
@@ -241,13 +257,6 @@ class Day23(aoc.Challenge):
         2   #D#A#B#A#
             #########
              2 4 6 8 
-
-          #############
-        0 #...........#
-        1 ###B#C#B#D###
-        2   #A#D#C#A#
-            #########
-           0 2 4 6 8 10
         """
         locations = []
         room_lines = puzzle_input.splitlines()[2:4]
