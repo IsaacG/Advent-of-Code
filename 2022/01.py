e564f6eb4215843749f13babeb2347d523251d1e
Isaac Good <github@isaacgood.com>
2022-11-30 21:19:17 -0800

    Solve 2022d01

---
+++ 2022/01.py
@@ -0,0 +1,49 @@
+#!/bin/python
+"""Advent of Code: Day 01."""
+
+import typer
+from lib import aoc
+
+SAMPLE = """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+InputType = list[list[int]]
+
+
+class Day01(aoc.Challenge):
+    """Day 1: Calorie Counting. Count how many calories the elves have."""
+
+    TESTS = (
+        aoc.TestCase(inputs=SAMPLE, part=1, want=24000),
+        aoc.TestCase(inputs=SAMPLE, part=2, want=45000),
+    )
+
+    def part1(self, parsed_input: InputType) -> int:
+        """Return the sum calories held by the elf with the most calories."""
+        return max(sum(i) for i in parsed_input)
+
+    def part2(self, parsed_input: InputType) -> int:
+        """Return the sum calories held by the top three elves with the most calories."""
+        return sum(sorted(sum(i) for i in parsed_input)[-3:])
+
+    def parse_input(self, puzzle_input: str) -> InputType:
+        """Parse the input data."""
+        return [[int(j) for j in i.splitlines()] for i in  puzzle_input.split("\n\n")]
+
+
+if __name__ == "__main__":
+    typer.run(Day01().run)
+
+# vim:expandtab:sw=4:ts=4
---
+++ 2022/tmpl.py
@@ -0,0 +1,51 @@
+#!/bin/python
+"""Advent of Code: Day ${day}."""
+
+import collections
+import functools
+import math
+import re
+
+import typer
+from lib import aoc
+
+SAMPLE = ${sample}
+InputType = list[int]
+
+
+class Day${day}(aoc.Challenge):
+
+    DEBUG = True
+    SUBMIT = {1: False, 2: False}
+
+    TESTS = (
+        aoc.TestCase(inputs=SAMPLE[0], part=1, want=0),
+        aoc.TestCase(inputs=SAMPLE[0], part=2, want=0),
+    )
+
+    # Convert lines to type:
+    INPUT_TYPES = int
+    # Split on whitespace and coerce types:
+    # INPUT_TYPES = [str, int]
+    # Apply a transform function
+    # TRANSFORM = lambda _, l: (l[0], int(l[1:]))
+
+    def part1(self, parsed_input: InputType) -> int:
+        return 0
+
+    def parse_input(self, puzzle_input: str) -> InputType:
+        """Parse the input data."""
+        return super().parse_input(puzzle_input)
+
+        return puzzle_input.splitlines()
+        return puzzle_input
+        return [int(i) for i in puzzle_input.splitlines()]
+
+        mutate = lambda x: (x[0], int(x[1])) 
+        return [mutate(line.split()) for line in puzzle_input.splitlines()]
+
+
+if __name__ == "__main__":
+    typer.run(Day${day}().run)
+
+# vim:expandtab:sw=4:ts=4
