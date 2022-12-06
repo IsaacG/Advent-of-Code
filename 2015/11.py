6cabcbd57ace70ef1fc2b1c0c58a850819991053
Isaac Good <github@isaacgood.com>
2022-12-05 17:39:16 -0800

    Tidy 2015/11

--- 2015/11.py
+++ 2015/11.py
@@ -1,23 +1,21 @@
 #!/bin/python
 """Advent of Code: Day 11."""
 
-import collections
-import functools
-import math
 import string
-import re
 
 import typer
 from lib import aoc
 
 SAMPLE = ["abcdefgh", "ghijklmn"]
 
-LineType = int
-InputType = list[LineType]
-NOT_ALLOWED = {string.ascii_lowercase.index(i) for i in "iol"}
+InputType = str
+NOT_ALLOWED = {
+    string.ascii_lowercase.index(i) for i in "iol"
+}
 
 
 def parts(num: int) -> list[int]:
+    """Return 8 places of base-26 values for a number."""
     parts = []
     for i in range(8):
         num, b = divmod(num, 26)
@@ -26,28 +24,31 @@ def parts(num: int) -> list[int]:
     return parts
 
 
-def valid(password: str) -> bool:
+def valid(password: list[int]) -> bool:
+    """Return if a password is valid."""
+    # Invalid chars.
     if any(i in password for i in NOT_ALLOWED):
         return False
-    if not any(password[i] + 2 == password[i + 1] + 1 == password[i + 2] for i in range(6)):
+    # Three incremental numbers.
+    if not any(
+        password[i] + 2 == password[i + 1] + 1 == password[i + 2]
+        for i in range(6)
+    ):
         return False
 
-    overlaps = 0
+    # Two non-overlapping pairs.
+    pairs = 0
     i = 0
     while i < 7:
         if password[i] == password[i + 1]:
             i += 1
-            overlaps += 1
+            pairs += 1
         i += 1
-    return overlaps > 1
+    return pairs > 1
 
 
 class Day11(aoc.Challenge):
-    """Day 11: Corporate Policy."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
+    """Day 11: Corporate Policy. Rotate passwords."""
 
     TESTS = [
         aoc.TestCase(inputs=SAMPLE[0], part=1, want="abcdffaa"),
@@ -55,27 +56,32 @@ class Day11(aoc.Challenge):
         aoc.TestCase(inputs=SAMPLE[0], part=2, want="abcdffbb"),
     ]
 
-    INPUT_TYPES = str
-
     def next_password(self, password: str) -> str:
+        """Generate the next password."""
+        # String to int
         num = 0
         for char in password:
             num *= 26
             num += string.ascii_lowercase.index(char)
+
+        # Increment until valid.
         num += 1
         while not valid(parts(num)):
             num += 1
 
+        # Int to string.
         password = "".join(string.ascii_lowercase[i] for i in parts(num))
         return password
 
     def part1(self, parsed_input: InputType) -> int:
+        """Rotate once."""
         assert valid(parts(334140716))  # abcdffaa
         assert valid(parts(50460204602))  # ghjaabcc
 
         return self.next_password(parsed_input)
 
     def part2(self, parsed_input: InputType) -> int:
+        """Rotate twice."""
         password = self.next_password(parsed_input)
         return self.next_password(password)
 
