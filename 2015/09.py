b0d535c3b0b537780107d2194a04e3631f8e331a
Isaac Good <github@isaacgood.com>
2022-12-07 13:10:19 -0800

    Tidy 2015/09

--- 2015/09.py
+++ 2015/09.py
@@ -1,10 +1,7 @@
 #!/bin/python
-"""Advent of Code: Day 09."""
+"""Advent of Code: Day 09. All in a Single Night."""
 
-import collections
-import functools
 import itertools
-import math
 import re
 
 import typer
@@ -16,53 +13,46 @@ London to Belfast = 518
 Dublin to Belfast = 141
 """]
 
-LineType = tuple[str, str, int]
-InputType = list[LineType]
+InputType = list[int]
 LINE_RE = re.compile(r"^(.+) to (.+) = (\d+)$")
 
 
 class Day09(aoc.Challenge):
-    """Day 9: All in a Single Night."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
+    """Day 9: Compute the cost of visiting all cities.."""
 
     TESTS = [
         aoc.TestCase(inputs=SAMPLE[0], part=1, want=605),
         aoc.TestCase(inputs=SAMPLE[0], part=2, want=982),
     ]
 
-    def distances(self, parsed_input: InputType) -> list[int]:
+    def part1(self, parsed_input: InputType) -> int:
+        """Return the shortest route."""
+        return min(parsed_input)
+
+    def part2(self, parsed_input: InputType) -> int:
+        """Return the longest route."""
+        return max(parsed_input)
+
+    def input_parser(self, puzzle_input: str) -> InputType:
+        """Parse the input and generate the total cost for each route."""
         distances = {}
         locations: set[str] = set()
-        for src, dst, dist in parsed_input:
+        for line in puzzle_input.splitlines():
+            src, dst, dist_raw = LINE_RE.match(line).groups()
+            dist = int(dist_raw)
+
             distances[src, dst] = dist
             distances[dst, src] = dist
             locations.add(src)
             locations.add(dst)
 
-        def cost(order: tuple[str, ...]) -> int:
-            return sum(
+        return [
+            sum(
                 distances[src, dst]
                 for src, dst in zip(order, order[1:])
             )
-
-        return [
-            cost(order)
             for order in itertools.permutations(locations, len(locations))
         ]
-
-    def part1(self, parsed_input: InputType) -> int:
-        return min(self.distances(parsed_input))
-
-    def part2(self, parsed_input: InputType) -> int:
-        return max(self.distances(parsed_input))
-
-    def line_parser(self, line: str) -> LineType:
-        """If defined, use this to parse single lines."""
-        src, dst, dist = LINE_RE.match(line).groups()
-        return (src, dst, int(dist))
 
 
 if __name__ == "__main__":
