5ca3a7e4e796a9ee7d88eb1c82658d1a33203880
Isaac Good <github@isaacgood.com>
2022-12-06 22:11:08 -0800

    Reduce 2022/06

--- 2022/07.py
+++ 2022/07.py
@@ -2,7 +2,6 @@
 """Advent of Code: Day 07."""
 
 from __future__ import annotations
-import collections
 import pathlib
 
 import typer
@@ -41,20 +40,15 @@ ROOT = pathlib.Path("/")
 class DirectoryEntry:
     """Information about a directory."""
 
-    def __init__(self, pwd: pathlib.Path, data: dir[pathlib.Path, DirectoryEntry]):
+    def __init__(self, pwd: pathlib.Path, data: dict[pathlib.Path, DirectoryEntry]):
         self.pwd = pwd
-        self.dirs = set()
-        self.files = {}
+        self.dirs: set[str] = set()
+        self.size = 0
         self.data = data
 
-    def __repr__(self) -> str:
-        return f"{self.pwd} {self.dirs} {self.files}"
-
-    def size(self) -> int:
+    def rsize(self) -> int:
         """Return the recursive directory size."""
-        size = sum(self.files.values())
-        size += sum(self.data[self.pwd / child].size() for child in self.dirs)
-        return size
+        return self.size + sum(self.data[self.pwd / child].rsize() for child in self.dirs)
 
 
 class Day07(aoc.Challenge):
@@ -64,43 +58,36 @@ class Day07(aoc.Challenge):
         aoc.TestCase(inputs=SAMPLE, part=1, want=95437),
         aoc.TestCase(inputs=SAMPLE, part=2, want=24933642),
     ]
-
     INPUT_TYPES = LineType
 
-    def directory_data(self, stdout: list[str]) -> dict[tuple, DirectoryEntry]:
+    def directory_data(self, stdout: list[str]) -> dict[pathlib.Path, DirectoryEntry]:
         """Return the parsed filesystem details."""
-        dirs = {}
+        dirs: dict[pathlib.Path, DirectoryEntry] = {}
         pwd = ROOT
-        lines = collections.deque(stdout)
-        while lines:
-            words = lines.popleft().split()
-            if words[0] == "$" and words[1] == "cd":
-                if words[2] == "/":
-                    pwd = ROOT
-                elif words[2] == "..":
-                    pwd = pwd.parent
+        for line in stdout:
+            if line == "$ cd /":
+                pwd = ROOT
+            elif line == "$ cd ..":
+                pwd = pwd.parent
+            elif line.startswith("$ cd"):
+                pwd /= line.split()[-1]
+            elif line == "$ ls":
+                dirs[pwd] = DirectoryEntry(pwd, dirs)
+            else:  # ls output
+                type_or_size, name = line.split()
+                if type_or_size == "dir":
+                    dirs[pwd].dirs.add(name)
                 else:
-                    pwd /= words[2]
-            elif words[0] == "$" and words[1] == "ls":
-                if pwd not in dirs:
-                    dirs[pwd] = DirectoryEntry(pwd, dirs)
-                while lines and not lines[0].startswith("$"):
-                    type_or_size, name = lines.popleft().split()
-                    if type_or_size == "dir":
-                        dirs[pwd].dirs.add(name)
-                    else:
-                        dirs[pwd].files[name] = int(type_or_size)
-            else:
-                raise ValueError("Unrecognized command. {words}")
+                    dirs[pwd].size += int(type_or_size)
         return dirs
 
     def part1(self, parsed_input: InputType) -> int:
         """Return the sum of all dirs over 100000 in size."""
         dirs = self.directory_data(parsed_input)
         return sum(
-            directory.size()
+            directory.rsize()
             for directory in dirs.values()
-            if directory.size() <= 100000
+            if directory.rsize() <= 100000
         )
 
     def part2(self, parsed_input: InputType) -> int:
@@ -109,15 +96,15 @@ class Day07(aoc.Challenge):
 
         # space_total = 70000000
         # target = 30000000
-        # space_used = dirs[tuple()].size()
+        # space_used = dirs[tuple()].rsize()
         # already_free = space_total - space_used
         # to_free = target - already_free
-        to_free = 30000000 - 70000000 + dirs[ROOT].size()
+        to_free = 30000000 - 70000000 + dirs[ROOT].rsize()
 
         return min(
-            directory.size()
+            directory.rsize()
             for directory in dirs.values()
-            if directory.size() >= to_free
+            if directory.rsize() >= to_free
         )
 
 
