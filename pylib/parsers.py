e1f40f1b4853080ac0343e70cc9fb224061eeb63
Isaac Good <github@isaacgood.com>
2022-12-10 12:17:39 -0800

    Create new parser lib

---
+++ pylib/parsers.py
@@ -0,0 +1,142 @@
+"""AoC Parser collection.
+
+aoc.Challenge classes can define an INPUT_PARSER.
+That value should be a ParserBase instance.
+The Parser class shall have a `parse()` method which takes the puzzle input
+and returns the parsed value.
+"""
+
+import inspect
+import re
+from typing import Any, Callable, Iterable
+
+
+def input_to_ints(inputs: Iterable[str]) -> list[int, ...]:
+    """Return a list of int values from an iterable."""
+    return [int(i) for i in inputs]
+
+
+def input_to_strs(inputs: Iterable[str]) -> list[str, ...]:
+    """Return a list of str values from an iterable."""
+    return [str(i) for i in inputs]
+
+
+def input_to_mixed(inputs: Iterable[str]) -> list[int | str, ...]:
+    """Return a list of int | str values from an iterable."""
+    return [int(i) if i.isdigit() else str(i) for i in inputs]
+
+
+class ParserBase:
+    """Base class for input parsing."""
+
+    def parse(self, puzzle_input: str) -> Any:
+        raise NotImplementedError
+
+
+class ParseOneWord:
+    """Parse an input which is a single line with a single word."""
+
+    def __init__(self, input_type: Callable[[str], Any]):
+        self.input_type = input_type
+
+    def parse(self, puzzle_input: str) -> Any:
+        return self.input_type(puzzle_input)
+
+
+class ParseOneWordPerLine:
+    """Parse an input which is multiple lines with a single word per line."""
+
+    def __init__(self, input_type: Callable[[str], Any]):
+        self.input_type = input_type
+
+    def parse(self, puzzle_input: str) -> list[Any]:
+        return [
+            self.input_type(line)
+            for line in puzzle_input.splitlines()
+        ]
+
+
+class BaseParseMultiPerLine(ParserBase):
+    """Parse an input which is multiple lines with multiple words per line."""
+
+    def __init__(self, convert: Callable[[Iterable[str]], Any]):
+        self.convert = convert
+
+    def parse(self, puzzle_input: str) -> list[list[Any, ...]]:
+        return [
+            self.convert(line.split())
+            for line in puzzle_input.splitlines()
+        ]
+
+
+class BaseParseRe(ParserBase):
+    """Parse multi-line input by applying a regex to find words on each line."""
+
+    def __init__(self, regex: str, convert: Callable[[Iterable[str]], list[Any, ...]]):
+        self.compiled = re.compile(regex)
+        self.convert = convert
+
+    def parse(self, puzzle_input: str) -> list[list[Any, ...]]:
+        return [
+            self.convert(self.compiled.findall(line))
+            for line in puzzle_input.splitlines()
+        ]
+
+    def line_to_iterable(self, line: str) -> Iterable[str]:
+        raise NotImplementedError
+
+
+class BaseParseReFindall(BaseParseRe):
+    """Parse an input by applying re.Pattern.findall to each line."""
+
+    def line_to_iterable(self, line: str) -> Iterable[str]:
+        return self.compiled.findall(line)
+
+
+class BaseParseReGroups(ParserBase):
+    """Parse an input by applying re.Pattern.groups to each line."""
+
+    def line_to_iterable(self, line: str) -> Iterable[str]:
+        return self.compiled.match(line).groups()
+
+
+class ParseBlocks(ParserBase):
+    """Parse an input by splitting into blocks and applying a parser to each block."""
+
+    def __init__(self, block_parsers: Iterable[ParserBase]):
+        for parser in block_parsers:
+            if inspect.isclass(parser):
+                raise ValueError(f"{parser!r} is a class and not an instance!")
+        self.block_parsers = block_parsers
+
+    def parse(self, puzzle_input: str) -> list[Any]:
+        blocks = puzzle_input.split("\n\n")
+        outputs = []
+        for block, parser in zip(blocks, itertools.cycle(self.block_parsers)):
+            outputs.append(parser.parse(block))
+        return outputs
+
+
+# Convert the entire input into one str.
+parse_one_str = ParseOneWord(str)
+# Convert the entire input into one int.
+parse_one_int = ParseOneWord(int)
+
+# Convert the input into list[type], one per line.
+parse_one_str_per_line = ParseOneWordPerLine(str)
+parse_one_int_per_line = ParseOneWordPerLine(int)
+
+# Convert the input into list[list[str]], splitting each line into multiple words.
+parse_multi_str_per_line = BaseParseMultiPerLine(input_to_strs)
+parse_re_group_str = lambda x: BaseParseReGroups(x, input_to_strs)
+parse_re_findall_str = lambda x: BaseParseReFindall(x, input_to_strs)
+
+# Convert the input into list[list[int]], splitting each line into multiple words.
+parse_multi_int_per_line = BaseParseMultiPerLine(input_to_ints)
+parse_re_group_int = lambda x: BaseParseReGroups(x, input_to_ints)
+parse_re_findall_int = lambda x: BaseParseReFindall(x, input_to_ints)
+
+# Convert the input into list[list[int | str]], splitting each line into multiple words.
+parse_multi_mixed_per_line = BaseParseMultiPerLine(input_to_mixed)
+parse_re_group_mixed = lambda x: BaseParseReGroups(x, input_to_mixed)
+parse_re_findall_mixed = lambda x: BaseParseReFindall(x, input_to_mixed)
