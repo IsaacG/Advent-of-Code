75840d9c05f2a2ba571cf64a293a5ebfdc2c976d
Isaac Good <github@isaacgood.com>
2022-12-12 22:14:23 -0800

    Tidy 2022/13

--- 2022/13.py
+++ 2022/13.py
@@ -2,18 +2,14 @@
 """Advent of Code, Day 13: Distress Signal."""
 from __future__ import annotations
 
-import random
-import collections
 import functools
-import itertools
-import math
-import re
+import json
+from typing import Any
 
 import typer
 from lib import aoc
 
-SAMPLE = [
-    """\
+SAMPLE = """\
 [1,1,3,1,1]
 [1,1,5,1,1]
 
@@ -36,131 +32,64 @@ SAMPLE = [
 [[]]
 
 [1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]""",  # 0
-]
+[1,[2,[3,[4,[5,6,0]]]],8,9]"""
 
-LineType = int
-InputType = list[LineType]
+InputType = list[list[Any]]
 
-
-def dosort(vals):
-    for i in range(len(vals) - 1):
-        for j in range(len(vals) - 1):
-            if inorder(vals[j], vals[j + 1]):
-                vals[j], vals[j + 1] = vals[j+1], vals[j]
-    return vals
+# cmp for integers.
+int_cmp = aoc.Helpers.cmp
 
 
-def inorder(a, b) -> int:
-    print(f"match {a=} {b=}")
-    match [a, b]:
+def cmp(first: Any, second: Any) -> int:
+    """Compare two values and return -1|0|1.
+
+    if first  < second: -1
+    if first == second:  0
+    if first  > second: +0
+    """
+    match [first, second]:
         case [int(), int()]:
-            if a < b:
-                return -1
-            if b < a:
-                return 1 
-            return 0 
+            return int_cmp(first, second)
         case [list(), list()]:
-            for i in range(min(len(a), len(b))):
-                o = inorder(a[i], b[i])
-                if o != 0:
-                    return o 
-            if len(a) < len(b):
-                print("a", f"{len(a)=} {len(b)=} {a=} {b=}")
-                return -1 
-            if len(a) > len(b):
-                return 1 
-            return 0 
+            for i, j in zip(first, second):
+                result = cmp(i, j)
+                if result:
+                    return result
+            return int_cmp(len(first), len(second))
         case [list(), int()]:
-            return inorder(a, [b]) 
+            return cmp(first, [second])
         case [int(), list()]:
-            return inorder([a], b) 
-        case _:
-            raise ValueError(f"Umatched {a} {b}")
-    raise ValueError
+            return cmp([first], second)
+    raise ValueError(f"Umatched {first} {second}")
+
 
 class Day13(aoc.Challenge):
     """Day 13: Distress Signal."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
-        aoc.TestCase(inputs=SAMPLE[0], part=1, want=13),
-        aoc.TestCase(inputs=SAMPLE[0], part=2, want=140),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
+        aoc.TestCase(inputs=SAMPLE, part=1, want=13),
+        aoc.TestCase(inputs=SAMPLE, part=2, want=140),
     ]
-    INPUT_PARSER = aoc.ParseBlocks([aoc.parse_one_str_per_line])
-
-    def inorder(self, a, b) -> int:
-        match [a, b]:
-            case [int(), int()]:
-                if a < b:
-                    return 1
-                if b < a:
-                    return -1
-                return 0
-            case [list(), list()]:
-                for i in range(min(len(a), len(b))):
-                    o = self.inorder(a[i], b[i])
-                    if o != 0:
-                        return o
-                if len(a) < len(b):
-                    return 1
-                if len(a) > len(b):
-                    return -1
-                return 0
-            case [list(), int()]:
-                return self.inorder(a, [b])
-            case [int(), list()]:
-                return self.inorder([a], b)
-            case _:
-                raise ValueError(f"Umatched {a} {b}")
-        return False
+    INPUT_PARSER = aoc.ParseBlocks([aoc.ParseOneWordPerLine(json.loads)])
 
     def part1(self, parsed_input: InputType) -> int:
-        return None
+        """Return the number of pairs which are in order."""
         pairs = parsed_input
-        count = 0
-        for i, (a, b) in enumerate(pairs, start=1):
-            o = self.inorder(eval(a), eval(b))
-            print(o, a, b)
-            if o == 1:
-                count += i
-        return count
+        return sum(
+            idx
+            for idx, (fst, snd) in enumerate(pairs, start=1)
+            if cmp(fst, snd) == -1
+        )
 
     def part2(self, parsed_input: InputType) -> int:
+        """Return the position of two markers when added and sorted."""
         pairs = parsed_input
-        vals = []
-        vals.append([[6]])
-        vals.append([[2]])
-        for a, b in pairs:
-            vals.append(eval(a))
-            vals.append(eval(b))
-        # print(vals)
-        # dosort(vals)
-        # print(vals)
-        vals.sort(key=functools.cmp_to_key(inorder))
-        print(vals)
-        # print(vals.index([[2]]))
-        # print(vals.index([[6]]))
-        s = [[[2]], [], [[6]]]
-        print(s)
-        s.sort(key=functools.cmp_to_key(inorder))
-        print(s)
-
-        # s = list(range(10))
-        # random.shuffle(s)
-        # print(s)
-        # dosort(s)
-        # print(s)
-
-        return (vals.index([[2]]) +1 )* (vals.index([[6]]) +1 )
-
-
-
-
+        dividers = ([[2]], [[6]])
+        vals = list(dividers)
+        for pair in pairs:
+            vals.extend(pair)
+        vals.sort(key=functools.cmp_to_key(cmp))
+        return self.mult(vals.index(divider) + 1 for divider in dividers)
 
 
 if __name__ == "__main__":
