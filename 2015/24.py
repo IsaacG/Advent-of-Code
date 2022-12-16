ae11599b05bddcd5815304cf19928f323843c93f
Isaac Good <github@isaacgood.com>
2022-12-15 16:52:50 -0800

    Tidy 2015/24

--- 2015/24.py
+++ 2015/24.py
@@ -1,12 +1,7 @@
 #!/bin/python
 """Advent of Code, Day 24: It Hangs in the Balance. Balance numbers into groups with contraints."""
-from __future__ import annotations
 
-import collections
-import functools
 import itertools
-import math
-import re
 
 import typer
 from lib import aoc
@@ -20,19 +15,14 @@ InputType = list[LineType]
 class Day24(aoc.Challenge):
     """Day 24: It Hangs in the Balance."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
         aoc.TestCase(inputs=SAMPLE, part=1, want=99),
         aoc.TestCase(inputs=SAMPLE, part=2, want=44),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
     ]
-
     INPUT_PARSER = aoc.parse_one_int_per_line
 
     def balance(self, packages: list[int], groups: int) -> int:
+        """Return the group of packages which meets the constraints."""
         group_size = sum(packages) // groups
         candidates = []
         for package_count in range(1, len(packages)):
@@ -44,9 +34,11 @@ class Day24(aoc.Challenge):
         return min(candidates)
 
     def part1(self, parsed_input: InputType) -> int:
+        """Return the smallest group, with 3 groups."""
         return self.balance(parsed_input, 3)
 
     def part2(self, parsed_input: InputType) -> int:
+        """Return the smallest group, with 4 groups."""
         return self.balance(parsed_input, 4)
 
 
