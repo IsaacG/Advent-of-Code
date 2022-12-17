f35093c6314fe670e0593433fcf3697b56a29301
Isaac Good <github@isaacgood.com>
2022-12-17 12:32:00 -0800

    Tidy 2022/17

--- 2022/17.py
+++ 2022/17.py
@@ -2,6 +2,7 @@
 """Advent of Code, Day 17: Pyroclastic Flow. Compute the height of a Tetris-like rock pile after rocks have landed."""
 
 import collections
+import itertools
 
 import typer
 from lib import aoc
@@ -62,62 +63,72 @@ class Day17(aoc.Challenge):
         landed = set()
         tower_height = 0
         seen = {}
+        height_deltas = []
 
-        cycle_detection = None
-        height_deltas = collections.deque([0] * 40)
+        # Index counter which wraps around.
+        wind_idx_iter = itertools.cycle(range(stream_size))
 
-        wind_idx = -1
-        rock_cnt = -1
+        # My input luckily gives flat tops every so often.
+        # I rely on flat tops to avoid parsing topologies.
+        def top_is_flat() -> bool:
+            """Return if the top is flat."""
+            return all(complex(x, tower_height) in landed for x in range(7))
 
-        while rock_cnt + 1 < target_rock_count:
-            rock_cnt += 1
-            if rock_cnt and rock_cnt % 1000000 == 0:
-                print(f"Step {rock_cnt}")
+        # Drop rocks.
+        for rock_cnt in range(target_rock_count):
             rock_shape = rocks[rock_cnt % 5]
             bottom_left_corner = complex(2, tower_height + 4)
 
+            # Apply wind then gravity until it hits rock bottom.
             while True:
-                wind_idx = (wind_idx + 1) % stream_size
+                # Apply wind when it doesn't blow into rock.
+                wind_idx = next(wind_idx_iter)
                 wind = -1 if stream[wind_idx] == "<" else +1
-                o2 = wind + bottom_left_corner
-                r2 = {r + o2 for r in rock_shape}
-                if all(0 <= r.real < 7 for r in r2) and landed.isdisjoint(r2):
-                    bottom_left_corner = o2
+                new_corner = wind + bottom_left_corner
+                new_rock_positions = {r + new_corner for r in rock_shape}
+                if all(0 <= r.real < 7 for r in new_rock_positions) and landed.isdisjoint(new_rock_positions):
+                    bottom_left_corner = new_corner
 
-                o2 = GRAVITY + bottom_left_corner
-                r2 = {r + o2 for r in rock_shape}
-                if all(0 < r.imag for r in r2) and landed.isdisjoint(r2):
-                    bottom_left_corner = o2
+                # Apply gravity or stop moving.
+                new_corner = GRAVITY + bottom_left_corner
+                new_rock_positions = {r + new_corner for r in rock_shape}
+                if all(0 < r.imag for r in new_rock_positions) and landed.isdisjoint(new_rock_positions):
+                    bottom_left_corner = new_corner
                 else:
                     break
 
+            # Update data.
             landed.update({bottom_left_corner + r for r in rock_shape})
-            tower_height = max(tower_height, int(bottom_left_corner.imag) + self.rock_heights[rock_cnt%5])
+            next_height = max(tower_height, int(bottom_left_corner.imag) + self.rock_heights[rock_cnt%5])
+            height_deltas.append(next_height - tower_height)
+            tower_height = next_height
 
-            if all(complex(x, tower_height) in landed for x in range(7)):
-                t = (((wind_idx) << 4) | (rock_cnt % 5))
-                if t not in seen:
-                    seen[t] = (rock_cnt, tower_height)
-                else:
-                    prior_rock_cnt, prior_height = seen[t]
+            # If the top is flat, check for cycles.
+            if top_is_flat():
+                fingerprint = (wind_idx, rock_cnt % 5)
+                # We have been here before!
+                if fingerprint in seen:
+                    prior_rock_cnt, prior_height = seen[fingerprint]
                     self.debug(f"Cycle found {prior_rock_cnt, prior_height} -> {rock_cnt, tower_height}.")
 
-                    if True:
-                        cycle_size = rock_cnt - prior_rock_cnt
-                        height_diff = tower_height - prior_height
+                    cycle_size = rock_cnt - prior_rock_cnt
+                    height_diff = tower_height - prior_height
 
-                        cycle_count = (target_rock_count - rock_cnt) // cycle_size
-                        rock_cnt += cycle_count * cycle_size
-                        tower_height += cycle_count * height_diff
-                        landed.update(complex(x, tower_height) for x in range(7))
-
+                    # Apply N cycles of changes plus the remainder of rocks needed.
+                    cycle_count, remaining = divmod(target_rock_count - rock_cnt, cycle_size)
+                    tower_height += cycle_count * height_diff
+                    tower_height += sum(height_deltas[prior_rock_cnt:prior_rock_cnt + remaining])
+                    return tower_height
+                seen[fingerprint] = (rock_cnt, tower_height)
 
         return tower_height
 
     def part1(self, parsed_input: InputType) -> int:
+        """Return the tower height after N rocks."""
         return self.solver(parsed_input, 2022)
 
     def part2(self, parsed_input: InputType) -> int:
+        """Return the tower height after N rocks."""
         return self.solver(parsed_input, 1000000000000)
 
 
