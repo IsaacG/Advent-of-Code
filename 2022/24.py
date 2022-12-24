a5a4846fc13b7526a8fca1fc761bf81c328b7e69
Isaac Good <github@isaacgood.com>
2022-12-24 12:24:55 -0800

    Tidy 2022/24 and switch to PriorityQueue for reasonable runtimes

--- 2022/24.py
+++ 2022/24.py
@@ -1,166 +1,137 @@
 #!/bin/python
-"""Advent of Code, Day 24: Blizzard Basin."""
+"""Advent of Code, Day 24: Blizzard Basin. Navigate across a basic, avoiding storms."""
 from __future__ import annotations
 
-import collections
-import functools
-import itertools
-import math
-import sys
-import re
+import dataclasses
+import queue
 
 import typer
 from lib import aoc
 
-SAMPLE = [
-    """\
-#.#####
-#.....#
-#>....#
-#.....#
-#...v.#
-#.....#
-#####.#""",  # 0
-    """\
+SAMPLE = """\
 #.######
 #>>.<^<#
 #.<..<<#
 #>v.><>#
 #<^v^^>#
-######.#""",  # 15
-]
-
-sys.setrecursionlimit(2500)
+######.#"""
+MAX_STEPS = 1000
 
 
-LineType = int
-InputType = list[LineType]
+@dataclasses.dataclass
+class Basin:
+    """Map a basic and the storm patterns."""
+
+    walls: set[complex]
+    blizzards: dict[complex, set[complex]]
+    width: int
+    height: int
+
+    def __post_init__(self) -> None:
+        """Compute storm patterns and entrance/exit."""
+        self.entrance = complex(1, 0)
+        self.exit_pos = complex(self.width - 2, self.height - 1)
+        self.blocked = self.compute_blocked()
+
+    def compute_blocked(self) -> list[set[complex]]:
+        """Compute MAX_STEPS iterations of the blizzard."""
+        blizzards = self.blizzards
+        blocked = []
+
+        for _ in range(MAX_STEPS):
+            next_blizzards = {}
+            for direction, storms in blizzards.items():
+                new_storms = set()
+                for storm in storms:
+                    storm += direction
+                    if storm.real == self.width - 1:
+                        storm = complex(1, storm.imag)
+                    elif storm.real == 0:
+                        storm = complex(self.width - 2, storm.imag)
+                    elif storm.imag == 0:
+                        storm = complex(storm.real, self.height - 2)
+                    elif storm.imag == self.height - 1:
+                        storm = complex(storm.real, 1)
+                    new_storms.add(storm)
+                next_blizzards[direction] = new_storms
+            blizzards = next_blizzards
+            blocked.append(set.union(self.walls, *blizzards.values()))
+        return blocked
+
+    @classmethod
+    def input_parser(cls, puzzle_input: str) -> Basin:
+        """Build a Basin from the input."""
+        positions: dict[str, set[complex]] = {char: set() for char in "#><v^"}
+        width = len(puzzle_input.splitlines()[0])
+        height = len(puzzle_input.splitlines())
+        # Map coordinates to chars.
+        for y_pos, line in enumerate(puzzle_input.splitlines()):
+            for x_pos, char in enumerate(line):
+                if char == ".":
+                    continue
+                positions[char].add(complex(x_pos, y_pos))
+        char_to_dir = {">": 1, "<": -1, "v": 1j, "^": -1j}
+        blizzards = {direction: positions[char] for char, direction in char_to_dir.items()}
+        return cls(
+            walls=positions["#"],
+            blizzards=blizzards,
+            width=width,
+            height=height,
+        )
 
 
 class Day24(aoc.Challenge):
     """Day 24: Blizzard Basin."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
-
     TESTS = [
-        aoc.TestCase(inputs=SAMPLE[1], part=1, want=18),
-        aoc.TestCase(inputs=SAMPLE[0], part=2, want=54),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
+        aoc.TestCase(inputs=SAMPLE, part=1, want=18),
+        aoc.TestCase(inputs=SAMPLE, part=2, want=54),
     ]
+    DEBUG = False
+    INPUT_PARSER = aoc.ParseOneWord(Basin.input_parser)
+    PARAMETERIZED_INPUTS = [1, 3]
 
-    @functools.cache
-    def nogo(self, moves: int) -> set[complex]:
-        return set.union(*self.get_bliz(moves).values()) | self.walls
+    def navigate(self, basin: Basin, reverse: bool, init_steps: int) -> int:
+        """Return move count after navigating from one side of the basin to the other."""
+        blocked = basin.blocked
+        start, end = basin.entrance, basin.exit_pos
+        if reverse:
+            start, end = end, start
 
-    @functools.cache
-    def get_bliz(self, moves: int) -> set[complex]:
-        if moves == 0:
-            return self.blizzards
-        new_blizz = {}
-        for direction, storms in self.get_bliz(moves - 1).items():
-            new_storms = set()
-            for storm in storms:
-                storm += direction
-                if storm.real == self.width - 1:
-                    storm = complex(1, storm.imag)
-                elif storm.real == 0:
-                    storm = complex(self.width - 2, storm.imag)
-                elif storm.imag == 0:
-                    storm = complex(storm.real, self.height - 2)
-                elif storm.imag == self.height - 1:
-                    storm = complex(storm.real, 1)
-                new_storms.add(storm)
-            new_blizz[direction] = new_storms
-        return new_blizz
+        to_explore: queue.Queue[tuple[float, int, float, float]] = queue.PriorityQueue()
+        to_explore.put((0, init_steps, start.real, start.imag))
+        cur = start
+        seen = set()
+        with self.context_timer("Compute path"):
+            # A-star exploration.
+            # Heuristic: moves taken + Manhattan distance to the end.
+            while cur != end:
+                _, moves, cur_x, cur_y = to_explore.get()
+                # complex values cannot be compared so store them by component.
+                cur = complex(cur_x, cur_y)
+                for direction in aoc.FOUR_DIRECTIONS + [0]:
+                    next_pos = cur + direction
+                    # Do not allow moving outside the basin.
+                    if next_pos.imag < 0 or next_pos.imag >= basin.height:
+                        continue
+                    if next_pos in blocked[moves]:
+                        continue
+                    if (moves + 1, next_pos) in seen:
+                        continue
+                    seen.add((moves + 1, next_pos))
+                    distance_to_end = abs(next_pos.real - end.real) + abs(next_pos.imag - end.imag)
+                    to_explore.put(
+                        (moves + distance_to_end, moves + 1, next_pos.real, next_pos.imag)
+                    )
+        return moves
 
-    def part1(self, parsed_input: InputType) -> int:
-        self.walls, self.blizzards, self.width, self.height = parsed_input
-        start = complex(1, 0)
-        if self.testing:
-            options = {(0, start),}
-        else:
-            options = {(570, start),}
-        """
-        for i in range(4):
-            print(f"Step {i}:")
-            print(aoc.render(self.nogo(i)))
-            print()
-        return
-        # assert (1+3j) in self.nogo(3)
-        """
-        walls = self.walls
-        while options:
-            moves_cur = min(options, key=lambda x: (x[0], -x[1].imag))
-            moves, cur = moves_cur
-            options.remove(moves_cur)
-            # print(f"{moves=}, {cur=}")
-            cur_nogo = self.nogo(moves)
-            assert cur not in cur_nogo
-            nogo = self.nogo(moves + 1)
-            for direction in aoc.FOUR_DIRECTIONS + [0]:
-                opt = cur + direction
-                if opt.imag < 0:
-                    continue
-                if opt in nogo:
-                    continue
-                if opt.imag == self.height - 1:
-                    return moves + 1
-                options.add((moves + 1, opt))
-
-    def part2(self, parsed_input: InputType) -> int:
-        self.walls, self.blizzards, self.width, self.height = parsed_input
-        start = complex(1, 0)
-        walls = self.walls
-        end = complex(self.width - 2, self.height - 1)
-        if self.testing:
-            options = {(18, end),}
-        else:
-            options = {(301, end),}
-        # end to start
-        while options:
-            moves_cur = min(options, key=lambda x: (x[0], x[1].imag))
-            moves, cur = moves_cur
-            options.remove(moves_cur)
-            # print(f"{moves=}, {cur=}")
-            cur_nogo = self.nogo(moves)
-            assert cur not in cur_nogo
-            nogo = self.nogo(moves + 1)
-            for direction in aoc.FOUR_DIRECTIONS + [0]:
-                opt = cur + direction
-                if opt.imag > self.height - 1:
-                    continue
-                if opt in nogo:
-                    continue
-                if opt.imag == 0:
-                    return moves + 1
-                options.add((moves + 1, opt))
-
-    def parse_chars(self, puzzle_input: str, on_char: str) -> set[complex]:
-        """Parse ASCII to find points in a map which are "on"."""
-        return {
-            complex(x, y)
-            for y, line in enumerate(puzzle_input.splitlines())
-            for x, char in enumerate(line)
-            if char == on_char
-        }
-
-    def input_parser(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
-        walls = self.parse_chars(puzzle_input, "#")
-        blizzards = {
-            +1:  self.parse_chars(puzzle_input, ">"),
-            -1:  self.parse_chars(puzzle_input, "<"),
-            1j:  self.parse_chars(puzzle_input, "v"),
-            -1j: self.parse_chars(puzzle_input, "^"),
-        }
-        width, height = len(puzzle_input.splitlines()[0]), len(puzzle_input.splitlines())
-        return (walls, blizzards, width, height)
-
-    # def line_parser(self, line: str):
-    #     pass
+    def solver(self, parsed_input: Basin, trip_count: int) -> int:
+        """Solve for n trips across the basin."""
+        moves = 0
+        for i in range(trip_count):
+            moves = self.navigate(parsed_input, bool(i % 2), moves)
+            self.debug(f"{i}: {moves=}")
+        return moves
 
 
 if __name__ == "__main__":
