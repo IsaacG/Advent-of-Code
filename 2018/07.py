6980e7a626ac9deff6e9de92d0c6ada7601ca7f3
Isaac Good <github@isaacgood.com>
2023-02-26 10:58:52 -0800

    Tidy 2018/07

--- 2018/07.py
+++ 2018/07.py
@@ -1,11 +1,7 @@
 #!/bin/python
-"""Advent of Code, Day 7: The Sum of Its Parts."""
-from __future__ import annotations
+"""Advent of Code, Day 7: The Sum of Its Parts. Unravel a dependency graph and time it."""
 
 import collections
-import functools
-import itertools
-import math
 import re
 
 import typer
@@ -20,17 +16,11 @@ Step B must be finished before step E can begin.
 Step D must be finished before step E can begin.
 Step F must be finished before step E can begin."""
 
-LineType = int
-InputType = list[LineType]
+InputType = tuple[set[str], dict[str, set[str]]]
 
 
 class Day07(aoc.Challenge):
     """Day 7: The Sum of Its Parts."""
-
-    # DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: True, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
 
     TESTS = [
         aoc.TestCase(inputs=SAMPLE, part=1, want="CABDFE"),
@@ -38,6 +28,7 @@ class Day07(aoc.Challenge):
     ]
 
     def part1(self, parsed_input: InputType) -> str:
+        """Return the order to work on jobs per a dependency graph."""
         nodes, blocked = parsed_input
         order = []
 
@@ -46,52 +37,52 @@ class Day07(aoc.Challenge):
             available.sort()
             node = available.pop(0)
             order.append(node)
-            self.debug(f"Next node: {node}")
 
             for n, dep in list(blocked.items()):
                 if node in dep:
                     dep.remove(node)
-                if not dep:
-                    available.append(n)
-                    del blocked[n]
+                    if not dep:
+                        available.append(n)
+                        del blocked[n]
         return "".join(order)
 
     def part2(self, parsed_input: InputType) -> int:
+        """Return the time to complete tasks given dependencies and N workers."""
         nodes, blocked = parsed_input
         if self.testing:
             workers = 2
             base = ord("A") - 1
         else:
             workers = 5
-            base = ord("A") - 1 - 60
+            base = ord("A") - 1 - 60  # Add 60 seconds to each job.
 
         available = [n for n in nodes if n not in blocked]
-        working = []
+        working: list[tuple[int, str]] = []
         clock = 0
 
         while available or working:
+            # Mark jobs done once time has arrived.
             while working and working[0][0] == clock:
-                done_click, node = working.pop(0)
+                node = working.pop(0)[1]
                 for n, dep in list(blocked.items()):
                     if node in dep:
                         dep.remove(node)
-                    if not dep:
-                        available.append(n)
-                        del blocked[n]
+                        if not dep:
+                            available.append(n)
+                            del blocked[n]
 
+            # Assign work to workers if work is available.
             available.sort()
             while len(working) < workers and available:
                 node = available.pop(0)
                 duration = ord(node) - base
                 working.append((clock + duration, node))
-                self.debug(f"Next node: {node}")
 
-            working.sort()
+            # Fast forward to the next event/job completion.
             if working:
+                working.sort()
                 clock = working[0][0]
 
-        if not self.testing:
-            assert clock > 177, clock
         return clock
 
     def input_parser(self, puzzle_input: str) -> InputType:
@@ -100,7 +91,9 @@ class Day07(aoc.Challenge):
         dependencies = collections.defaultdict(set)
         nodes = set()
         for line in puzzle_input.splitlines():
-            a, b = patt.match(line).groups()
+            m = patt.match(line)
+            assert m is not None
+            a, b = m.groups()
             dependencies[b].add(a)
             nodes.add(a)
             nodes.add(b)
