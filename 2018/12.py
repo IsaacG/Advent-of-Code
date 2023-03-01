16d295c88a2ead85c61a7ce77190c4f0a0e30c13
Isaac Good <github@isaacgood.com>
2023-02-28 17:40:42 -0800

    Tidy 2018/12

--- 2018/12.py
+++ 2018/12.py
@@ -1,11 +1,6 @@
 #!/bin/python
-"""Advent of Code, Day 12: Subterranean Sustainability."""
-from __future__ import annotations
+"""Advent of Code, Day 12: Subterranean Sustainability. Compute what plants will be alive in future generations."""
 
-import collections
-import functools
-import itertools
-import math
 import re
 
 import typer
@@ -31,51 +26,55 @@ initial state: #..#.#..##......###...###
 ####. => #""",  # 23
 ]
 
-LineType = int
-InputType = list[LineType]
+InputType = tuple[list[list[bool]], list[list[bool]]]
 
 
 class Day12(aoc.Challenge):
     """Day 12: Subterranean Sustainability."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
         aoc.TestCase(inputs=SAMPLE[0], part=1, want=325),
         aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
     ]
-    INPUT_PARSER = aoc.ParseBlocks([aoc.BaseParseReFindall(r"[.#]", lambda line: [i == "#" for i in line])] * 2)
+    INPUT_PARSER = aoc.ParseBlocks(
+        [aoc.BaseParseReFindall(r"[.#]", lambda line: [i == "#" for i in line])] * 2
+    )
+    # How many generations to simulate.
     PARAMETERIZED_INPUTS = [20, 50000000000]
 
     def solver(self, parsed_input: InputType, *args, **kwargs) -> int:
+        """Simulate N generations of plant growth."""
         target_gen = args[0]
         state = {i for i, j in enumerate(parsed_input[0][0]) if j}
+        # Rules for new generations.
         rules = {tuple(rule[:5]) for rule in parsed_input[1] if rule[5]}
+        # Cycle detection.
         seen = {}
+
         for gen in range(target_gen):
             new_state = set()
             for pot in range(min(state) - 2, max(state) + 3):
                     window = tuple(i in state for i in range(pot - 2, pot + 3))
                     if window in rules:
                         new_state.add(pot)
+
             state = new_state
+            # For cycle detection, shift pots so the pattern is relative to "0".
             offset = min(state)
             shift_state = frozenset(pot - offset for pot in state)
+            # Cycle detected.
             if shift_state in seen:
                 prior_gen, prior_offset = seen[shift_state]
                 gen_delta, offset_delta = (gen - prior_gen, offset - prior_offset)
+                assert gen_delta == 1  # Less math if the cycle is 1 gen each.
                 steps = target_gen - gen - 1
                 total_shift = steps * offset_delta
-                assert gen_delta == 1
                 state = {pot + total_shift for pot in state}
                 break
-            seen[shift_state] = gen, offset
-            if gen % 10000 == 0:
-                print(f"{gen=}, {len(state)=}")
-        assert sum(state) > 0
-        assert sum(state) < 1150000000381
+            else:
+                # Record generation.
+                seen[shift_state] = gen, offset
+
         return sum(state)
 
 
