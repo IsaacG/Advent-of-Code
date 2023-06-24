46bca54549e18f4d5e96a8b94d86ca1f63c50cc7
Isaac Good <github@isaacgood.com>
2023-06-23 21:33:36 -0700

    Tidy 2016/02

--- 2016/02.py
+++ 2016/02.py
@@ -1,12 +1,5 @@
 #!/bin/python
-"""Advent of Code, Day 2: Bathroom Security."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
+"""Advent of Code, Day 2: Bathroom Security. Follow directions to compute keypad code."""
 
 from lib import aoc
 
@@ -23,56 +16,32 @@ InputType = list[LineType]
 class Day02(aoc.Challenge):
     """Day 2: Bathroom Security."""
 
-    DEBUG = True
-    # PARAMETERIZED_INPUTS = [5, 50]
-
     TESTS = [
-        aoc.TestCase(inputs=SAMPLE, part=1, want=1985),
+        aoc.TestCase(inputs=SAMPLE, part=1, want="1985"),
         aoc.TestCase(inputs=SAMPLE, part=2, want="5DB3"),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
     ]
-
     INPUT_PARSER = aoc.parse_one_str_per_line
+    PARAMETERIZED_INPUTS = ["123\n456\n789", "1\n234\n56789\nABC\nD"]
 
-    def part1(self, parsed_input: InputType) -> int:
-        x, y = 1, 1
-        out = 0
-        for line in parsed_input:
-            for val in line:
-                match val:
-                    case "U":
-                        y = max(0, y - 1)
-                    case "D":
-                        y = min(2, y + 1)
-                    case "L":
-                        x = max(0, x - 1)
-                    case "R":
-                        x = min(2, x + 1)
-            out = out * 10 + (x + y * 3 + 1)
-        return out
+    def solver(self, parsed_input: InputType, pad_layout: str) -> str:
+        """Walk a keypad following directions to compute a code."""
+        pad_rows = pad_layout.splitlines()
+        size = max(len(row) for row in pad_rows)
+        pad = {
+            complex(x, y): key
+            for y, row in enumerate(pad_rows)
+            for x, key in enumerate(row.center(size))
+            if key != " "
+        }
+        cur = next(pos for pos, key in pad.items() if key == "5")
+        directions = aoc.LETTER_DIRECTIONS
 
-
-    def part2(self, parsed_input: InputType) -> int:
-        size = 5
-        pad = [i.center(size) for i in ["1", "234", "56789", "ABC", "D"]]
-        x, y = 0, 2
         out = []
         for line in parsed_input:
             for val in line:
-                match val:
-                    case "U":
-                        if y > 0 and pad[y - 1][x] != " ":
-                            y -= 1
-                    case "D":
-                        if y < size - 1 and pad[y + 1][x] != " ":
-                            y += 1
-                    case "L":
-                        if x > 0 and pad[y][x - 1] != " ":
-                            x -= 1
-                    case "R":
-                        if x < size - 1 and pad[y][x + 1] != " ":
-                            x += 1
-            out.append(pad[y][x])
+                if cur + directions[val] in pad:
+                    cur += directions[val]
+            out.append(pad[cur])
         return "".join(out)
 
 # vim:expandtab:sw=4:ts=4
