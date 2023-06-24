ef8c79046e86c9b0bb052431919a60588e2c6db5
Isaac Good <github@isaacgood.com>
2023-06-23 22:19:55 -0700

    Tidy 2016/04

--- 2016/04.py
+++ 2016/04.py
@@ -11,57 +11,48 @@ import string
 
 from lib import aoc
 
-SAMPLE = [
-    """\
+SAMPLE = """\
 aaaaa-bbb-z-y-x-123[abxyz]
 a-b-c-d-e-f-g-h-987[abcde]
 not-a-real-room-404[oarel]
 totally-real-room-200[decoy]"""
-]
 
-LineType = int
-InputType = list[LineType]
+InputType = list[tuple[str, int, str]]
 
 
 class Day04(aoc.Challenge):
     """Day 4: Security Through Obscurity."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
-
     TESTS = [
-        aoc.TestCase(inputs=SAMPLE[0], part=1, want=1514),
-        aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
+        aoc.TestCase(inputs=SAMPLE, part=1, want=1514),
+        aoc.TestCase(inputs=SAMPLE, part=2, want=aoc.TEST_SKIP),
     ]
-
     INPUT_PARSER = aoc.parse_re_group_mixed(r"([\w-]+)-(\d+)\[(\w+)\]")
 
     def is_real(self, name: str, checksum: str) -> bool:
+        """Return if a name matches its checksum."""
         name = name.replace("-", "")
         top = sorted((-v, k) for k, v in collections.Counter(name).items())
         return "".join(b for a, b in top[:5]) == checksum
 
     def part1(self, parsed_input: InputType) -> int:
-        total = 0
-        for name, sector, checksum in parsed_input:
-            name = name.replace("-", "")
-            top = sorted((-v, k) for k, v in collections.Counter(name).items())
-            check = "".join(b for a, b in top[:5])
-            if check == checksum:
-                total += sector
-        return total
+        """Return the sum of real sectors."""
+        return sum(
+            sector 
+            for name, sector, checksum in parsed_input
+            if self.is_real(name, checksum)
+        )
 
     def part2(self, parsed_input: InputType) -> int:
+        """Return the sector with Northpole storage."""
         for name, sector, checksum in parsed_input:
             if not self.is_real(name, checksum):
                 continue
             name = "".join(
-                i if not i.isalpha() else string.ascii_lowercase[
+                string.ascii_lowercase[
                     (string.ascii_lowercase.index(i) + sector) % 26
-                ]
+                ] if i.isalpha() else " "
                 for i in name
             )
-            if "northpole" in name.split("-"):
+            if "northpole" in name.split():
                 return sector
