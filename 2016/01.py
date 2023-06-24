96f1b29a8c62bef9d2910ca2e1bc7d0633469cc1
Isaac Good <github@isaacgood.com>
2023-06-23 20:55:43 -0700

    Solve 2016/01

---
+++ 2016/01.py
@@ -0,0 +1,44 @@
+#!/bin/python
+"""Advent of Code, Day 1: No Time for a Taxicab. Compute how far we travelled."""
+
+from lib import aoc
+
+SAMPLE = [
+    'R2, L3',
+    'R2, R2, R2',
+    'R5, L5, R5, R3',
+    'R8, R4, R4, R8',
+]
+
+LineType = list[str]
+InputType = list[LineType]
+
+
+class Day01(aoc.Challenge):
+    """Day 1: No Time for a Taxicab."""
+
+    TESTS = [
+        aoc.TestCase(inputs=SAMPLE[0], part=1, want=5),
+        aoc.TestCase(inputs=SAMPLE[1], part=1, want=2),
+        aoc.TestCase(inputs=SAMPLE[2], part=1, want=12),
+        aoc.TestCase(inputs=SAMPLE[3], part=2, want=4),
+    ]
+    INPUT_PARSER = aoc.parse_re_findall_str(r"[LR]\d+")
+    PARAMETERIZED_INPUTS = [False, True]
+
+    def solver(self, parsed_input: InputType, p2: bool) -> int:
+        """Compute how far we walked."""
+        cur = complex(0, 0)
+        seen = {cur}
+        heading = complex(0, 1)
+
+        for instruction in parsed_input[0]:
+            rot, dist = instruction[0], int(instruction[1:])
+            heading *= {"R": -1j, "L": +1j}[rot]
+            for _ in range(dist):
+                cur += heading
+                if p2 and cur in seen:
+                    return int(abs(cur.real) + abs(cur.imag))
+                seen.add(cur)
+
+        return int(abs(cur.real) + abs(cur.imag))
