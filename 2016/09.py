a8c4170714703c078a8ad186a2e92abc54158d48
Isaac Good <github@isaacgood.com>
2023-06-28 19:52:28 -0700

    Tidy 2016/09

--- 2016/09.py
+++ 2016/09.py
@@ -1,12 +1,5 @@
 #!/bin/python
-"""Advent of Code, Day 9: Explosives in Cyberspace."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
+"""Advent of Code, Day 9: Explosives in Cyberspace. Compute the length of an encoded sequence."""
 
 from lib import aoc
 
@@ -23,65 +16,33 @@ SAMPLE = [
     ('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', 445),
 ]
 
-LineType = int
-InputType = list[LineType]
-
 
 class Day09(aoc.Challenge):
     """Day 9: Explosives in Cyberspace."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
 
     TESTS = [
         aoc.TestCase(inputs=i[0], part=1, want=len(i[1])) for i in SAMPLE[:6]
     ] + [
         aoc.TestCase(inputs=i[0], part=2, want=i[1]) for i in SAMPLE[6:]
     ]
-
     INPUT_PARSER = aoc.parse_one_str
 
-    def part1(self, parsed_input: InputType) -> int:
-        out = []
+    def solver(self, parsed_input: str, expand: bool) -> int:
+        """Compute the length of an encoded sequence."""
+        length = 0
         data = list(reversed(parsed_input))
 
         while data:
             char = data.pop()
             if char != "(":
-                out.append(char)
-            else:
-                buf = []
-                while (char := data.pop()) != ")":
-                    buf.append(char)
-                count, repeat = "".join(buf).split("x")
-                buf = "".join(data.pop() for _ in range(int(count)))
-                out.append(buf * int(repeat))
-
-        return len("".join(out))
-
-    def seq_len(self, sequence: str) -> int:
-        count = 0
-        data = list(reversed(sequence))
-
-        while data:
-            char = data.pop()
-            if char != "(":
-                count += 1
+                length += 1
             else:
                 buf = []
                 while (char := data.pop()) != ")":
                     buf.append(char)
                 sub_count, repeat = "".join(buf).split("x")
                 sub_sequence = "".join(data.pop() for _ in range(int(sub_count)))
-                count += int(repeat) * self.seq_len(sub_sequence)
+                sub_length = self.solver(sub_sequence, expand) if expand else len(sub_sequence)
+                length += int(repeat) * sub_length
 
-        return count
-
-    def part2(self, parsed_input: InputType) -> int:
-        return self.seq_len(parsed_input)
-
-
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
+        return length
