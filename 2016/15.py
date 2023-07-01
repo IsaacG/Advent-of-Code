d9898e7075e2f7863de4b53a892f9ecda3d047df
Isaac Good <github@isaacgood.com>
2023-06-30 18:58:50 -0700

    Tidy 2016/15

--- 2016/15.py
+++ 2016/15.py
@@ -1,12 +1,5 @@
 #!/bin/python
-"""Advent of Code, Day 15: Timing is Everything."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
+"""Advent of Code, Day 15: Timing is Everything. Determine when a hole in disks line up."""
 
 from lib import aoc
 
@@ -14,17 +7,11 @@ SAMPLE = """\
 Disc #1 has 5 positions; at time=0, it is at position 4.
 Disc #2 has 2 positions; at time=0, it is at position 1."""
 
-LineType = int
-InputType = list[LineType]
+InputType = list[list[int]]
 
 
 class Day15(aoc.Challenge):
     """Day 15: Timing is Everything."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
 
     TESTS = [
         aoc.TestCase(inputs=SAMPLE, part=1, want=5),
@@ -32,21 +19,18 @@ class Day15(aoc.Challenge):
     ]
 
     def part1(self, parsed_input: InputType) -> int:
-        print(parsed_input)
+        """Return the correct time to press the button to get a capsule."""
         disks = [
             (number + start, positions)
             for number, positions, _, start in parsed_input
         ]
-        print(disks)
-        for time in itertools.count():
-            if all(
-                ((start + time) % positions) == 0
-                for start, positions in disks
-            ):
-                return time
+        return next(
+            time
+            for time in itertools.count()
+            if all(((start + time) % positions) == 0 for start, positions in disks)
+        )
 
     def part2(self, parsed_input: InputType) -> int:
-        last = len(parsed_input)
-        got = self.part1(parsed_input + [[last + 1, 11, 0, 0]])
-        assert got < 3730820
-        return got
+        """Solve with an additional disk."""
+        new_disk = [parsed_input[-1][0] + 1, 11, 0, 0]
+        return self.part1(parsed_input + [new_disk])
