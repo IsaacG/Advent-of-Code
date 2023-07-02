a710c19597b36e1c2b10c455850a70794055cf31
Isaac Good <github@isaacgood.com>
2023-07-02 09:38:16 -0700

    Tidy 2016/17

--- 2016/17.py
+++ 2016/17.py
@@ -1,14 +1,7 @@
 #!/bin/python
-"""Advent of Code, Day 17: Two Steps Forward."""
-from __future__ import annotations
-
+"""Advent of Code, Day 17: Two Steps Forward. Compute the route through a maze or doors which lock and unlock."""
 import collections
-import functools
-import itertools
 import hashlib
-import math
-import re
-
 from lib import aoc
 
 SAMPLE = [
@@ -17,17 +10,9 @@ SAMPLE = [
     ('ulqzkmiv', 'DRURDRUDDLLDLUURRDULRLDUUDDDRR', 830),
 ]
 
-LineType = int
-InputType = list[LineType]
-
 
 class Day17(aoc.Challenge):
     """Day 17: Two Steps Forward."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
 
     TESTS = [
         aoc.TestCase(inputs=s[0], part=1, want=s[1]) for s in SAMPLE
@@ -35,38 +20,41 @@ class Day17(aoc.Challenge):
         aoc.TestCase(inputs=s[0], part=2, want=s[2]) for s in SAMPLE
     ]
 
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        seen = {""}
+    def solver(self, parsed_input: str, find_longest: bool) -> int | str:
+        """Compute the route through a maze or doors which lock and unlock."""
         todo = collections.deque()
         todo.append(("", 0, 0))
 
         dirs = {"L": (-1, 0), "R": (1, 0), "D": (0, 1), "U": (0, -1)}
         doors = "UDLR"
-        open = "bcdef"
+        unlocked = "bcdef"
         longest = 0
 
         def options(steps) -> list[str]:
+            """Return which doors are unlocked for a given set of steps taken."""
             keys = hashlib.md5((parsed_input + steps).encode()).hexdigest()[:4]
-            return [door for door, key in zip(doors, keys) if key in open]
+            return [door for door, key in zip(doors, keys) if key in unlocked]
 
         while todo:
             steps, cur_x, cur_y = todo.popleft()
+
+            # Got to the end.
             if cur_x == 3 and cur_y == 3:
                 step_count = len(steps)
                 if step_count > longest:
                     longest = step_count
-                if param:
+                if find_longest:
+                    # Do not explore this path further.
                     continue
                 else:
+                    # Return shortest path.
                     return steps
+
             for option in options(steps):
                 d_x, d_y = dirs[option]
                 next_x, next_y = cur_x + d_x, cur_y + d_y
+                # Check if the move is within the maze.
                 if 0 <= next_x < 4 and 0 <= next_y < 4:
-                    next_steps = steps + option
-                    if next_steps in seen:
-                        continue
-                    seen.add(next_steps)
-                    todo.append((next_steps, next_x, next_y))
+                    todo.append((steps + option, next_x, next_y))
 
         return longest
