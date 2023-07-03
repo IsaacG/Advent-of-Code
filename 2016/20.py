a4b3a340a72baaedc390f7b2d3c5c54fc2ea8fb9
Isaac Good <github@isaacgood.com>
2023-07-03 07:45:08 -0700

    Tidy 2016/20

--- 2016/20.py
+++ 2016/20.py
@@ -1,63 +1,43 @@
 #!/bin/python
-"""Advent of Code, Day 20: Firewall Rules."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
+"""Advent of Code, Day 20: Firewall Rules. Find valid IPs between blacklisted ranges."""
 
 from lib import aoc
 
-SAMPLE = """\
-5-8
-0-2
-4-7"""
-
-LineType = int
+SAMPLE = "5-8\n0-2\n4-7"
+LineType = tuple[int, int]
 InputType = list[LineType]
 
 
 class Day20(aoc.Challenge):
     """Day 20: Firewall Rules."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
-
     TESTS = [
         aoc.TestCase(inputs=SAMPLE, part=1, want=3),
         aoc.TestCase(inputs=SAMPLE, part=2, want=2),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
     ]
 
-    def part1(self, parsed_input: InputType) -> int:
-        cur = 0
-        for low, high in parsed_input:
-            if low <= cur and cur <= high:
-                cur = high + 1
-            elif cur < low:
-                return cur
-
-    def part2(self, parsed_input: InputType) -> int:
+    def solver(self, parsed_input: InputType, param: bool) -> int:
+        """Find valid IPs between blacklisted ranges."""
+        # Set the upper limit.
         if self.testing:
             parsed_input.append((10, 10))
         else:
             parsed_input.append((4294967296, 4294967296))
+
         cur = 0
         allowed = 0
         for low, high in parsed_input:
             if low <= cur and cur <= high:
                 cur = high + 1
             elif cur < low:
+                # Part 1: return the first allowed IP.
+                if not param:
+                    return cur
+                # Part 2: count valid IPS.
                 allowed += low - cur
                 cur = high + 1
         return allowed
 
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
 
     def input_parser(self, puzzle_input: str) -> InputType:
         """Parse the input data."""
