3f4cee67f6850edbac97e6f79785bb28ea9aebe8
Isaac Good <github@isaacgood.com>
2023-11-23 19:32:43 -0800

    Tidy and benchmark 2018/14

--- 2018/14.py
+++ 2018/14.py
@@ -1,29 +1,14 @@
 #!/bin/python
 """Advent of Code, Day 14: Chocolate Charts."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
 
 from lib import aoc
 
-SAMPLE = ["9", "5", "18", "2018"]
-
-LineType = int
-InputType = list[LineType]
-
 
 class Day14(aoc.Challenge):
-    """Day 14: Chocolate Charts."""
+    """Day 14: Chocolate Charts. Mix recipes until a pattern is found."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
-
+    PARAMETERIZED_INPUTS = [False, True]
+    INPUT_PARSER = aoc.parse_one_str
     TESTS = [
         aoc.TestCase(inputs="9", part=1, want="5158916779"),
         aoc.TestCase(inputs="5", part=1, want="0124515891"),
@@ -35,48 +20,38 @@ class Day14(aoc.Challenge):
         aoc.TestCase(inputs="59414", part=2, want=2018),
     ]
 
-    INPUT_PARSER = aoc.parse_one_str
+    def solver(self, parsed_input: str, param: bool) -> int:
+        """Solve for the target recipes."""
+        # Compute target data.
+        p1_target = int(parsed_input)
+        p1_stop = p1_target + 10
+        # Note: switching to dequeue made things slower.
+        p2_want = [int(i) for i in parsed_input]
+        p2_want_len = len(parsed_input)
 
-    def part1(self, parsed_input: str) -> int:
-        stop = int(parsed_input) + 10
-        recipes = [3, 7]
-        elf1, elf2 = 0, 1
-        count = 2
-        while count < stop:
-            # print(recipes)
-            v1, v2 = recipes[elf1], recipes[elf2]
-            new = v1 + v2
-            if new >= 10:
-                recipes.append(1)
-                count += 1
-            recipes.append(new % 10)
-            count += 1
-
-            elf1, elf2 = (elf1 + 1 + v1) % count, (elf2 + 1 + v2) % count
-        return "".join(str(i) for i in recipes[int(parsed_input):int(parsed_input) + 10])
-
-    def part2(self, parsed_input: InputType) -> int:
-        # stop = parsed_input + 10
-        want = [int(i) for i in parsed_input]
-        want_len = len(parsed_input)
-
+        # Initialize loop variables.
         recipes = [3, 7]
         elf1, elf2 = 0, 1
         count = 2
 
         while count < 100000000:
-            # print(recipes)
-            v1, v2 = recipes[elf1], recipes[elf2]
-            new = v1 + v2
-            if new >= 10:
-                recipes.append(1)
+            # Compute the new values to add.
+            recipe1, recipe2 = recipes[elf1], recipes[elf2]
+            new_recipe = recipe1 + recipe2
+            if new_recipe >= 10:
+                to_add = [1, new_recipe % 10]
+            else:
+                to_add = [new_recipe]
+
+            # Add new recipes and check for end conditions.
+            for recipe in to_add:
+                recipes.append(recipe)
                 count += 1
-                if recipes[-want_len:] == want:
-                    return count - want_len
-            recipes.append(new % 10)
-            count += 1
-            if recipes[-want_len:] == want:
-                return count - want_len
+                if param and recipes[-p2_want_len:] == p2_want:
+                    return count - p2_want_len
 
-            elf1, elf2 = (elf1 + 1 + v1) % count, (elf2 + 1 + v2) % count
+            if not param and count >= p1_stop:
+                return "".join(str(i) for i in recipes[p1_target:p1_stop])
 
+            # Update the elves' recipes.
+            elf1, elf2 = (elf1 + 1 + recipe1) % count, (elf2 + 1 + recipe2) % count
