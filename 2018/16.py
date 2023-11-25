4d4bbae4ca94adfbd973efbc7e6c50cb61bbbdce
Isaac Good <github@isaacgood.com>
2023-11-25 09:39:22 -0800

    Tidy 2018/16

--- 2018/16.py
+++ 2018/16.py
@@ -23,26 +23,23 @@ After:  [3, 2, 2, 1]
 
 1 2 3 4"""
 
-LineType = int
-InputType = list[LineType]
+InputType = list[list[list[list[int]]], list[list[int]]]
 
 
 class Day16(aoc.Challenge):
-    """Day 16: Chronal Classification."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
+    """Day 16: Chronal Classification. Map op instructions and simulate a CPU."""
 
     TESTS = [
         aoc.TestCase(inputs=SAMPLE, part=1, want=2),
         aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
     ]
 
+    # Split the input into the captures and program.
+    # Split captures into single captures which are a set of 3*4 digits.
     INPUT_PARSER = aoc.ParseBlocks([aoc.ParseBlocks([aoc.parse_ints]), aoc.parse_ints], "\n" * 4)
 
     def compute(self, instruction: int, op_a: int, op_b: int, memory: list[int]) -> int:
+        """Return the result of a single instruction."""
         match instruction:
             case 0:
                 # addr (add register) stores into register C the result of adding register A and register B.
@@ -96,78 +93,44 @@ class Day16(aoc.Challenge):
                 raise ValueError("Unknown instruction")
 
     def part1(self, parsed_input: InputType) -> int:
-        captures, program = parsed_input
-
-        for prior, (instruction, op_a, op_b, target), post in captures:
-            a = prior.copy()
-            b = post.copy()
-            del a[target]
-            del b[target]
-            assert a == b
-
-        count = sum(
+        """Return the number of instructions which map to three or more potential operations."""
+        captures, _ = parsed_input
+        return sum(
             sum(
                 post[target] == self.compute(op, op_a, op_b, prior)
                 for op in range(16)
             ) >= 3
             for prior, (_, op_a, op_b, target), post in captures
         )
-        assert count != 85
-        return count
 
     def part2(self, parsed_input: InputType) -> int:
+        """Decode instruction mappings and run a program."""
         captures, program = parsed_input
-        op_mapping = {i: set(range(16)) for i in range(16)}
+
+        # Assume all mappings are possible. Then resolve via elimination.
+        op_possibilities = {i: set(range(16)) for i in range(16)}
+        op_map = {}  # Resolved operations.
         for prior, (instruction, op_a, op_b, target), post in captures:
+            if instruction in op_map:
+                # Optimization: do not check instructions we already resolved. 30ms => 20ms.
+                continue
             candidates = {
                 op
                 for op in range(16)
                 if post[target] == self.compute(op, op_a, op_b, prior)
             }
-            if len(candidates) == 0:
-                raise RuntimeError("Failed to find a candidate")
-            op_mapping[instruction] &= candidates
-            if len(op_mapping[instruction]) == 1:
-                found = list(op_mapping[instruction])[0]
-                for other, candidates in op_mapping.items():
+            op_possibilities[instruction] &= candidates
+            if len(op_possibilities[instruction]) == 1:
+                found = list(op_possibilities[instruction])[0]
+                op_map[instruction] = found
+                for other, candidates in op_possibilities.items():
                     if other != instruction and found in candidates:
                         candidates.remove(found)
-        if sum(len(candidates) == 1 for candidates in op_mapping.values()) != 16:
-            raise RuntimeError("Could not decode instructions.")
 
-        op_map = {i: list(candidates)[0] for i, candidates in op_mapping.items()}
+        # Run the program.
         memory = [0] * 4
         for instruction, op_a, op_b, target in program:
             memory[target] = self.compute(op_map[instruction], op_a, op_b, memory)
         return memory[0]
-
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
-
-    def input_parser(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
-        return super().input_parser(puzzle_input)
-        return puzzle_input.splitlines()
-        return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
-        # Words: mixed str and int
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in line.split()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-        # Regex splitting
-        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in patt.match(line).groups()
-            )
-            for line in puzzle_input.splitlines()
-        ]
 
 # vim:expandtab:sw=4:ts=4
