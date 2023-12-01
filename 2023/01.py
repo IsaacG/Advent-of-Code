e19a9b5f4d9221d656e68b413f3401378485e722
Isaac Good <github@isaacgood.com>
2023-11-30 21:52:31 -0800

    2023/01 with fix to my first attempt

--- 2023/01.py
+++ 2023/01.py
@@ -40,8 +40,9 @@ class Day01(aoc.Challenge):
 
     TESTS = [
         aoc.TestCase(inputs=SAMPLE[0], part=1, want=142),
-        aoc.TestCase(inputs="eightwo", part=2, want=82),
-        aoc.TestCase(inputs=SAMPLE[1], part=2, want=281),
+        # aoc.TestCase(inputs=SAMPLE[1], part=2, want=281),
+        # aoc.TestCase(inputs="eightwo", part=2, want=82),
+        aoc.TestCase(inputs="hczrldvxffninemzbhsv2two5eightwozfh", part=2, want=92),
         # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
     ]
 
@@ -51,7 +52,6 @@ class Day01(aoc.Challenge):
         c = 0
         for line in parsed_input:
             n = [int(i) for i in line if i.isdigit()]
-            print(n)
             c += n[0] * 10 + n[-1]
 
         return c
@@ -60,6 +60,9 @@ class Day01(aoc.Challenge):
         va = {a: str(b + 1) for b, a in enumerate("one two three four five six seven eight nine".split())}
         total = 0
         for line in parsed_input:
+            # =================
+            # Second approach which I got working first.
+
             nums = []
             for i in range(len(line)):
                 if line[i:i+1].isdigit():
@@ -68,18 +71,51 @@ class Day01(aoc.Challenge):
                     for a, b in va.items():
                         if line[i:].startswith(a):
                             nums.append(b)
-            print(nums, line)
-            total += int(nums[0]) * 10 + int(nums[-1])
-        return total
+            first_last = f"{nums[0]}{nums[-1]}"
+            # =================
 
-
-        return
-        c = 0
-        va = {a: str(b + 1) for b, a in enumerate("one two three four five six seven eight nine".split())}
-        for line in parsed_input:
+            # =================
+            # First approach that had a bug and took a while to fix.
             og = line
             line2 = line
-            replace = any(i in line for i in va.keys())
+
+            loop = True
+            while loop:
+                loop = False
+                for i in range(len(line)):
+                    for a, b in va.items():
+                        if line[i:].startswith(a):
+                            line = line.replace(a, str(b), 1)
+                            loop = True
+                            break
+                    if loop:
+                        break
+
+            line2 = og[::-1]
+            loop = True
+            while loop:
+                loop = False
+                for i in range(len(line2)):
+                    for a, b in va.items():
+                        if line2[i:].startswith(a[::-1]):
+                            line2 = line2.replace(a[::-1], str(b), 1)
+                            loop = True
+                            break
+                    if loop:
+                        break
+            line2 = line2[::-1]
+
+            n = [int(i) for i in line if i.isdigit()]
+            m = [int(i) for i in line2 if i.isdigit()]
+            line_num  = f"{n[0]}{m[-1]}"
+            # =================
+
+            total += int(line_num)
+        return total
+
+        c = 0
+        for line in parsed_input:
+            line2 = line
 
             loop = True
             while loop:
@@ -106,15 +142,9 @@ class Day01(aoc.Challenge):
                     if loop:
                         break
             line2 = line2[::-1]
-            print(f"{og=} {line=} {line2=}")
 
             n = [int(i) for i in line if i.isdigit()]
             m = [int(i) for i in line2 if i.isdigit()]
-            if replace and False:
-                print(og)
-                print(line)
-                print(f"{n[0]}{m[-1]}")
-                print()
             line_num  = f"{n[0]}{m[-1]}"
             c += int(line_num)
 
