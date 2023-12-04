2ae333c5e49b5277c49494e4e9aee8f4ba74df32
Isaac Good <github@isaacgood.com>
2023-12-03 21:48:46 -0800

    2023/04: change lists to sets

--- 2023/04.py
+++ 2023/04.py
@@ -11,7 +11,7 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
-InputType = list[tuple[list[int], list[int]]]
+InputType = list[tuple[set[int], set[int]]]
 
 
 class Day04(aoc.Challenge):
@@ -27,7 +27,7 @@ class Day04(aoc.Challenge):
         return sum(
             2 ** (count - 1)
             for winning, has in parsed_input
-            if (count := sum(h in winning for h in has))
+            if (count := len(winning & has))
         )
 
     def part2(self, parsed_input: InputType) -> int:
@@ -36,7 +36,7 @@ class Day04(aoc.Challenge):
         card_count = {i: 1 for i in range(len(parsed_input))}
 
         for card_number, (winning, has) in enumerate(parsed_input):
-            points = sum(h in winning for h in has)
+            points = len(winning & has)
             count = card_count[card_number]
             # For all copies we get, add `count` times more.
             for i in range(card_number + 1, card_number + points + 1):
@@ -49,8 +49,8 @@ class Day04(aoc.Challenge):
         for line in puzzle_input.splitlines():
             winning, has = line.split(" | ")
             winning = winning.split(": ")[1].strip()
-            winning_numbers = [int(i) for i in winning.split()]
-            has_numbers = [int(i) for i in has.split()]
+            winning_numbers = {int(i) for i in winning.split()}
+            has_numbers = {int(i) for i in has.split()}
             out.append((winning_numbers, has_numbers))
         return out
 
