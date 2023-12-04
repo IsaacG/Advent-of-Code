09a99cc445ca128a7a1519f12b592748c187257d
Isaac Good <github@isaacgood.com>
2023-12-03 21:26:34 -0800

    Tidy 2023/04

--- 2023/04.py
+++ 2023/04.py
@@ -1,117 +1,57 @@
 #!/bin/python
 """Advent of Code, Day 4: Scratchcards."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
 
 from lib import aoc
 
-SAMPLE = [
-    """\
+SAMPLE = """\
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""",  # 1
-]
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
-LineType = int
-InputType = list[LineType]
+InputType = list[tuple[list[int], list[int]]]
 
 
 class Day04(aoc.Challenge):
-    """Day 4: Scratchcards."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [5, 50]
+    """Day 4: Scratchcards. Count up scorecard points."""
 
     TESTS = [
-        aoc.TestCase(inputs=SAMPLE[0], part=1, want=13),
-        aoc.TestCase(inputs=SAMPLE[0], part=2, want=30),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
+        aoc.TestCase(inputs=SAMPLE, part=1, want=13),
+        aoc.TestCase(inputs=SAMPLE, part=2, want=30),
     ]
 
-    # INPUT_PARSER = aoc.parse_one_str_per_line
-    # INPUT_PARSER = aoc.parse_one_str
-    # INPUT_PARSER = aoc.parse_one_int
-    # INPUT_PARSER = aoc.parse_one_str_per_line
-    # INPUT_PARSER = aoc.parse_one_int_per_line
-    # INPUT_PARSER = aoc.parse_multi_str_per_line
-    # INPUT_PARSER = aoc.parse_re_group_str(r"(a) .* (b) .* (c)")
-    # INPUT_PARSER = aoc.parse_re_findall_str(r"(a|b|c)")
-    # INPUT_PARSER = aoc.parse_multi_int_per_line
-    # INPUT_PARSER = aoc.parse_re_group_int(r"(\d+)")
-    # INPUT_PARSER = aoc.parse_re_findall_int(r"\d+")
-    # INPUT_PARSER = aoc.parse_multi_mixed_per_line
-    # INPUT_PARSER = aoc.parse_re_group_mixed(r"(foo) .* (\d+)")
-    # INPUT_PARSER = aoc.parse_re_findall_mixed(r"\d+|foo|bar")
-    # INPUT_PARSER = aoc.ParseBlocks([aoc.parse_one_str_per_line, aoc.parse_re_findall_int(r"\d+")])
-    # INPUT_PARSER = aoc.ParseOneWord(aoc.Board.from_int_block)
-    # INPUT_PARSER = aoc.parse_ascii_bool_map("#")
-
     def part1(self, parsed_input: InputType) -> int:
-        total = 0
-        for winning, has in parsed_input:
-            count = sum(h in winning for h in has)
-            if count:
-                points = 2 ** (count-1)
-                total += points
-        return total
+        """Count how many winning numbers we have."""
+        return sum(
+            2 ** (count - 1)
+            for winning, has in parsed_input
+            if (count := sum(h in winning for h in has))
+        )
 
     def part2(self, parsed_input: InputType) -> int:
-        total = 0
-        card_count = {i: 1 for i in range(1, len(parsed_input) + 1)}
-        for idx, (winning, has) in enumerate(parsed_input, start=1):
-            count = sum(h in winning for h in has)
-            if count:
-                c = card_count[idx]
-                for i in range(idx + 1, idx + count + 1):
-                    card_count[i] += c
-        return sum(card_count.values())
+        """Count how many scorecards we have."""
+        # Initialize the card count to one of each.
+        card_count = {i: 1 for i in range(len(parsed_input))}
 
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
+        for card_number, (winning, has) in enumerate(parsed_input):
+            points = sum(h in winning for h in has)
+            count = card_count[card_number]
+            # For all copies we get, add `count` times more.
+            for i in range(card_number + 1, card_number + points + 1):
+                card_count[i] += count
+        return sum(card_count.values())
 
     def input_parser(self, puzzle_input: str) -> InputType:
         """Parse the input data."""
         out = []
         for line in puzzle_input.splitlines():
-            a, b = line.split(" | ")
-            a = a.split(": ")[1].strip()
-            an = [int(i) for i in a.split()]
-            bn = [int(i) for i in b.split()]
-            out.append((an, bn))
+            winning, has = line.split(" | ")
+            winning = winning.split(": ")[1].strip()
+            winning_numbers = [int(i) for i in winning.split()]
+            has_numbers = [int(i) for i in has.split()]
+            out.append((winning_numbers, has_numbers))
         return out
-
-        return super().input_parser(puzzle_input)
-        return puzzle_input.splitlines()
-        return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
-        # Words: mixed str and int
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in line.split()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-        # Regex splitting
-        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in patt.match(line).groups()
-            )
-            for line in puzzle_input.splitlines()
-        ]
 
 # vim:expandtab:sw=4:ts=4
