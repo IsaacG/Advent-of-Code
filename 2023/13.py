08667433cd2892f39dcb8957eaf12537afc64457
Isaac Good <github@isaacgood.com>
2023-12-12 22:16:04 -0800

    Solve 2023/13 (rough)

---
+++ 2023/13.py
@@ -0,0 +1,178 @@
+#!/bin/python
+"""Advent of Code, Day 13: Point of Incidence."""
+from __future__ import annotations
+
+import collections
+import functools
+import itertools
+import math
+import re
+
+from lib import aoc
+
+SAMPLE = [
+    """\
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#""",  # 2
+    """\
+123456789
+    ><   
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+    ><   
+123456789""",  # 3
+    """\
+1 #...##..# 1
+2 #....#..# 2
+3 ..##..### 3
+4v#####.##.v4
+5^#####.##.^5
+6 ..##..### 6
+7 #....#..# 7""",  # 4
+    '5',  # 5
+    '4',  # 6
+]
+
+LineType = int
+InputType = list[LineType]
+
+
+class Day13(aoc.Challenge):
+    """Day 13: Point of Incidence."""
+
+    DEBUG = True
+    # Default is True. On live solve, submit one tests pass.
+    # SUBMIT = {1: False, 2: False}
+    # PARAMETERIZED_INPUTS = [5, 50]
+
+    TESTS = [
+        aoc.TestCase(inputs=SAMPLE[0], part=1, want=405),
+        aoc.TestCase(inputs=SAMPLE[0], part=2, want=400),
+        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
+    ]
+
+    # INPUT_PARSER = aoc.parse_one_str_per_line
+    # INPUT_PARSER = aoc.parse_one_str
+    # INPUT_PARSER = aoc.parse_one_int
+    # INPUT_PARSER = aoc.parse_one_str_per_line
+    # INPUT_PARSER = aoc.parse_one_int_per_line
+    # INPUT_PARSER = aoc.parse_multi_str_per_line
+    # INPUT_PARSER = aoc.parse_re_group_str(r"(a) .* (b) .* (c)")
+    # INPUT_PARSER = aoc.parse_re_findall_str(r"(a|b|c)")
+    # INPUT_PARSER = aoc.parse_multi_int_per_line
+    # INPUT_PARSER = aoc.parse_re_group_int(r"(\d+)")
+    # INPUT_PARSER = aoc.parse_re_findall_int(r"\d+")
+    # INPUT_PARSER = aoc.parse_multi_mixed_per_line
+    # INPUT_PARSER = aoc.parse_re_group_mixed(r"(foo) .* (\d+)")
+    # INPUT_PARSER = aoc.parse_re_findall_mixed(r"\d+|foo|bar")
+    INPUT_PARSER = aoc.ParseBlocks([aoc.parse_ascii_bool_map("#")])
+
+    def find(self, points) -> int:
+        min_x, min_y, max_x, max_y = aoc.bounding_coords(points)
+        for x in range(min_x, max_x):
+            shift = 2 * x + 1
+            if all(complex(shift - p.real, p.imag) in points for p in points if min_x <= shift - p.real <= max_x):
+                return x + 1
+        for y in range(min_y, max_y):
+            shift = 2 * y + 1
+            if all(complex(p.real, shift - p.imag) in points for p in points if min_y <= shift - p.imag <= max_y):
+                return 100 * (y + 1)
+        return None
+
+    def find_b(self, points, point) -> int:
+        min_x, min_y, max_x, max_y = aoc.bounding_coords(points)
+        for x in range(min_x, max_x):
+            shift = 2 * x + 1
+            if not min_x <= shift - point.real <= max_x:
+                continue
+            if all(complex(shift - p.real, p.imag) in points for p in points if min_x <= shift - p.real <= max_x):
+                return x + 1
+        for y in range(min_y, max_y):
+            shift = 2 * y + 1
+            if not (min_y <= shift - point.imag <= max_y):
+                continue
+            if all(complex(p.real, shift - p.imag) in points for p in points if min_y <= shift - p.imag <= max_y):
+                return 100 * (y + 1)
+        return None
+
+    def part1(self, parsed_input: InputType) -> int:
+        total = 0
+        for points in parsed_input:
+            f = self.find(points)
+            total += f
+        return total
+
+    def find2(self, points: InputType) -> int:
+        min_x, min_y, max_x, max_y = aoc.bounding_coords(points)
+        for x in range(min_x, max_x + 1):
+            for y in range(min_y, max_y + 1):
+                point = complex(x, y)
+                exists = point in points
+                if exists:
+                    points.remove(point)
+                    f = self.find_b(points, point)
+                    points.add(point)
+                    if f:
+                        return f
+                else:
+                    points.add(point)
+                    f = self.find_b(points, point)
+                    points.remove(point)
+                    if f:
+                        return f
+
+    def part2(self, parsed_input: InputType) -> int:
+        total = 0
+        for idx, points in enumerate(parsed_input):
+            f = self.find2(points)
+            total += f
+        return total
+
+    def solver(self, parsed_input: InputType, param: bool) -> int | str:
+        raise NotImplementedError
+
+    def input_parser(self, puzzle_input: str) -> InputType:
+        """Parse the input data."""
+        return super().input_parser(puzzle_input)
+        return puzzle_input.splitlines()
+        return puzzle_input
+        return [int(i) for i in puzzle_input.splitlines()]
+        mutate = lambda x: (x[0], int(x[1])) 
+        return [mutate(line.split()) for line in puzzle_input.splitlines()]
+        # Words: mixed str and int
+        return [
+            tuple(
+                int(i) if i.isdigit() else i
+                for i in line.split()
+            )
+            for line in puzzle_input.splitlines()
+        ]
+        # Regex splitting
+        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
+        return [
+            tuple(
+                int(i) if i.isdigit() else i
+                for i in patt.match(line).groups()
+            )
+            for line in puzzle_input.splitlines()
+        ]
+
+# vim:expandtab:sw=4:ts=4
