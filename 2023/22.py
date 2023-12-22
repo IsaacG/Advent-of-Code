d030ae5e53a12bfaeb893e34a9d28c184708df00
Isaac Good <github@isaacgood.com>
2023-12-22 10:04:31 -0800

    Tidy 2023/22

--- 2023/22.py
+++ 2023/22.py
@@ -29,47 +29,57 @@ class Day22(aoc.Challenge):
         # Brick ID to coordinates.
         bricks = parsed_input
 
+        # Sort bricks by lowest bottom, i.e. the order in which they land.
+        landing_order = sorted(bricks, key=lambda x: bricks[x][2])
+
         # Track which bricks exist by x-y column for faster lookups.
-        brick_by_x_y = collections.defaultdict(list)
-        for num, (start_x, start_y, start_z, end_x, end_y, _) in bricks.items():
-            for x_col in range(start_x, end_x + 1):
-                for y_col in range(start_y, end_y + 1):
-                    brick_by_x_y[x_col, y_col].append(num)
+        footprints = {
+            brick: [
+                (x_col, y_col)
+                for x_col in range(start_x, end_x + 1)
+                for y_col in range(start_y, end_y + 1)
+            ] for brick, (start_x, start_y, start_z, end_x, end_y, _) in bricks.items()
+        }
+        brick_by_x_y = collections.defaultdict(set)
+        for brick, footprint in footprints.items():
+            for coord in footprint:
+                brick_by_x_y[coord].add(brick)
+        brick_overlaps = collections.defaultdict(set)
+        for brick, footprint in footprints.items():
+            for coord in footprint:
+                brick_overlaps[brick].update(brick_by_x_y[coord])
 
         # Track which other bricks a given brick is supporting.
         holding: dict[int, list[int]] = {brick: [] for brick in bricks}
         held_by: dict[int, list[int]] = {brick: [] for brick in bricks}
-        # Drop bricks until all bricks have settled.
+        # Drop bricks until all bricks have settled; this is the updated brick list.
         settled: set[int] = set()
-        # Sort bricks by lowest bottom, i.e. most likely to settle next.
-        inflight = sorted(bricks, key=lambda x: bricks[x][2])
+        settled_bricks: dict[int, list[int]] = {}
 
-        while inflight:
-            for brick in inflight:
-                start_x, start_y, start_z, end_x, end_y, _ = bricks[brick]
-                # Locate other bricks in the same column(s) as this brick but lower down.
-                potential_tops = {
-                    other for (x, y), others in brick_by_x_y.items()
-                    if start_x <= x <= end_x and start_y <= y <= end_y for other in others
-                }
-                # Check if this brick is resting on top of any other bricks.
-                landed = {other for other in potential_tops & settled if bricks[other][5] + 1 == start_z}
-                if bricks[brick][2] == 1 or landed:
-                    inflight.remove(brick)
-                    settled.add(brick)
-                    for other in landed:
-                        holding[other].append(brick)
-                    held_by[brick].extend(landed)
-                else:
-                    new_bottom = 1 + max(
-                        (bricks[other][5] for other in potential_tops if bricks[other][5] < start_z),
-                        default=0,
-                    )
-                    if new_bottom < start_z:
-                        dist = start_z - new_bottom
-                        bricks[brick][2] -= dist
-                        bricks[brick][5] -= dist
-                        break
+        for brick in landing_order:
+            start_x, start_y, start_z, end_x, end_y, end_z = bricks[brick]
+            # Locate settled bricks in the same column(s) as this brick.
+            potential_tops = brick_overlaps[brick] & settled
+            # Find the highest point beneath the current brick.
+            lands_at_height = max(
+                (settled_bricks[other][5] for other in potential_tops if settled_bricks[other][5] < start_z),
+                default=0,
+            )
+            
+            # Add the brick in it's final position to the settled data.
+            drop_distance = start_z - lands_at_height - 1
+            start_z -= drop_distance
+            end_z -= drop_distance
+            settled_bricks[brick] = (start_x, start_y, start_z, end_x, end_y, end_z)
+            settled.add(brick)
+
+            # Which bricks will be the support?
+            lands_on_bricks = {other for other in potential_tops if settled_bricks[other][5] == lands_at_height}
+            for other in lands_on_bricks:
+                holding[other].append(brick)
+            held_by[brick].extend(lands_on_bricks)
+
+        bricks = settled_bricks
 
         # Part 1: count the bricks which can safely be removed.
         # If a brick is not a sole support of any other brick, it can be removed.
@@ -104,7 +114,6 @@ class Day22(aoc.Challenge):
     def input_parser(self, puzzle_input: str) -> InputType:
         """Parse the input data."""
         data = aoc.parse_re_findall_int(aoc.RE_INT).parse(puzzle_input)
-        bricks = dict(enumerate(data))
-        return bricks
+        return {idx: tuple(values) for idx, values in enumerate(data)}
 
 # vim:expandtab:sw=4:ts=4
