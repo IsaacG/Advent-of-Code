9dadd7dca3ae3b1c942dbdb0ee86683ae67121cf
Isaac Good <github@isaacgood.com>
2023-12-25 00:08:04 -0800

    Tidy 2023/25

--- 2023/25.py
+++ 2023/25.py
@@ -12,8 +12,7 @@ import re
 
 from lib import aoc
 
-SAMPLE = [
-    """\
+SAMPLE = """\
 jqt: rhn xhk nvd
 rsh: frs pzl lsr
 xhk: hfx
@@ -26,91 +25,58 @@ ntq: jqt hfx bvb xhk
 nvd: lhk
 lsr: lhk
 rzs: qnr cmg lsr rsh
-frs: qnr lhk lsr""",  # 4
-]
+frs: qnr lhk lsr"""
 
-LineType = int
-InputType = list[LineType]
+InputType = dict[int, set[int]]
 
 
 class Day25(aoc.Challenge):
     """Day 25: Snowverload."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [False, True]
-
     TESTS = [
         aoc.TestCase(inputs=SAMPLE[0], part=1, want=54),
-        aoc.TestCase(inputs=SAMPLE[0], part=2, want=0),
-        # aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
+        aoc.TestCase(inputs=SAMPLE[0], part=2, want=aoc.TEST_SKIP),
     ]
 
-    # INPUT_PARSER = aoc.parse_one_str_per_line
-    # INPUT_PARSER = aoc.parse_one_str
-    # INPUT_PARSER = aoc.parse_one_int
-    # INPUT_PARSER = aoc.parse_one_str_per_line
-    # INPUT_PARSER = aoc.parse_one_int_per_line
-    # INPUT_PARSER = aoc.parse_multi_str_per_line
-    # INPUT_PARSER = aoc.parse_re_group_str(r"(a) .* (b) .* (c)")
-    # INPUT_PARSER = aoc.parse_re_findall_str(r"(a|b|c)")
-    # INPUT_PARSER = aoc.parse_multi_int_per_line
-    # INPUT_PARSER = aoc.parse_re_group_int(r"(\d+)")
-    # INPUT_PARSER = aoc.parse_re_findall_int(aoc.RE_INT)
-    # INPUT_PARSER = aoc.parse_multi_mixed_per_line
-    # INPUT_PARSER = aoc.parse_re_group_mixed(r"(foo) .* (\d+)")
-    # INPUT_PARSER = aoc.parse_re_findall_mixed(r"\d+|foo|bar")
-    # INPUT_PARSER = aoc.ParseBlocks([aoc.parse_one_str_per_line, aoc.parse_re_findall_int(r"\d+")])
-    # INPUT_PARSER = aoc.ParseOneWord(aoc.Board.from_int_block)
-    # INPUT_PARSER = aoc.parse_ascii_bool_map("#")
-    # ---
-    # INPUT_PARSER = aoc.CharCoordinatesParser("S.#")
-    # (width, height), start, garden, rocks = parsed_input
-    # max_x, max_y = width - 1, height - 1
-
     def part1(self, parsed_input: InputType) -> int:
-        graph = {src: list(dsts) for src, dsts in parsed_input.items()}
+        graph = parsed_input
+        graph_list = {src: list(dsts) for src, dsts in graph.items()}
         nodes = list(graph)
+        node_count = len(nodes)
         counts = collections.Counter()
 
         for j in range(200):
             for i in range(50):
+                # Randomly wander the graph from start until we find end. Record edges traversed.
                 start = random.choice(nodes)
                 end = random.choice(nodes)
                 cur = start
                 edges = set()
                 while cur != end:
-                    neighbor = random.choice(graph[cur])
+                    neighbor = random.choice(graph_list[cur])
                     edges.add(tuple(sorted([cur, neighbor])))
                     cur = neighbor
                 counts.update(edges)
 
-            cpy = copy.deepcopy(parsed_input)
-            for (a, b), _ in counts.most_common(3):
-                cpy[a].remove(b)
-                cpy[b].remove(a)
+            # See if removing the most commonly traversed three edges results in a split graph.
+            candidates = [i for i, _ in counts.most_common(3)]
+            for a, b in candidates:
+                graph[a].remove(b)
+                graph[b].remove(a)
 
-            todo = set(cpy)
-            groups = []
+            todo = {nodes[0]}
+            group = set()
             while todo:
-                gtodo = {todo.pop()}
-                group = set()
-                while gtodo:
-                    cur = gtodo.pop()
-                    todo.discard(cur)
-                    group.add(cur)
-                    gtodo.update(cpy[cur] - group)
-                groups.append(group)
-            if len(groups) == 2:
-                print(counts.most_common(3))
-                return math.prod(len(g) for g in groups)
-
-    def part2(self, parsed_input: InputType) -> int:
-        raise NotImplementedError
-
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
+                cur = todo.pop()
+                group.add(cur)
+                for neighbor in graph[cur]:
+                    if neighbor not in group:
+                        todo.add(neighbor)
+            if len(group) != node_count:
+                return len(group) * (node_count - len(group))
+            for a, b in candidates:
+                graph[a].add(b)
+                graph[b].add(a)
 
     def input_parser(self, puzzle_input: str) -> InputType:
         """Parse the input data."""
@@ -120,6 +86,8 @@ class Day25(aoc.Challenge):
             for dst in dsts.split():
                 connected[src].add(dst)
                 connected[dst].add(src)
-        return connected
+        labels = {val: idx for idx, val in enumerate(connected)}
+        # Relabel the nodes using index numbers vs strings.
+        return {labels[src]: {labels[dst] for dst in dsts} for src, dsts in connected.items()}
 
 # vim:expandtab:sw=4:ts=4
