a936effbb000f4ca584921edaa67b1971d3a39b3
Isaac Good <github@isaacgood.com>
2024-05-05 14:53:46 -0700

    Fix formatting (2019)

--- 2019/d05.py
+++ 2019/d05.py
@@ -14,37 +14,37 @@ class Day05(aoc.Challenge):
 
     TESTS = (
         # Part 1
-        aoc.TestCase(inputs='4,0,99', part=1, want=4),
-        aoc.TestCase(inputs='04,2,99', part=1, want=99),
-        aoc.TestCase(inputs='104,6,99', part=1, want=6),
-        aoc.TestCase(inputs='1002,7,3,5,104,5,99,33', part=1, want=99),
+        aoc.TestCase(inputs="4,0,99", part=1, want=4),
+        aoc.TestCase(inputs="04,2,99", part=1, want=99),
+        aoc.TestCase(inputs="104,6,99", part=1, want=6),
+        aoc.TestCase(inputs="1002,7,3,5,104,5,99,33", part=1, want=99),
         # Part 2
-        aoc.TestCase(inputs='3,9,8,9,10,9,4,9,99,-1,8', part=2, want=0),  # input == 8 POS Mode
-        aoc.TestCase(inputs='3,9,8,9,10,9,4,9,99,-1,5', part=2, want=1),  # input == 5 POS Mode
-        aoc.TestCase(inputs='3,9,7,9,10,9,4,9,99,-1,8', part=2, want=1),  # input  < 8 POS Mode
-        aoc.TestCase(inputs='3,9,7,9,10,9,4,9,99,-1,5', part=2, want=0),  # input  < 5 POS Mode
-        aoc.TestCase(inputs='3,3,1108,-1,8,3,4,3,99', part=2, want=0),    # input == 8 IMM Mode
-        aoc.TestCase(inputs='3,3,1108,-1,5,3,4,3,99', part=2, want=1),    # input == 5 IMM Mode
-        aoc.TestCase(inputs='3,3,1107,-1,8,3,4,3,99', part=2, want=1),    # input  < 8 IMM Mode
-        aoc.TestCase(inputs='3,3,1107,-1,5,3,4,3,99', part=2, want=0),    # input  < 5 IMM Mode
+        aoc.TestCase(inputs="3,9,8,9,10,9,4,9,99,-1,8", part=2, want=0),  # input == 8 POS Mode
+        aoc.TestCase(inputs="3,9,8,9,10,9,4,9,99,-1,5", part=2, want=1),  # input == 5 POS Mode
+        aoc.TestCase(inputs="3,9,7,9,10,9,4,9,99,-1,8", part=2, want=1),  # input  < 8 POS Mode
+        aoc.TestCase(inputs="3,9,7,9,10,9,4,9,99,-1,5", part=2, want=0),  # input  < 5 POS Mode
+        aoc.TestCase(inputs="3,3,1108,-1,8,3,4,3,99", part=2, want=0),    # input == 8 IMM Mode
+        aoc.TestCase(inputs="3,3,1108,-1,5,3,4,3,99", part=2, want=1),    # input == 5 IMM Mode
+        aoc.TestCase(inputs="3,3,1107,-1,8,3,4,3,99", part=2, want=1),    # input  < 8 IMM Mode
+        aoc.TestCase(inputs="3,3,1107,-1,5,3,4,3,99", part=2, want=0),    # input  < 5 IMM Mode
     )
     INPUT_PARSER = aoc.parse_one_str
 
     def part1(self, parsed_input: str) -> int:
         computer = intcode.Computer(parsed_input)
         if not self.testing:
-            computer.io.append(1)
+            computer.input.append(1)
         computer.run()
-        *results, output = computer.io
+        *results, output = computer.output
         if any(results):
             raise RuntimeError("Test Failed")
         return output
 
     def part2(self, parsed_input: str) -> int:
         computer = intcode.Computer(parsed_input)
-        computer.io.append(5)
+        computer.input.append(5)
         computer.run()
-        return computer.io.popleft()
+        return computer.output.popleft()
 
 
 # vim:expandtab:sw=4:ts=4
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -11,46 +11,79 @@ from lib import aoc
 
 
 class State(enum.Enum):
-    INIT = enum.auto()
-    RUN  = enum.auto()
-    STOP = enum.auto()
+    READY  = enum.auto()
+    RUN    = enum.auto()
+    IOWAIT = enum.auto()
+    STOP   = enum.auto()
 
 
-class OpCode(enum.IntEnum):
-    ADD     =  1
-    MUL     =  2
-    INPUT   =  3
-    OUTPUT  =  4
-    J_TRUE  =  5  # jump if true
-    J_FALSE =  6  # jump if false
-    LT      =  7  # less than
-    EQ      =  8  # equal
-    STOP    = 99
+OP_CODE_ADD     =  1
+OP_CODE_MUL     =  2
+OP_CODE_INPUT   =  3
+OP_CODE_OUTPUT  =  4
+OP_CODE_J_TRUE  =  5  # jump if true
+OP_CODE_J_FALSE =  6  # jump if false
+OP_CODE_LT      =  7  # less than
+OP_CODE_EQ      =  8  # equal
+OP_CODE_RELBASE =  9  # set relative base
+OP_CODE_STOP    = 99
+
+OP_CODE_NAMES = {
+    OP_CODE_ADD:     "ADD",
+    OP_CODE_MUL:     "MUL",
+    OP_CODE_INPUT:   "INPUT",
+    OP_CODE_OUTPUT:  "OUTPUT",
+    OP_CODE_J_TRUE:  "J_TRUE",
+    OP_CODE_J_FALSE: "J_FALSE",
+    OP_CODE_LT:      "LT",
+    OP_CODE_EQ:      "EQ",
+    OP_CODE_RELBASE: "RELBASE",
+    OP_CODE_STOP:    "STOP",
+}
+
+PARAMETER_MODE_POSITIONAL = 0
+PARAMETER_MODE_IMMEDIATE  = 1
+PARAMETER_MODE_RELATIVE   = 2
 
 
 class Computer:
     """Intcode computer."""
 
-    def __init__(self, program: str):
+    def __init__(
+        self,
+        program: str,
+        input_q: collections.deque | None = None,
+        output_q: collections.deque | None = None,
+        debug: bool = False,
+    ):
         """Initialize computer 'hardware'."""
-        self.memory = [int(num) for num in program.split(',')]
-        self.ptr = 0
-        self.io = collections.deque()
-        self.state = State.INIT
-        self.debug = False
+        if input_q is None:
+            input_q = collections.deque()
+        if output_q is None:
+            output_q = input_q
 
-        self._debug(program)
+        self.memory = collections.defaultdict(int)
+        self.memory.update(dict(enumerate(int(num) for num in program.split(","))))
+        self.ptr = 0
+        self.relative_base = 0
+        self.input = input_q
+        self.output = output_q
+        self.state = State.READY
+        self.debug = debug
+
+        self._debug(f"Program: {program}")
 
         self.operators = {
-            OpCode.ADD:     (2, 1, operator.add,       self.store),
-            OpCode.MUL:     (2, 1, operator.mul,       self.store),
-            OpCode.INPUT:   (0, 1, self.io.popleft,    self.store),
-            OpCode.OUTPUT:  (1, 0, lambda x: x,        self.io.append),
-            OpCode.J_TRUE:  (1, 1, operator.truth,     self.jump),
-            OpCode.J_FALSE: (1, 1, operator.not_,      self.jump),
-            OpCode.LT:      (2, 1, operator.lt,        self.store),
-            OpCode.EQ:      (2, 1, operator.eq,        self.store),
-            OpCode.STOP:    (0, 0, lambda: State.STOP, self.set_state),
+            OP_CODE_ADD:     (2, 1, operator.add,       self.store),
+            OP_CODE_MUL:     (2, 1, operator.mul,       self.store),
+            OP_CODE_INPUT:   (0, 1, self.get_input,     self.store),
+            OP_CODE_OUTPUT:  (1, 0, lambda x: x,        self.output.append),
+            OP_CODE_J_TRUE:  (1, 1, operator.truth,     self.jump),
+            OP_CODE_J_FALSE: (1, 1, operator.not_,      self.jump),
+            OP_CODE_LT:      (2, 1, operator.lt,        self.store),
+            OP_CODE_EQ:      (2, 1, operator.eq,        self.store),
+            OP_CODE_RELBASE: (1, 0, lambda x: x,        self.update_relative_base),
+            OP_CODE_STOP:    (0, 0, lambda: State.STOP, self.set_state),
         }
 
     def __str__(self):
@@ -58,13 +91,21 @@ class Computer:
         out = []
         for chunk in more_itertools.ichunked(enumerate(self.memory), 16):
             chunk = list(chunk)
-            out.append('Addr:  ' + '  '.join(f'{c[0]:02d}' for c in chunk))
-            out.append('Vals:  ' + '  '.join(f'{c[1]:02d}' for c in chunk))
+            out.append("Addr:  " + "  ".join(f"{c[0]:02d}" for c in chunk))
+            out.append("Vals:  " + "  ".join(f"{c[1]:02d}" for c in chunk))
         return "\n".join(out)
 
     def _debug(self, msg: str) -> None:
         if self.debug:
             print(msg)
+
+    def get_input(self) -> int | None:
+        """Read input or change state to blocked."""
+        if self.input:
+            return self.input.popleft()
+        self.ptr -= 2
+        self.set_state(State.IOWAIT)
+        return None
 
     def get_next(self) -> int:
         """Read one value from ptr and increment ptr."""
@@ -72,10 +113,26 @@ class Computer:
         self.ptr += 1
         return value
 
-    def operand(self, immediate: bool) -> int:
+    def operand(self, param_mode: int) -> int:
         """Load a paramemter from ptr, either immediate or positional mode."""
-        value = self.get_next()
-        return value if immediate else self.memory[value]
+        value = self.ptr
+        self.ptr += 1
+        if param_mode == PARAMETER_MODE_POSITIONAL:
+            return self.memory[value]
+        if param_mode == PARAMETER_MODE_IMMEDIATE:
+            return value
+        if param_mode == PARAMETER_MODE_RELATIVE:
+            # self._debug(f"Look ip operand, {param_mode=}, {value=}, {self.relative_base=} {self.memory[value] + self.relative_base}")
+            return self.memory[value] + self.relative_base
+        raise ValueError(f"Invalid param mode, {param_mode}")
+
+    @property
+    def stopped(self) -> bool:
+        return self.state == State.STOP
+
+    def update_relative_base(self, value: int) -> None:
+        self._debug(f"Update relative_base = {self.relative_base} + {value} = {self.relative_base + value}")
+        self.relative_base += value
 
     def set_state(self, state: State) -> None:
         self.state = state
@@ -83,16 +140,20 @@ class Computer:
     def jump(self, value: bool, target: int) -> None:
         """Conditionally set the ptr."""
         if value:
-            self.ptr = target
+            self.ptr = self.memory[target]
 
     def store(self, value: int | bool, target: int) -> None:
         """Write a value to memory."""
+        if value is None:
+            return
         value = int(value)
         self.memory[target] = value
         # self._debug(f" ==> MEM[{target:2}] = {value}")
 
-    def run(self):
+    def run(self) -> None:
         """Run a program until it stops."""
+        if self.state == State.IOWAIT and not self.input:
+            return
         self.state = State.RUN
         while self.state == State.RUN:
             self.step()
@@ -100,23 +161,24 @@ class Computer:
     def step(self):
         """Run one step."""
         op = self.get_next()
-        param_modes, opcode_num = divmod(op, 100)
+        param_modes, opcode = divmod(op, 100)
 
         # Read the operand values.
-        opcode = OpCode(opcode_num)
         op_count_compute, op_count_act, compute, act = self.operators[opcode]
 
-        operands = []
+        compute_operands = []
         for _ in range(op_count_compute):
             param_modes, this_mode = divmod(param_modes, 10)
-            operands.append(self.operand(bool(this_mode)))
-        if op_count_act:
-            param_modes, this_mode = divmod(param_modes, 10)
-            operands.append(self.operand(this_mode or act == self.store))  # store is always immediate mode.
+            compute_operands.append(self.memory[self.operand(this_mode)])
 
-        # op_str = f"{opcode.name}({', '.join(str(i) for i in operands[:op_count_compute])})"
-        value = compute(*(operands[:op_count_compute]))
-        values = [value] + operands[op_count_compute:]
+        act_operands = []
+        for _ in range(op_count_act):
+            param_modes, this_mode = divmod(param_modes, 10)
+            act_operands.append(self.operand(this_mode))
+
+        value = compute(*compute_operands)
+        values = [value] + act_operands
+        # op_str = f"{OP_CODE_NAMES[opcode]}({', '.join(str(i) for i in compute_operands)})"
         # self._debug(f"{op_str:>16} => {act.__name__}({values})")
         act(*values)
 
