c91f5fe243af1d870587f001b3337b88ca5f6c56
Isaac Good <github@isaacgood.com>
2024-05-11 09:51:46 -0700

    Tidy 2017/03

--- 2017/d03.py
+++ 2017/d03.py
@@ -1,94 +1,55 @@
 #!/bin/python
-"""Advent of Code, Day 3: Spiral Memory."""
-from __future__ import annotations
+"""Advent of Code, Day 3: Spiral Memory.
 
-import collections
-import functools
-import itertools
-import math
-import re
+I was debating two approaches for p1.
+I got lucky in picking the one that worked better for p2 so p2 was pretty simple.
+
+I was thinking for p1 I could compute the solution without actually filling a
+full spiral matrix by walking the diagonals.
+1^2, 3^2, 5^2, 7^2, etc until the right "ring" then walking the "ring"
+until I found the number.
+But that wouldn't have helped for p2.
+
+Instead, I went with the approach that I thought might be less efficient
+but simpler to write: actually fill out a matrix.
+I was able to reuse that p1 code for p2.
+"""
 
 from lib import aoc
 
-SAMPLE = [
-    '1024',  # 10
-]
-
-LineType = int
-InputType = list[LineType]
+ROTATE_LEFT = 1j
 
 
 class Day03(aoc.Challenge):
     """Day 3: Spiral Memory."""
 
     TESTS = [
-        aoc.TestCase(inputs=SAMPLE[0], part=1, want=31),
+        aoc.TestCase(inputs="1024", part=1, want=31),
         aoc.TestCase(inputs="747", part=2, want=806),
     ]
     PARAMETERIZED_INPUTS = [False, True]
     INPUT_PARSER = aoc.parse_one_int
 
-    def part1(self, parsed_input: InputType) -> int:
-        location = complex()
-        number = 1
+    def solver(self, parsed_input: int, param: bool) -> int:
+        """Fill out a spiral matrix until we get to the input number."""
+        location = complex(0, 0)
         direction = complex(0, -1)
-        rotate_left = 1j
+        number = 1
 
         matrix = {location: number}
         for number in range(2, parsed_input + 1):
-            if location + direction * rotate_left not in matrix:
-                direction *= rotate_left
+            if location + direction * ROTATE_LEFT not in matrix:
+                direction *= ROTATE_LEFT
             location += direction
-            matrix[location] = number
-
-        return int(abs(location.real) + abs(location.imag))
-
-    def part2(self, parsed_input: InputType) -> int:
-        location = complex()
-        number = 1
-        direction = complex(0, -1)
-        rotate_left = 1j
-
-        matrix = {location: number}
-        for number in range(2, parsed_input + 1):
-            if location + direction * rotate_left not in matrix:
-                direction *= rotate_left
-            location += direction
-            value = sum(matrix.get(location + offset, 0) for offset in aoc.EIGHT_DIRECTIONS)
+            if param:
+                value = sum(
+                    matrix.get(location + offset, 0) for offset in aoc.EIGHT_DIRECTIONS)
+            else:
+                value = number
             matrix[location] = value
-            if value > parsed_input:
+            if param and value > parsed_input:
                 return value
 
-        raise RuntimeError("Not found")
-
-
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
-
-    def input_parser(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
-        return super().input_parser(puzzle_input)
-        return puzzle_input.splitlines()
-        return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
-        # Words: mixed str and int
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in line.split()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-        # Regex splitting
-        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in patt.match(line).groups()
-            )
-            for line in puzzle_input.splitlines()
-        ]
+        return int(abs(location.real) + abs(location.imag))
 
 # vim:expandtab:sw=4:ts=4
