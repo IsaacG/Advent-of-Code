f8b5110b2b24ba854531bf60b7c5a9cef4be5e1a
Isaac Good <github@isaacgood.com>
2024-05-25 08:22:53 -0700

    Solve 2017/d04

---
+++ 2017/d04.py
@@ -0,0 +1,41 @@
+#!/bin/python
+"""Advent of Code, Day 4: High-Entropy Passphrases."""
+
+import itertools
+from lib import aoc
+
+SAMPLE = [
+    """\
+aa bb cc dd ee
+aa bb cc dd aa
+aa bb cc dd aaa""",
+    """\
+abcde fghij
+abcde xyz ecdab
+a ab abc abd abf abj
+iiii oiii ooii oooi oooo
+oiii ioii iioi iiio"""
+]
+
+
+class Day04(aoc.Challenge):
+    """Day 4: High-Entropy Passphrases. Count valid passphrases."""
+
+    TESTS = [
+        aoc.TestCase(inputs=SAMPLE[0], part=1, want=2),
+        aoc.TestCase(inputs=SAMPLE[1], part=2, want=3),
+    ]
+    INPUT_PARSER = aoc.parse_multi_str_per_line
+
+    def part1(self, parsed_input: list[list[str]]) -> int:
+        """Count passphrases where no word is repeated."""
+        return sum(len(set(line)) == len(line) for line in parsed_input)
+
+    def part2(self, parsed_input: list[list[str]]) -> int:
+        """Count passphrases where no word is an anagram of another."""
+        return sum(
+            all(sorted(a) != sorted(b) for a, b in itertools.combinations(line, 2))
+            for line in parsed_input
+        )
+
+# vim:expandtab:sw=4:ts=4
