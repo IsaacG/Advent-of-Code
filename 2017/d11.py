5a37374e4ac06c2ddbaaa7e95f177b65be7327ee
Isaac Good <github@isaacgood.com>
2024-07-04 23:56:46 -0700

    Tidy 2017/11

--- 2017/d11.py
+++ 2017/d11.py
@@ -1,12 +1,5 @@
 #!/bin/python
 """Advent of Code, Day 11: Hex Ed."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
 
 from lib import aoc
 
@@ -16,9 +9,7 @@ SAMPLE = [
     ('ne,ne,s,s', 2),
     ('se,sw,se,sw,sw', 3),
 ]
-
-LineType = int
-InputType = list[LineType]
+OFFSETS = aoc.HEX_AXIAL_DIRS_FLAT_TOP
 
 
 class Day11(aoc.Challenge):
@@ -29,14 +20,14 @@ class Day11(aoc.Challenge):
         aoc.TestCase(part=1, inputs=data, want=want)
         for data, want in SAMPLE
     ] + [
-        aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
+        aoc.TestCase(part=2, inputs="", want=aoc.TEST_SKIP),
     ]
     INPUT_PARSER = aoc.parse_one_str
 
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        location = complex()
+    def solver(self, parsed_input: str, param: bool) -> int:
+        """Walk a hex grid and compute distances."""
+        location = complex(0, 0)
         farthest = 0
-        offsets = aoc.HEX_AXIAL_DIRS_FLAT_TOP
 
         def axial_distance() -> int:
             """Taken from https://www.redblobgames.com/grids/hexagons/#distances."""
@@ -44,9 +35,6 @@ class Day11(aoc.Challenge):
             return (abs(q) + abs(q + r) + abs(r)) // 2
 
         for direction in parsed_input.split(","):
-            location += offsets[direction]
+            location += OFFSETS[direction]
             farthest = max(farthest, axial_distance())
-        distance = axial_distance()
-        return distance if param else farthest
-
-# vim:expandtab:sw=4:ts=4
+        return axial_distance() if param else farthest
