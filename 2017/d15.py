d00367cc72f163eb752b0513b028255e9b045cd3
Isaac Good <github@isaacgood.com>
2024-08-04 09:38:01 -0700

    2017/15: refactor into one solver function

--- 2017/d15.py
+++ 2017/d15.py
@@ -23,54 +23,31 @@ InputType = list[LineType]
 class Day15(aoc.Challenge):
     """Day 15: Dueling Generators."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [False, True]
-
+    PARAMETERIZED_INPUTS = [False, True]
     TESTS = [
         aoc.TestCase(part=1, inputs=SAMPLE[0], want=588),
         aoc.TestCase(part=2, inputs=SAMPLE[0], want=309),
-        # aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
     ]
-
     INPUT_PARSER = aoc.parse_ints_per_line
 
-    def part1(self, parsed_input: InputType) -> int:
+    def solver(self, parsed_input: list[list[int]], param: bool) -> int:
         (val_a,), (val_b,) = parsed_input
-        count = 0
-        seen = {}
-        for step in range(40_000_000):
-            if step and step % 1_000_000 == 0:
-                print(step)
-            val_a = val_a * FACTOR_A % MOD
-            val_b = val_b * FACTOR_B % MOD
-            if (val_a & MASK) == (val_b & MASK):
-                count += 1
-
-        print("\n".join(["==="] + [f"{k}={v!r}" for k, v in locals().items()] + ["==="]))
-        return count
-
-    def part2(self, parsed_input: InputType) -> int:
-        (val_a,), (val_b,) = parsed_input
+        part_two = param
+        part_one = not part_two
 
         def gen(value, factor, mask):
             while True:
                 value = value * factor % MOD
-                if value & mask == 0:
+                if part_one or value & mask == 0:
                     yield value & MASK
 
-        count = 0
         gen_a = gen(val_a, FACTOR_A, 4 - 1)
         gen_b = gen(val_b, FACTOR_B, 8 - 1)
-        for step in range(5_000_000):
-            if next(gen_a) == next(gen_b):
-                count += 1
 
-        print("\n".join(["==="] + [f"{k}={v!r}" for k, v in locals().items()] + ["==="]))
-        return count
-
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
+        return sum(
+            1
+            for step in range(40_000_000 if part_one else 5_000_000)
+            if next(gen_a) == next(gen_b)
+        )
 
 # vim:expandtab:sw=4:ts=4
