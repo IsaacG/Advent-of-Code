86ea244d9394e9ccd5ae57659f1107e3ffb96411
Isaac Good <github@isaacgood.com>
2024-08-10 09:15:21 -0700

    Solve 2017/16 (rough)

--- 2017/d15.py
+++ 2017/d15.py
@@ -2,6 +2,7 @@
 """Advent of Code, Day 15: Dueling Generators."""
 
 import more_itertools
+import itertools
 from lib import aoc
 
 FACTOR_A = 16807
@@ -22,6 +23,11 @@ class Day15(aoc.Challenge):
     INPUT_PARSER = aoc.parse_ints_per_line
 
     def solver(self, parsed_input: list[list[int]], param: bool) -> int:
+        """Count the number of generated pairs that have matching ends.
+
+        NUC + cpython: 60s
+        Desktop + pypy: 3s
+        """
         ((val_a,), (val_b,)), part_one = parsed_input, param
 
         def gen(value, factor, mask):
@@ -33,7 +39,7 @@ class Day15(aoc.Challenge):
         gen_a = gen(val_a, FACTOR_A, 4 - 1)
         gen_b = gen(val_b, FACTOR_B, 8 - 1)
         steps = 40_000_000 if part_one else 5_000_000
-        pairs = more_itertools.take(steps, zip(gen_a, gen_b))
-        return sum(1 for val_a, val_b in pairs if val_a == val_b)
+        pairs = itertools.islice(zip(gen_a, gen_b), steps)
+        return sum(for val_a, val_b in pairs if val_a == val_b)
 
 # vim:expandtab:sw=4:ts=4
