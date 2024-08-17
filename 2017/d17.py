0bab14ecd9fbf8c8a54c25db9a4800c0c7269ded
Isaac Good <github@isaacgood.com>
2024-08-17 14:01:37 -0700

    2017/17 tidy

--- 2017/d17.py
+++ 2017/d17.py
@@ -1,51 +1,31 @@
 #!/bin/python
 """Advent of Code, Day 17: Spinlock."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import math
-import re
-
 from lib import aoc
-
-SAMPLE = ["3"
-]
-
-LineType = int
-InputType = list[LineType]
 
 
 class Day17(aoc.Challenge):
     """Day 17: Spinlock."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-    # PARAMETERIZED_INPUTS = [False, True]
-
     TESTS = [
-        aoc.TestCase(part=1, inputs=SAMPLE[0], want=638),
-        aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
+        aoc.TestCase(part=1, inputs="3", want=638),
+        aoc.TestCase(part=2, inputs="3", want=aoc.TEST_SKIP),
     ]
-
     INPUT_PARSER = aoc.parse_one_int
 
-    def part1(self, parsed_input: InputType) -> int:
+    def part1(self, parsed_input: int) -> int:
+        """Step and insert 2017 times then return the next value."""
         step = parsed_input
         data = [0]
         pos = 0
         for i in range(1, 2017 + 1):
             pos = (pos + step) % i + 1
-            data.insert((pos + 1) % (i + 1), i)
-            pos += 1
+            data.insert(pos % (i + 1), i)
 
-        return data[(pos + 1) % 2018]
+        return data[(pos + 1) % len(data)]
 
-    def part2(self, parsed_input: InputType) -> int:
+    def part2(self, parsed_input: int) -> int:
+        """Return the value after 0 after 50M step-inserts."""
         step = parsed_input
-        data = [0]
         pos = 0
         last_insert = 0
         for i in range(1, 50000000 + 1):
@@ -54,35 +34,5 @@ class Day17(aoc.Challenge):
                 last_insert = i
 
         return last_insert
-
-    def solver(self, parsed_input: InputType, param: bool) -> int | str:
-        raise NotImplementedError
-
-    def input_parser(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
-        return super().input_parser(puzzle_input)
-        return puzzle_input.splitlines()
-        return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
-        # Words: mixed str and int
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in line.split()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-        # Regex splitting
-        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in patt.match(line).groups()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-
 
 # vim:expandtab:sw=4:ts=4
