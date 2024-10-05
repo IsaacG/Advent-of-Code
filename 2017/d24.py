3719b1f5f8208f93a3b7c8a228e2e93435318bc6
Isaac Good <github@isaacgood.com>
2024-10-05 13:28:29 -0700

    2017/24: cache result between parts

--- 2017/d24.py
+++ 2017/d24.py
@@ -1,6 +1,7 @@
 #!/bin/python
 """Advent of Code, Day 24: Electromagnetic Moat."""
 
+import functools
 from lib import aoc
 
 SAMPLE = """\
@@ -22,30 +23,31 @@ class Day24(aoc.Challenge):
         aoc.TestCase(part=2, inputs=SAMPLE, want=19),
     ]
 
+    @functools.cache
+    def strongest(self, start: int, options: frozenset[tuple[int, int]]) -> tuple[int, int, int]:
+        strength = 0
+        longest_length = 0
+        longest_strength = 0
+
+        for option in options:
+            if start not in option:
+                continue
+            cur = option[0] + option[1]
+            next_start = cur - start
+            n_str, n_len, n_long_str = self.strongest(next_start, options - {option})
+            strength = max(strength, n_str + cur)
+            n_long_str += cur
+            if n_len > longest_length:
+                longest_length = n_len
+                longest_strength = n_long_str
+            elif n_len == longest_length and n_long_str > longest_strength:
+                longest_strength = n_long_str
+        return strength, longest_length + 1, longest_strength
+
     def solver(self, puzzle_input: frozenset[tuple[int, int]], part_one: bool) -> int:
         """Compute the strongest (longest) magnetic bridge which can be built."""
 
-        def strongest(start: int, options: frozenset[tuple[int, int]]) -> tuple[int, int, int]:
-            strength = 0
-            longest_length = 0
-            longest_strength = 0
-
-            for option in options:
-                if start not in option:
-                    continue
-                cur = option[0] + option[1]
-                next_start = cur - start
-                n_str, n_len, n_long_str = strongest(next_start, options - {option})
-                strength = max(strength, n_str + cur)
-                n_long_str += cur
-                if n_len > longest_length:
-                    longest_length = n_len
-                    longest_strength = n_long_str
-                elif n_len == longest_length and n_long_str > longest_strength:
-                    longest_strength = n_long_str
-            return strength, longest_length + 1, longest_strength
-
-        return strongest(0, puzzle_input)[0 if part_one else 2]
+        return self.strongest(0, puzzle_input)[0 if part_one else 2]
 
     def input_parser(self, puzzle_input: str) -> frozenset[tuple[int, int]]:
         """Parse the input data."""
