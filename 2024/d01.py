9438ff622f5f5e73e35b03c97ebf22c356ad291c
Isaac Good <github@isaacgood.com>
2024-11-30 21:48:29 -0800

    Tidy 2024/01

--- 2024/d01.py
+++ 2024/d01.py
@@ -1,99 +1,36 @@
 #!/bin/python
 """Advent of Code, Day 1: Historian Hysteria."""
-from __future__ import annotations
 
 import collections
-import functools
-import itertools
-import math
-import re
-
 from lib import aoc
 
-SAMPLE = [
-    """\
+SAMPLE = """\
 3   4
 4   3
 2   5
 1   3
 3   9
-3   3""",  # 0
-]
-
-LineType = int
-InputType = list[LineType]
+3   3"""
 
 
 class Day01(aoc.Challenge):
     """Day 1: Historian Hysteria."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
-        aoc.TestCase(part=1, inputs=SAMPLE[0], want=11),
-        aoc.TestCase(part=2, inputs=SAMPLE[0], want=31),
-        # aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
+        aoc.TestCase(part=1, inputs=SAMPLE, want=11),
+        aoc.TestCase(part=2, inputs=SAMPLE, want=31),
     ]
 
-    # INPUT_PARSER = aoc.parse_one_str
-    # INPUT_PARSER = aoc.parse_one_str_per_line
-    # INPUT_PARSER = aoc.parse_multi_str_per_line
-    # INPUT_PARSER = aoc.parse_one_int
-    # INPUT_PARSER = aoc.parse_one_int_per_line
-    # INPUT_PARSER = aoc.parse_ints_one_line
-    # INPUT_PARSER = aoc.parse_ints_per_line
-    # INPUT_PARSER = aoc.parse_re_group_str(r"(a) .* (b) .* (c)")
-    # INPUT_PARSER = aoc.parse_re_findall_str(r"(a|b|c)")
-    # INPUT_PARSER = aoc.parse_multi_mixed_per_line
-    # INPUT_PARSER = aoc.parse_re_group_mixed(r"(foo) .* (\d+)")
-    # INPUT_PARSER = aoc.parse_re_findall_mixed(r"\d+|foo|bar")
-    # INPUT_PARSER = aoc.ParseBlocks([aoc.parse_one_str_per_line, aoc.parse_re_findall_int(r"\d+")])
-    # INPUT_PARSER = aoc.ParseOneWord(aoc.Board.from_int_block)
-    # INPUT_PARSER = aoc.AsciiBoolMapParser("#")
-    # ---
-    # INPUT_PARSER = aoc.CharCoordinatesParser("S.#")
-    # (width, height), start, garden, rocks = puzzle_input
-    # max_x, max_y = width - 1, height - 1
-
-    def part1(self, puzzle_input: InputType) -> int:
-        a = [l[0] for l in puzzle_input]
-        b = [l[1] for l in puzzle_input]
-        a.sort()
-        b.sort()
+    def part1(self, puzzle_input: list[list[int]]) -> int:
+        """Return the sum of differences between the sorted lists."""
+        a = sorted(l[0] for l in puzzle_input)
+        b = sorted(l[1] for l in puzzle_input)
         return sum(abs(i - j) for i, j in zip(a, b))
 
-    def part2(self, puzzle_input: InputType) -> int:
+    def part2(self, puzzle_input: list[list[int]]) -> int:
+        """Return the sum of items times counts."""
         a = [l[0] for l in puzzle_input]
-        b = [l[1] for l in puzzle_input]
-        return sum(i * b.count(i) for i in a)
-
-
-    def input_parser(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
-        return super().input_parser(puzzle_input)
-        return puzzle_input.splitlines()
-        return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
-        # Words: mixed str and int
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in line.split()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-        # Regex splitting
-        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in patt.match(line).groups()
-            )
-            for line in puzzle_input.splitlines()
-        ]
+        b = collections.Counter(l[1] for l in puzzle_input)
+        return sum(i * b[i] for i in a)
 
 # vim:expandtab:sw=4:ts=4
