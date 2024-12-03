1fa1d5931c41d503a3f7c45f16d5bd59c4da45f3
Isaac Good <github@isaacgood.com>
2024-12-02 21:40:54 -0800

    Tidy python/2024/03

--- 2024/d03.py
+++ 2024/d03.py
@@ -9,6 +9,7 @@ SAMPLE = [
     "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
 ]
 
+
 class Day03(aoc.Challenge):
     """Day 3: Mull It Over."""
 
@@ -18,17 +19,15 @@ class Day03(aoc.Challenge):
     ]
     INPUT_PARSER = aoc.parse_one_str
 
-    def solver(self, puzzle_input: InputType, part_one: bool) -> int | str:
+    def solver(self, puzzle_input: str, part_one: bool) -> int:
         """Return the sum of all the multiplications."""
+        puzzle_input = puzzle_input.replace("\n", " ")
         if not part_one:
-            # Account for do() and don't() instructions.
-            parts = puzzle_input.split("don't()")
-            t = [p for p in parts[1:] if "do()" in p]
-            t = [p.split("do()", 1)[1] for p in t]
-            puzzle_input = parts[0] + "".join(t)
+            # Remove any don't()-do() pairs and any trailing don't().
+            puzzle_input = re.sub(r"don't\(\).*?(do\(\)|$)", "", puzzle_input)
         return sum(
             int(a) * int(b)
-            for a, b in re.findall(r"mul\((\d+),(\d+)\)", puzzle_input)
+            for a, b in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", puzzle_input)
         )
 
 # vim:expandtab:sw=4:ts=4
