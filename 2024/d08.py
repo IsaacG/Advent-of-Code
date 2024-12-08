b334eae18a591804156d7cd258e33a2369bb3079
Isaac Good <github@isaacgood.com>
2024-12-07 22:10:40 -0800

    Tidy python/2024/08

--- 2024/d08.py
+++ 2024/d08.py
@@ -1,13 +1,7 @@
 #!/bin/python
 """Advent of Code, Title."""
-from __future__ import annotations
 
-import collections
-import functools
 import itertools
-import math
-import re
-
 from lib import aoc
 
 SAMPLE = """\
@@ -24,73 +18,37 @@ SAMPLE = """\
 ............
 ............"""
 
-LineType = int
-InputType = list[LineType]
-
 
 class Day08(aoc.Challenge):
     """Title."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
         aoc.TestCase(part=1, inputs=SAMPLE, want=14),
         aoc.TestCase(part=2, inputs=SAMPLE, want=34),
-        # aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
     ]
+    INPUT_PARSER = aoc.CharCoordParser()
 
-    def part1(self, puzzle_input: InputType) -> int:
-        size = puzzle_input[0]
+    def solver(self, puzzle_input: dict[str, set[complex]], part_one: bool) -> int:
+        all_locations = set().union(*puzzle_input.values())
         antinodes = set()
-        all_locations = set().union(*puzzle_input[1].values())
-        for freq, coords in puzzle_input[1].items():
-            if freq == ".":
-                continue
-            f_nodes = set()
-            for a, b in itertools.combinations(coords, 2):
-                f_nodes.add(b + (b - a))
-                f_nodes.add(a + (a - b))
-            f_nodes &= all_locations
-            antinodes |= f_nodes
-
-        # aoc.print_point_set(antinodes)
-        return len(antinodes)
-
-    def part2(self, puzzle_input: InputType) -> int:
-        antinodes = set()
-        all_locations = set().union(*puzzle_input[1].values())
-        for freq, coords in puzzle_input[1].items():
+        for freq, coords in puzzle_input.items():
             if freq == ".":
                 continue
             for a, b in itertools.combinations(coords, 2):
-                delta = a - b
-                for i in itertools.count():
-                    if a + i * delta in all_locations:
-                        antinodes.add(a + i * delta)
-                    else:
-                        break
+                delta = b - a
 
-                for i in itertools.count():
-                    if a - i * delta in all_locations:
-                        antinodes.add(a - i * delta)
-                    else:
-                        break
+                if part_one:
+                    sequences = ([2], [-1])
+                else:
+                    sequences = (itertools.count(), itertools.count(step=-1))
 
-        # aoc.print_point_set(antinodes)
+                for steps in sequences:
+                    for i in steps:
+                        if (node := a + i * delta) in all_locations:
+                            antinodes.add(node)
+                        else:
+                            break
+
         return len(antinodes)
-
-    # def solver(self, puzzle_input: InputType, part_one: bool) -> int | str:
-
-    def input_parser(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
-        lines = puzzle_input.splitlines()
-        size = (len(lines[0]), len(lines))
-        data = collections.defaultdict(set)
-        for y, line in enumerate(lines):
-            for x, char in enumerate(line):
-                data[char].add(complex(x, y))
-        return size, dict(data)
 
 # vim:expandtab:sw=4:ts=4
