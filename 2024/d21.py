b5574a192ec55364fc3adc48da2f0b95a4d39ac6
Isaac Good <github@isaacgood.com>
2024-12-21 00:01:01 -0800

    python/2024/21 freq map initial attempt

--- 2024/d21.py
+++ 2024/d21.py
@@ -32,10 +32,10 @@ class Day21(aoc.Challenge):
 
     TESTS = [
         aoc.TestCase(part=1, inputs=SAMPLE[0], want=126384),
-        aoc.TestCase(part=2, inputs=SAMPLE[0], want=None),
+        aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
     ]
 
-    def part1(self, puzzle_input: InputType) -> int:
+    def solver(self, puzzle_input: InputType, part_one: bool) -> int:
         NUMPAD_LAYOUT = "789\n456\n123\nX0A"
         ARRPAD_LAYOUT = "X^A\n<v>"
         NUMPAD = {
@@ -48,13 +48,12 @@ class Day21(aoc.Challenge):
             for y, line in enumerate(ARRPAD_LAYOUT.splitlines())
             for x, char in enumerate(line)
         }
+        X_ARR = ARRPAD["X"]
+        X_DIG = NUMPAD["X"]
 
-        regular = True
-
-        def pushpad(pad, src, dst):
-            nonlocal regular
-            p_src = pad[src]
-            offset = pad[dst] - p_src
+        def digipad(src, dst):
+            p_src = NUMPAD[src]
+            offset = NUMPAD[dst] - p_src
             parts = []
             if offset.real > 0:
                 parts.append( ">" * int(offset.real))
@@ -64,79 +63,89 @@ class Day21(aoc.Challenge):
                 parts.append( "v" * int(offset.imag))
             else:
                 parts.append( "^" * -int(offset.imag))
-            if regular:
-                if p_src + complex(offset.real, 0) == pad["X"]:
-                    regular = not regular
-                    return parts[1] + parts[0] + "A"
-                else:
-                    return parts[0] + parts[1] + "A"
+
+            options = []
+            if p_src + complex(offset.real, 0) != X_DIG:
+                options.append(parts[0] + parts[1] + "A")
+            if p_src + complex(0, offset.imag) != X_DIG:
+                options.append( parts[1] + parts[0] + "A")
+            return options
+
+        def arrpad(src, dst):
+            p_src = ARRPAD[src]
+            offset = ARRPAD[dst] - p_src
+            parts = []
+            if offset.real > 0:
+                parts.append( ">" * int(offset.real))
             else:
-                if p_src + complex(0, offset.imag) == pad["X"]:
-                    regular = not regular
-                    return parts[0] + parts[1] + "A"
-                else:
-                    return parts[1] + parts[0] + "A"
+                parts.append( "<" * -int(offset.real))
+            if offset.imag > 0:
+                parts.append( "v" * int(offset.imag))
+            else:
+                parts.append( "^" * -int(offset.imag))
+
+            options = set()
+            if p_src + complex(offset.real, 0) != X_ARR:
+                options.add(parts[0] + parts[1])
+            if p_src + complex(0, offset.imag) != X_ARR:
+                options.add( parts[1] + parts[0])
+            return options
+
+        def arrseq(seq, count):
+            v = [arrpad(src, dst) for src, dst in zip("A" + seq, seq + "A")]
+            counts = [collections.Counter(i) for i in itertools.product(*v)]
+            return [{k: v * count for k, v in c.items()} for c in counts]
 
         total = 0
-        if False:
-            for line in puzzle_input:
-                options = {""}
-                for src, dst in zip("A" + line, line):
-                    n = set()
-                    for opt_a in pushpad(NUMPAD, src, dst):
-                        for opt_b in options:
-                            n.add(opt_a + opt_b)
-                    options = n
+        for line in puzzle_input:
+            options = {""}
+            for src, dst in zip("A" + line, line):
+                n = set()
+                for opt_a in digipad(src, dst):
+                    for opt_b in options:
+                        n.add(opt_a + opt_b)
+                options = n
+            outputs = options
+            outputs = [
+                collections.Counter(o.rstrip("A").split("A"))
+                for o in outputs
+            ]
+            if False:
+                outputs = []
+                for option in options:
+                    segments = collections.Counter(option.split("A"))
+                    opt = [({segment}, count) for segment, count in segments.items()]
+                    outputs.append(opt)
 
-                inputs = options
-                outputs = set()
-                for one_input in inputs:
-                    options = {"",}
-                    for src, dst in zip("A" + one_input, one_input):
-                        n = set()
-                        for opt_a in pushpad(ARRPAD, src, dst):
-                            for opt_b in options:
-                                n.add(opt_a + opt_b)
-                        options = n
-                    outputs.update(options)
-
+            for _ in range(2 if part_one else 25):
                 inputs = outputs
-                outputs = set()
+                outputs = []
+                # print(line, inputs)
                 for one_input in inputs:
-                    options = {""}
-                    for src, dst in zip("A" + one_input, one_input):
-                        n = set()
-                        for opt_a in pushpad(ARRPAD, src, dst):
-                            for opt_b in options:
-                                n.add(opt_a + opt_b)
-                        options = n
-                    outputs.update(options)
+                    # print(line, one_input)
+                    ways = [arrseq(seq, count) for seq, count in one_input.items()]
+                    # print("ways", ways)
+                    options = list(itertools.product(*ways))
+                    for opt in options:
+                        coll = collections.defaultdict(int)
+                        for group in opt:
+                            for k, v in group.items():
+                                coll[k] += v
+                        outputs.append(coll)
 
-                size = min(len(i) for i in outputs)
-                complexity = size * int(line.removesuffix("A"))
-                print(f"{line}: complexity = {size} * {int(line.removesuffix("A"))} = {complexity}")
-                total += complexity
+            # print(outputs[0])
+            costs = [sum((len(k) + 1) * v for k, v in output.items()) for output in outputs]
+            size = min(costs)
+            complexity = size * int(line.removesuffix("A"))
+            # print(f"{line}: complexity = {size} * {int(line.removesuffix("A"))} = {complexity}")
+            total += complexity
 
-
-
-        if True:
-            for line in puzzle_input:
-                regular = True
-                pushes = "".join(pushpad(NUMPAD, src, dst) for src, dst in zip("A" + line, line))
-                pushes = "".join(pushpad(ARRPAD, src, dst) for src, dst in zip("A" + pushes, pushes))
-                pushes = "".join(pushpad(ARRPAD, src, dst) for src, dst in zip("A" + pushes, pushes))
-                complexity = len(pushes) * int(line.removesuffix("A"))
-                print(f"{line}: complexity = {len(pushes)} * {int(line.removesuffix("A"))} = {complexity}")
-                total += complexity
-                        
-
-        if not self.testing:
+        if not self.testing and part_one:
             assert total != 155462
             assert total < 157942
         return total
 
-    # def part2(self, puzzle_input: InputType) -> int:
 
-    # def solver(self, puzzle_input: InputType, part_one: bool) -> int | str:
+
 
 # vim:expandtab:sw=4:ts=4
