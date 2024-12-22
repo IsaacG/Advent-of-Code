c7304703d82374aec1105fab4bfa41a05ae6aba8
Isaac Good <github@isaacgood.com>
2024-12-22 00:15:39 -0800

    Tidy python/2024/22

--- 2024/d22.py
+++ 2024/d22.py
@@ -1,13 +1,14 @@
 #!/bin/python
 """Advent of Code, Day 22: Monkey Market."""
 
+import collections
 from lib import aoc
 
 SAMPLE = [
     "1 10 100 2024".replace(" ", "\n"),
     "1 2 3 2024".replace(" ", "\n"),
 ]
-MOD = 16777216 
+MOD = 16777216
 
 
 class Day22(aoc.Challenge):
@@ -18,7 +19,8 @@ class Day22(aoc.Challenge):
         aoc.TestCase(part=2, inputs=SAMPLE[1], want=23),
     ]
 
-    def pseudo_randon(self, number: int) -> int:
+    @staticmethod
+    def pseudo_randon(number: int) -> int:
         """Generate the next pseudo random number."""
         number = (number ^ (number << 6)) % MOD   # i <<  6 == i  *   64
         number = (number ^ (number >> 5)) % MOD   # i >>  5 == i //   32
@@ -39,34 +41,43 @@ class Day22(aoc.Challenge):
 
         def delta_values():
             """Generate the deltas between the last digit of 2000 prices."""
-            sequences = set()
+            patterns_seen = set()
             delta_collections = []
             for number in puzzle_input:
-                sequence = [number % 10]
-                for _ in range(2000):
-                    number = self.pseudo_randon(number)
-                    sequence.append(number % 10)
-                coll = {}
-                for i in range(len(sequence) - 4):
-                    nums = sequence[i:i + 5]
-                    deltas = [b - a + 9 for a, b in zip(nums, nums[1:])]
-                    fp = deltas[0] + deltas[1] * 100 + deltas[2] * 10000 + deltas[3] * 1000000
-                    sequences.add(fp)
-                    if fp not in coll:
-                        coll[fp] = nums[-1]
-                delta_collections.append(coll)
-            return delta_collections, sequences
+                deltas = collections.deque(maxlen=4)
+                # Compute the first three deltas.
+                for _ in range(3):
+                    next_num = self.pseudo_randon(number)
+                    deltas.append((next_num % 10) - (number % 10))
+                    number = next_num
+                prices_per_pattern = {}
+                # Load the next number, compute the delta. Store the price for the run of the last 5 prices.
+                for _ in range(1997):
+                    next_num = self.pseudo_randon(number)
+                    deltas.append((next_num % 10) - (number % 10))
+                    number = next_num
 
-        sequence_prices, sequences = delta_values()
+                    # Create a pattern based on four deltas.
+                    pattern = 0
+                    for delta in deltas:
+                        pattern = pattern * 100 + delta
+
+                    patterns_seen.add(pattern)
+                    if pattern not in prices_per_pattern:
+                        prices_per_pattern[pattern] = number % 10
+                delta_collections.append(prices_per_pattern)
+            return delta_collections, patterns_seen
+
+        sequence_prices, patterns_seen = delta_values()
 
         if self.testing:
             deltas = [-2, 1, -1, 3]
-            fp = deltas[0] + deltas[1] * 100 + deltas[2] * 10000 + deltas[3] * 1000000
-            return sum(prices.get(fp, 0) for prices in sequence_prices)
+            pattern = deltas[0] * 1000000 + deltas[1] * 10000 + deltas[2] * 100 + deltas[3] * 1
+            return sum(prices.get(pattern, 0) for prices in sequence_prices)
 
         return max(
-            sum(prices.get(fp, 0) for prices in sequence_prices)
-            for fp in sequences
+            sum(prices.get(pattern, 0) for prices in sequence_prices)
+            for pattern in patterns_seen
         )
 
 # vim:expandtab:sw=4:ts=4
