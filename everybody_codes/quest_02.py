9654334bf329a237e8f76349cab4a0f2a5e19f8a
Isaac Good <github@isaacgood.com>
2024-12-27 08:52:43 -0800

    Everybody Codes: py/2024/02 unnest parts 2/3

--- everybody_codes/quest_02.py
+++ everybody_codes/quest_02.py
@@ -6,29 +6,28 @@ def solve(part: int, data: str) -> int:
     if part == 1:
         words = chunks[0].split(":", 1)[1].split(",")
         return sum(len(re.findall(word, chunks[1])) for word in words)
-    else:
-        words = chunks[0].split(":", 1)[1].split(",")
-        chars = {complex(x, y): char for y, line in enumerate(lines) for x, char in enumerate(line)}
-        width = len(lines[0])
+    words = chunks[0].split(":", 1)[1].split(",")
+    chars = {complex(x, y): char for y, line in enumerate(lines) for x, char in enumerate(line)}
+    width = len(lines[0])
+    if part == 2:
+        width *= 2  # disable wrapping by making the board wider than it actually is.
+    directions = [complex(1), complex(-1), complex(0, 1), complex(0, -1)]
+    included = set()
+    for pos in chars:
+        x = int(pos.real)
+        y = complex(0, pos.imag)
+        for direction in [1, -1]:
+            for word in words:
+                if all(chars.get(y + ((x + direction * offset) % width)) == char for offset, char in enumerate(word)):
+                    included.update(y + ((x + direction * offset) % width) for offset in range(len(word)))
         if part == 2:
-            width *= 2  # disable wrapping by making the board wider than it actually is.
-        directions = [complex(1), complex(-1), complex(0, 1), complex(0, -1)]
-        included = set()
-        for pos in chars:
-            x = int(pos.real)
-            y = complex(0, pos.imag)
-            for direction in [1, -1]:
-                for word in words:
-                    if all(chars.get(y + ((x + direction * offset) % width)) == char for offset, char in enumerate(word)):
-                        included.update(y + ((x + direction * offset) % width) for offset in range(len(word)))
-            if part == 2:
-                # No vertical checks for part 2.
-                continue
-            for direction in [1j, -1j]:
-                for word in words:
-                    if all(chars.get(pos + direction * offset) == char for offset, char in enumerate(word)):
-                        included.update(pos + direction * offset for offset in range(len(word)))
-        return len(included)
+            # No vertical checks for part 2.
+            continue
+        for direction in [1j, -1j]:
+            for word in words:
+                if all(chars.get(pos + direction * offset) == char for offset, char in enumerate(word)):
+                    included.update(pos + direction * offset for offset in range(len(word)))
+    return len(included)
 
 
 TEST_DATA = [
