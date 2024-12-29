e41fc7a8c586c8e1724843128b3f76a6b2ed2397
Isaac Good <github@isaacgood.com>
2024-12-28 18:50:42 -0800

    Everybody Codes: check in a template file

---
+++ everybody_codes/tmpl.py
@@ -0,0 +1,11 @@
+def solve(part: int, data: str) -> int:
+    pass
+
+
+TEST_DATA = [
+]
+TESTS = [
+    # (1, TEST_DATA[0], None),
+    # (2, TEST_DATA[1], None),
+    # (3, TEST_DATA[2], None),
+]
