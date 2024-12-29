531d6ba8dd0189f21c76a61a9d32fd0b17b2907a
Isaac Good <github@isaacgood.com>
2024-12-28 18:50:21 -0800

    Everybody Codes: tidy py/2024/07

--- everybody_codes/quest_07.py
+++ everybody_codes/quest_07.py
@@ -1,3 +1,4 @@
+import collections.abc
 import itertools
 
 
@@ -32,7 +33,8 @@ S+= +=-== +=++=     =+=+=--=    =-= ++=     +=-  =+=++=-+==+ =++=-=-=--
 
 
 def track(diagram: str) -> list[str]:
-    rotations = [1j, -1j]
+    """Parse a track into a list of actions."""
+    # Convert the track to a coord map.
     chars = {}
     for y, line in enumerate(diagram.splitlines()):
         for x, char in enumerate(line):
@@ -41,92 +43,65 @@ def track(diagram: str) -> list[str]:
             chars[complex(x, y)] = char
             if char == "S":
                 pos = complex(x, y)
+    # Walk the track from S to S.
+    rotations = [1j, -1j]
     direction = complex(1, 0)
     track_actions = []
     while True:
         if pos + direction not in chars:
-            for rotate in rotations:
-                if pos + direction * rotate in chars:
-                    direction *= rotate
-                    break
-            else:
-                raise RuntimeError()
+            direction *= next(
+                rotate
+                for rotate in rotations
+                if pos + direction * rotate in chars
+            )
         pos += direction
         track_actions.append(chars[pos])
         if chars[pos] == "S":
             return track_actions
 
 
+def race(actions: collections.abc.Iterable[str]) -> int:
+    """Return the power collected by executing a sequence of actions."""
+    collected = 0
+    power = 10
+    for action in actions:
+        if action == "+":
+            power += 1
+        elif action == "-" and power:
+            power -= 1
+        collected += power
+    return collected
+
+
+def combine_actions(actions: collections.abc.Iterable[tuple[str, str]]) -> collections.abc.Iterable[str]:
+    """Combine track and device actions."""
+    for track_action, device_action in actions:
+        yield track_action if track_action in "+-" else device_action
+
+
 def solve(part: int, data: str, testing: bool) -> int:
-    collects = {}
-    if part == 1:
+    if part in [1, 2]:
+        if part == 1:
+            racer = lambda x: race(itertools.islice(itertools.cycle(x.split(",")), 10))
+        else:
+            track_actions = track(TRACK2[testing])
+            racer = lambda x: race(combine_actions(zip(track_actions * 10, itertools.cycle(actions.split(",")))))
+        collects = {}
         for line in data.splitlines():
             name, actions = line.split(":")
-            collected = 0
-            power = 10
-            for _, action in zip(range(10), itertools.cycle(actions.split(","))):
-                if action == "+":
-                    power += 1
-                elif action == "-" and power:
-                    power -= 1
-                collected += power
-            collects[name] = collected
-        return "".join(sorted(collects, key=lambda x: collects[x], reverse=True))
-    if part == 2:
-        track_actions = track(TRACK2[testing])
-        for line in data.splitlines():
-            name, actions = line.split(":")
-            collected = 0
-            power = 10
-            for track_action, device_action in zip(track_actions * 10, itertools.cycle(actions.split(","))):
-                if track_action in "+-":
-                    action = track_action
-                else:
-                    action = device_action
-                if action == "+":
-                    power += 1
-                elif action == "-" and power:
-                    power -= 1
-                collected += power
-            collects[name] = collected
+            collects[name] = racer(actions)
         return "".join(sorted(collects, key=lambda x: collects[x], reverse=True))
     if part == 3:
-        track_actions = track(TRACK3) * 2024
-        for line in data.splitlines():
-            name, actions = line.split(":")
-            collected = 0
-            power = 10
-            for track_action, device_action in zip(track_actions, itertools.cycle(actions.split(","))):
-                if track_action in "+-":
-                    action = track_action
-                else:
-                    action = device_action
-                if action == "+":
-                    power += 1
-                elif action == "-" and power:
-                    power -= 1
-                collected += power
-            collects[name] = collected
-        assert set(collects) == {"A"}
-        threshold = collects["A"]
-        options = set(itertools.permutations("+" * 5 + "-=" * 3))
-        count = 0
-        for actions in options:
-            collected = 0
-            power = 10
-            for track_action, device_action in zip(track_actions, itertools.cycle(actions)):
-                if track_action in "+-":
-                    action = track_action
-                else:
-                    action = device_action
-                if action == "+":
-                    power += 1
-                elif action == "-" and power:
-                    power -= 1
-                collected += power
-            if collected > threshold:
-                count += 1
-        return count
+        # 2024 == 184 * 11
+        # Running the track 2024 times would give 184x the result of 11 loops.
+        # When testing patterns, 11 loops is sufficient.
+        track_actions = track(TRACK3) * 11
+        racer = lambda x: race(combine_actions(zip(track_actions, itertools.cycle(x))))
+        threshold = racer(data.split(":")[1].split(","))
+        return sum(
+            racer(actions) > threshold
+            for actions in set(itertools.permutations("+" * 5 + "-=" * 3))
+        )
 
 
 TEST_DATA = [
