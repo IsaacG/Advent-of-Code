eda4e82e874e8e30c0d0c81b7bbf875733c7127a
Isaac Good <github@isaacgood.com>
2025-01-06 20:04:16 -0800

    Everybody Codes: tidy py/2024/14

--- everybody_codes/quest_14.py
+++ everybody_codes/quest_14.py
@@ -1,6 +1,6 @@
 """Everyone Codes Day N."""
 import queue
-
+import typing
 
 OFFSET = {
     "U": ( 0, +1,  0),
@@ -12,8 +12,9 @@ OFFSET = {
 }
 
 
-def solve(part: int, data: str, testing) -> int:
+def solve(part: int, data: str) -> int:
     """Solve the parts."""
+    # Track various pieces of growth info.
     all_pos = set()
     leaves = set()
     height = 0
@@ -21,41 +22,42 @@ def solve(part: int, data: str, testing) -> int:
         position = (0, 0, 0)
         for step in line.split(","):
             direction, amount = step[0], step[1:]
-            for step in range(int(amount)):
-                position = tuple(pos + offset for pos, offset in zip(position, OFFSET[direction]))
+            for _ in range(int(amount)):
+                position = tuple(pos + offset for pos, offset in zip(position, OFFSET[direction]))  # type: ignore
                 all_pos.add(position)
             height = max(height, position[1])
         leaves.add(position)
-        
+
     if part == 1:
         return height
     if part == 2:
         return len(all_pos)
 
+    # All possible trunk heights and the count.
     trunks = {pos[1] for pos in all_pos if pos[0] == pos[2] == 0}
     num_trunks = len(trunks)
 
-    def trunk_distance(leaf) -> dict:
-        todo = queue.PriorityQueue()
+    def trunk_distance(leaf: tuple[int, int, int]) -> dict[int, int]:
+        """Return the distance of a leaf to all parts of the main trunk."""
+        todo: queue.PriorityQueue[tuple[int, int, tuple[int, int, int]]] = queue.PriorityQueue()
         todo.put((sum(abs(i) for i in leaf), 0, leaf))
         seen = set()
-        distances = {}
+        distances: dict[int, int] = {}
         while len(distances) < num_trunks:
             _, steps, pos = todo.get()
             if pos[0] == pos[2] == 0:
                 distances[pos[1]] = steps
+
             steps = steps + 1
             for offset in OFFSET.values():
-                n = tuple(i + j for i, j in zip(pos, offset))
+                n = typing.cast(tuple[int, int, int], tuple(i + j for i, j in zip(pos, offset)))
                 if n not in all_pos or n in seen:
                     continue
                 seen.add(n)
                 todo.put((sum(abs(i) for i in n) + steps, steps, n))
         return distances
 
-    def murkiness(pos: tuple[int, int, int]) -> int:
-        return sum(trunk_distance(leaf, pos) for leaf in leaves)
-
+    # Collect the distance from all leaves to all parts of the trunk.
     distances = {leaf: trunk_distance(leaf) for leaf in leaves}
     return min(
         sum(distances[leaf][trunk_height] for leaf in leaves)
