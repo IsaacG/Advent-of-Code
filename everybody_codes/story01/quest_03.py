98af4052c87f2847061848e3e1d27e0f048d2efd
Isaac Good <github@isaacgood.com>
2025-06-29 16:06:25 -0700

    Tidy EveryBody Codes Story 1 Quest 3

--- everybody_codes/story01/quest_03.py
+++ everybody_codes/story01/quest_03.py
@@ -1,36 +1,36 @@
 """Everyone Codes Day N."""
 
-import logging
 import math
 import re
 
-log = logging.info
 
 def solve(part: int, data: str) -> int:
     """Solve the parts."""
-    steps = 100
-    
+    snails = []
+    for line in data.splitlines():
+        match = re.match(r"x=(.*) y=(.*)", line)
+        assert match
+        x, y = [int(i) for i in match.groups()]
+        period = x + y - 1
+        snails.append((x, y, period))
+
     if part == 1:
         total = 0
-        for line in data.splitlines():
-            x, y = [int(i) for i in re.match(r"x=(.*) y=(.*)", line).groups()]
-            period = x + y - 1
+        steps = 100
+        for x, y, period in snails:
             final_x = (x + steps - 1) % period + 1
             final_y = (y - steps - 1) % period + 1
             total += final_x + 100 * final_y
         return total
 
-    if part == 2 or part == 3:
-        step = 1
-        offset = 0
-        for line in data.splitlines():
-            x, y = [int(i) for i in re.match(r"x=(.*) y=(.*)", line).groups()]
-            period = x + y - 1
-            while (y - offset - 1) % period != 0:
-                offset += step
-            step = math.lcm(step, period)
+    combined_period = 1
+    days = 0
+    for x, y, period in snails:
+        while (y - days - 1) % period != 0:
+            days += combined_period
+        combined_period = math.lcm(combined_period, period)
 
-        return offset
+    return days
 
 
 TEST_DATA = [
@@ -56,5 +56,4 @@ TESTS = [
     (1, TEST_DATA[0], 1310),
     (2, TEST_DATA[1], 14),
     (2, TEST_DATA[2], 13659),
-    # (3, TEST_DATA[2], None),
 ]
