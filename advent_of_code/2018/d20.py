0c745701c0017380fe7ce09b1ff742a7149695f0
Isaac Good <github@isaacgood.com>
2025-07-20 23:16:25 -0700

    Tidy AoC 2018/20

--- advent_of_code/2018/d20.py
+++ advent_of_code/2018/d20.py
@@ -1,26 +1,14 @@
 #!/bin/python
 """Advent of Code, Day 20: A Regular Map."""
-from __future__ import annotations
 
 import collections
 import functools
-import itertools
-import logging
-import math
-import re
 
 from lib import aoc
-
-def log(*args, **kwargs) -> None:
-    logging.info(*args, **kwargs)
 
 
 class Day20(aoc.Challenge):
     """Day 20: A Regular Map."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
 
     TESTS = [
         aoc.TestCase(part=1, inputs="^WNE$", want=3),
@@ -30,16 +18,17 @@ class Day20(aoc.Challenge):
         aoc.TestCase(part=1, inputs="^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$", want=31),
         aoc.TestCase(part=2, inputs="", want=aoc.TEST_SKIP),
     ]
-
     INPUT_PARSER = aoc.parse_one_str
 
     def solver(self, puzzle_input: str, part_one: bool) -> int:
+        """Walk a regex to explore a map."""
         position = complex(0, 0)
         graph = collections.defaultdict(set)
         puzzle_input = puzzle_input.removeprefix("^").removesuffix("$")
 
         @functools.cache
-        def explore(pos: complex, instructions: str):
+        def explore(pos: complex, instructions: str) -> None:
+            """Explore a segment of the map, with support for branching."""
             for idx, char in enumerate(instructions):
                 if char in aoc.COMPASS_DIRECTIONS:
                     next_pos = pos + aoc.COMPASS_DIRECTIONS[char]
@@ -50,6 +39,7 @@ class Day20(aoc.Challenge):
                     assert char == "("
                     depth = 1
                     splits = []
+                    i = 0
                     for i, x in enumerate(instructions[idx + 1:], idx + 1):
                         if x == "(":
                             depth += 1
@@ -62,33 +52,27 @@ class Day20(aoc.Challenge):
                     chunks = [instructions[a + 1:b] for a, b in zip([idx] + splits, splits + [i])]
                     for chunk in chunks:
                         explore(pos, chunk + instructions[i + 1:])
-
-                    log(f"All {instructions}")
-                    log(f"Pre {instructions[:idx]}")
-                    log(f"Post {instructions[i + 1:]}")
-                    log(f"Block {instructions[idx + 1:i]}")
-                    log(f"Chunks {chunks}")
                     return
 
+        # Explore the map, building a graph.
         explore(position, puzzle_input)
 
-        todo = collections.deque()
+        # Walk the graph, breadth first, and track doors.
+        todo: collections.deque[tuple[int, complex]] = collections.deque()
         todo.append((0, complex(0, 0)))
-        seen = {complex(0, 0), }
+        added = {complex(0, 0), }
         far = set()
         while todo:
             steps, pos = todo.popleft()
             if steps >= 1000:
                 far.add(pos)
-            steps += 1
             for adjacent in graph[pos]:
-                if adjacent in seen:
+                if adjacent in added:
                     continue
-                seen.add(adjacent)
-                todo.append((steps, adjacent))
+                added.add(adjacent)
+                todo.append((steps + 1, adjacent))
         if part_one:
-            return steps - 1
+            return steps
         return len(far)
-
 
 # vim:expandtab:sw=4:ts=4
