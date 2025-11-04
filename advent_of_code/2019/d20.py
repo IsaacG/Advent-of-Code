d3309d32b1ab1bbfb0938f09769f4bf8a9febcc9
Isaac Good <github@isaacgood.com>
2025-11-03 22:23:08 -0800

    AoC 2019/20 tidy

--- advent_of_code/2019/d20.py
+++ advent_of_code/2019/d20.py
@@ -1,14 +1,8 @@
 #!/bin/python
 """Advent of Code, Day 20: Donut Maze."""
-from __future__ import annotations
-
 import collections
-import functools
-import itertools
-import logging
-import math
-import re
 import string
+from typing import cast
 
 from lib import aoc
 
@@ -111,33 +105,27 @@ RE....#.#                           #......RF
                A A D   M                     """,
 ]
 
-LineType = int
-InputType = list[LineType]
-log = logging.info
-
 
 class Day20(aoc.Challenge):
     """Day 20: Donut Maze."""
-
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
 
     TESTS = [
         aoc.TestCase(part=1, inputs=SAMPLE[0], want=23),
         aoc.TestCase(part=1, inputs=SAMPLE[1], want=58),
         aoc.TestCase(part=2, inputs=SAMPLE[2], want=396),
     ]
+    INPUT_PARSER = aoc.CoordinatesParser()
 
-    INPUT_PARSER = aoc.CoordinatesParser(origin_top_left=True)
+    def get_portals(self, puzzle_input: aoc.Map, part_one: bool) -> dict[str, list[tuple[complex, int]]]:
+        """Return a map of all the portals (their positions and in/out direction)."""
+        real, imag = (lambda x: x.real), (lambda x: x.imag)
+        # Outer edges
+        edge_fns = [(min, real), (max, real), (min, imag), (max, imag)]
+        # min|max(c.real|imag for c in puzzle_input.coords["."])
+        outer = [f(g(c) for c in puzzle_input.coords["."]) for f, g in edge_fns]
+        o_left, o_right, o_top, o_bottom = outer
 
-    def part1(self, puzzle_input: InputType) -> int:
-        # Outer portals
-        o_left = min(c.real for c in puzzle_input.coords["."])
-        o_right = max(c.real for c in puzzle_input.coords["."])
-        o_top = min(c.imag for c in puzzle_input.coords["."])
-        o_bottom = max(c.imag for c in puzzle_input.coords["."])
-        # Inner portals
+        # Inner edges
         centers = {
             coord: char for coord, char in puzzle_input.chars.items()
             if (
@@ -146,118 +134,27 @@ class Day20(aoc.Challenge):
                 and o_top < coord.imag < o_bottom
             )
         }
-        i_left = max(c.real for c in centers) + 1
-        i_right = min(c.real for c in centers) - 1
-        i_top = max(c.imag for c in centers) + 1
-        i_bottom = min(c.imag for c in centers) - 1
-        # print("inner", i_left, i_right, i_top, i_bottom)
-        # print(centers)
+        edge_fns = [
+            (lambda x: max(x) + 1, real), (lambda x: min(x) - 1, real),
+            (lambda x: max(x) + 1, imag), (lambda x: min(x) - 1, imag),
+        ]
+        inner = [f(g(c) for c in centers) for f, g in edge_fns]
+        i_left, i_right, i_top, i_bottom = inner
 
-        portals_pos = collections.defaultdict(list)
-        for chars, left, right, top, bottom in [
-            (puzzle_input.chars, o_left, o_right, o_top, o_bottom),
-            (centers, i_left, i_right, i_top, i_bottom),
-        ]:
-            for coord, char in chars.items():
-                # print(coord, char)
-                if char not in string.ascii_uppercase:
-                    continue
-                # if char in "BC":
-                #     print("pre", coord, char, left, right, top, bottom)
-                if coord.real == left - 1:
-                    adjacent = coord - 1
-                elif coord.real == right + 1:
-                    adjacent = coord + 1
-                elif coord.imag == top - 1:
-                    adjacent = coord - 1j
-                elif coord.imag == bottom + 1:
-                    adjacent = coord + 1j
-                else:
-                    continue
-                # if char in "BC":
-                #     print("post", coord, char)
-                if coord.real == left - 1 or coord.imag == top - 1:
-                    key = chars[adjacent] + char
-                else:
-                    key = char + chars[adjacent]
-                portals_pos[key].append(coord)
-        # print(portals_pos)
-        assert "AA" in portals_pos
-        assert "ZZ" in portals_pos
-        assert all(len(c) == 2 for k, c in portals_pos.items() if k not in ["AA", "ZZ"])
-
-        portal_adjacent = lambda pos: next(
-            p for p in puzzle_input.neighbors(pos) if p in puzzle_input.coords["."]
-        )
-
-        portal = {}
-        start, end = None, None
-        for key, pair in portals_pos.items():
-            if key == "AA":
-                start = pair[0]
-            elif key == "ZZ":
-                end = pair[0]
-            else:
-                a, b = pair
-                portal[a] = portal_adjacent(b)
-                portal[b] = portal_adjacent(a)
-        # print(portal)
-
-        todo = collections.deque([(0, start)])
-        seen = {start}
-        valid = set(puzzle_input.coords["."]) | set(portal) | {start, end}
-        while todo:
-            steps, pos = todo.popleft()
-            if pos == end:
-                return steps - 2
-            steps += 1
-            to_add = []
-            for neighbor in puzzle_input.neighbors(pos):
-                if neighbor in portal:
-                    neighbor = portal[neighbor]
-                if neighbor in valid and neighbor not in seen:
-                    seen.add(neighbor)
-                    to_add.append((steps, neighbor))
-            todo.extend(to_add)
-            # print(pos, steps, "add", to_add)
-
-
-        # print("\n".join(["==="] + [f"{k}={v!r}" for k, v in locals().items()] + ["==="]))
-        return None
-
-    def part2(self, puzzle_input: InputType) -> int:
-        # Outer portals
-        o_left = min(c.real for c in puzzle_input.coords["."])
-        o_right = max(c.real for c in puzzle_input.coords["."])
-        o_top = min(c.imag for c in puzzle_input.coords["."])
-        o_bottom = max(c.imag for c in puzzle_input.coords["."])
-        # Inner portals
-        centers = {
-            coord: char for coord, char in puzzle_input.chars.items()
-            if (
-                char in string.ascii_uppercase + " "
-                and o_left < coord.real < o_right
-                and o_top < coord.imag < o_bottom
-            )
-        }
-        i_left = max(c.real for c in centers) + 1
-        i_right = min(c.real for c in centers) - 1
-        i_top = max(c.imag for c in centers) + 1
-        i_bottom = min(c.imag for c in centers) - 1
-        # print("inner", i_left, i_right, i_top, i_bottom)
-        # print(centers)
-
-        portals_pos = collections.defaultdict(list)
+        # Find all portals that lie along the edge.
+        # Connect with the adjacent label to form the name.
+        # Connect with the adjacent "." for the coordinate.
+        portals_pos: dict[str, list[tuple[complex, int]]] = collections.defaultdict(list)
         for chars, depth, left, right, top, bottom in [
             (puzzle_input.chars, -1, o_left, o_right, o_top, o_bottom),
             (centers, +1, i_left, i_right, i_top, i_bottom),
         ]:
+            # Part one doesn't use a depth.
+            if part_one:
+                depth = 0
             for coord, char in chars.items():
-                # print(coord, char)
                 if char not in string.ascii_uppercase:
                     continue
-                # if char in "BC":
-                #     print("pre", coord, char, left, right, top, bottom)
                 if coord.real == left - 1:
                     adjacent = coord - 1
                 elif coord.real == right + 1:
@@ -268,18 +165,22 @@ class Day20(aoc.Challenge):
                     adjacent = coord + 1j
                 else:
                     continue
-                # if char in "BC":
-                #     print("post", coord, char)
                 if coord.real == left - 1 or coord.imag == top - 1:
                     key = chars[adjacent] + char
                 else:
                     key = char + chars[adjacent]
                 portals_pos[key].append((coord, depth))
+        return portals_pos
 
+    def build_map(
+            self, puzzle_input: aoc.Map, part_one: bool
+        ) -> tuple[complex, complex, dict[complex, tuple[complex, int]]]:
+        """Return the start, end, portal map: coordinate to coordinate with depth change."""
         portal_adjacent = lambda pos: next(
             p for p in puzzle_input.neighbors(pos) if p in puzzle_input.coords["."]
         )
 
+        portals_pos = self.get_portals(puzzle_input, part_one)
         portal = {}
         start, end = None, None
         for key, pair in portals_pos.items():
@@ -291,7 +192,12 @@ class Day20(aoc.Challenge):
                 (a, depth_a), (b, depth_b) = pair
                 portal[a] = (portal_adjacent(b), depth_a)
                 portal[b] = (portal_adjacent(a), depth_b)
-        print(portal)
+
+        return start, end, portal
+
+    def solver(self, puzzle_input: aoc.Map, part_one: bool) -> int:
+        """Return the number of steps required to solve the maze."""
+        start, end, portal = self.build_map(puzzle_input, part_one)
 
         todo = collections.deque([(0, 0, start)])
         seen = {(start, 0)}
@@ -306,17 +212,14 @@ class Day20(aoc.Challenge):
                 if neighbor in portal:
                     neighbor, depth_offset = portal[neighbor]
                     n_depth += depth_offset
+                    # We can go "inside" indefinitely but cannot go outside beyond "0".
                     if n_depth < 0:
                         continue
                 if neighbor in valid and (neighbor, n_depth) not in seen:
                     seen.add((neighbor, n_depth))
                     todo.append((steps, n_depth, neighbor))
-            # print(pos, steps, "add", to_add)
 
-
-        # print("\n".join(["==="] + [f"{k}={v!r}" for k, v in locals().items()] + ["==="]))
-        return None
-
+        raise RuntimeError("No solution found.")
 
 
 # vim:expandtab:sw=4:ts=4
