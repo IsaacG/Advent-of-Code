c3d0ddb9b02e379ad81f6a30ea0135f4f17e3104
Isaac Good <github@isaacgood.com>
2025-11-06 16:14:05 -0800

    Everybody Codes: tidy 2025/04

--- everybody_codes/event2025/quest_04.py
+++ everybody_codes/event2025/quest_04.py
@@ -1,34 +1,15 @@
 """Everyone Codes Day N."""
-
-import logging
 import math
 from lib import parsers
 
-log = logging.info
 
-def solve(part: int, data: str) -> int:
+def solve(part: int, data: list[list[int]]) -> int:
     """Solve the parts."""
-    if part == 1 or part == 2:
-        gears = [i[0] for i in data]
-        ratio = 1
-        for a, b in zip(gears, gears[1:]):
-            ratio *= a / b
-        if part == 1:
-            return int(ratio * 2025)
-        if part == 2:
-            return math.ceil(10000000000000 / ratio)
-    first, *rest, last = data
-    gears = [[1, first[0]], *rest, [last[0], 1]]
-    ratio = 1
-    for a, b in zip(gears, gears[1:]):
-        ratio *= a[1] / b[0]
-    print(ratio)
-    return int(ratio * 100)
+    ratio = math.prod(a[-1] / b[0] for a, b in zip(data, data[1:]))
+    return [int(ratio * 2025), math.ceil(10000000000000 / ratio), int(ratio * 100)][part - 1]
 
 
 PARSER = parsers.parse_ints
-TEST_DATA = [
-]
 TESTS = [
     (1, "128\n64\n32\n16\n8", 32400),
     (1, """102\n75\n50\n35\n13""", 15888),
@@ -42,7 +23,7 @@ if __name__ == "__main__":
     for _part, _data, expected in TESTS:
         assert solve(_part, PARSER.parse(_data)) == expected
     print("Tests pass.")
-    day = __file__.split("_", maxsplit=1)[-1].split(".")[0]
+    day = __file__.split("_", maxsplit=-1)[-1].split(".")[0]
     for _part in range(1, 4):
         with open(f"inputs/{day}.{_part}.txt", encoding="utf-8") as f:
             print(_part, solve(_part, PARSER.parse(f.read())))
--- everybody_codes/tmpl.py
+++ everybody_codes/tmpl.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     for _part, _data, expected in TESTS:
         assert solve(_part, PARSER.parse(_data)) == expected
     print("Tests pass.")
-    day = __file__.split("_", maxsplit=1)[-1].split(".")[0]
+    day = __file__.split("_", maxsplit=-1)[-1].split(".")[0]
     for _part in range(1, 4):
         with open(f"inputs/{day}.{_part}.txt", encoding="utf-8") as f:
             print(_part, solve(_part, PARSER.parse(f.read())))
