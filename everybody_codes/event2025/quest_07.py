9f6d493e8bd77c63dacf59b5a3e2de70c4b90522
Isaac Good <github@isaacgood.com>
2025-11-11 17:41:03 -0800

    Everybody Codes: solve 2025/07 (rough)

---
+++ everybody_codes/event2025/quest_07.py
@@ -0,0 +1,131 @@
+"""Everyone Codes Day N."""
+
+import collections
+import functools
+from lib import parsers
+
+
+def solve(part: int, data: str) -> int:
+    """Solve the parts."""
+    rev_pairs = collections.defaultdict(set)
+    pairs = collections.defaultdict(set)
+    for line in data.split("\n\n")[1].splitlines():
+        a, b = line.split(" > ")
+        for c in b.split(","):
+            pairs[a].add(c)
+            rev_pairs[c].add(a)
+
+    @functools.cache
+    def possibilities(size: int, letter: str) -> int:
+        if size == 11:
+            return 1
+        count = 0 if size < 7 else 1
+        size += 1
+        return count + sum(possibilities(size, i) for i in pairs[letter])
+
+    total = 0
+    names = data.split("\n\n")[0].split(",")
+    if part == 3:
+        names = set(names)
+        names = {n for n in names if not any(n != m and n.startswith(m) for m in names)}
+
+    for idx, name in enumerate(names, start=1):
+        for a, b in zip(name, name[1:]):
+            if a not in rev_pairs[b]:
+                break
+        else:
+            if part == 1:
+                return name
+            if part == 2:
+                total += idx
+            else:
+                total += possibilities(len(name), name[-1])
+    return total
+
+
+TEST_DATA = [
+    """\
+Oronris,Urakris,Oroneth,Uraketh
+
+r > a,i,o
+i > p,w
+n > e,r
+o > n,m
+k > f,r
+a > k
+U > r
+e > t
+O > r
+t > h""",
+    """\
+Xanverax,Khargyth,Nexzeth,Helther,Braerex,Tirgryph,Kharverax
+
+r > v,e,a,g,y
+a > e,v,x,r
+e > r,x,v,t
+h > a,e,v
+g > r,y
+y > p,t
+i > v,r
+K > h
+v > e
+B > r
+t > h
+N > e
+p > h
+H > e
+l > t
+z > e
+X > a
+n > v
+x > z
+T > i""",
+    """\
+Xaryt
+
+X > a,o
+a > r,t
+r > y,e,a
+h > a,e,v
+t > h
+v > e
+y > p,t""",
+    """\
+Khara,Xaryt,Noxer,Kharax
+
+r > v,e,a,g,y
+a > e,v,x,r,g
+e > r,x,v,t
+h > a,e,v
+g > r,y
+y > p,t
+i > v,r
+K > h
+v > e
+B > r
+t > h
+N > e
+p > h
+H > e
+l > t
+z > e
+X > a
+n > v
+x > z
+T > i""",
+]
+TESTS = [
+    (1, TEST_DATA[0], "Oroneth"),
+    (2, TEST_DATA[1], 23),
+    (3, TEST_DATA[2], 25),
+    (3, TEST_DATA[3], 1154),
+]
+
+if __name__ == "__main__":
+    for _part, _data, expected in TESTS:
+        assert solve(_part, PARSER.parse(_data)) == expected
+    print("Tests pass.")
+    day = __file__.split("_", maxsplit=-1)[-1].split(".")[0]
+    for _part in range(1, 4):
+        with open(f"inputs/{day}.{_part}.txt", encoding="utf-8") as f:
+            print(_part, solve(_part, PARSER.parse(f.read())))
