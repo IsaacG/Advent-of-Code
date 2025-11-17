a3ca873fff11416cda46d7ec22d58effcbef7382
Isaac Good <github@isaacgood.com>
2025-11-16 21:58:34 -0800

    Everybody Codes: 2025/08 Go optimization for 20x faster p3

--- everybody_codes/go/e2025/quest_08.go
+++ everybody_codes/go/e2025/quest_08.go
@@ -10,31 +10,36 @@ import (
 )
 
 // Quest08 for Event 2025.
-type Quest08 struct {}
+type Quest08 struct{}
 
 // Solve Quest 8.
 func (q Quest08) Solve(data string, part int) string {
+	nails := 32
+	if part > 1 {
+		nails = 256
+	}
+
 	var numbers []int
-	chords := make(map[[2]int]int)
 	for i := range strings.SplitSeq(data, ",") {
-		numbers = append(numbers, helpers.Atoi(i))
+		numbers = append(numbers, helpers.Atoi(i)-1)
+	}
+	chords := make(map[int]map[int]int)
+	for i := range nails {
+		chords[i] = make(map[int]int)
 	}
 	var lines [][2]int
+
 	for idx := range len(numbers) - 1 {
 		a, b := numbers[idx], numbers[idx+1]
 		if a > b {
 			a, b = b, a
 		}
 		// Ignore lines between adjacent nails; they play no role.
-		if b - a != 1 {
+		if b-a != 1 {
 			lines = append(lines, [2]int{a, b})
 		}
-		chords[[2]int{a, b}]++
-	}
-
-	nails := 32
-	if part > 1 {
-		nails = 256
+		chords[a][b]++
+		chords[b][a]++
 	}
 
 	total := 0
@@ -42,7 +47,7 @@ func (q Quest08) Solve(data string, part int) string {
 		a, b := line[0], line[1]
 		if part == 1 {
 			// Lines pass through the center if the start and end are opposite each other.
-			if 2 * (b - a) == nails {
+			if 2*(b-a) == nails {
 				total++
 			}
 		}
@@ -58,21 +63,27 @@ func (q Quest08) Solve(data string, part int) string {
 			}
 		}
 	}
-	if part == 3 {
-		for a := 1; a < nails; a++ {
-			for b := a + 1; b <= nails; b++ {
-				count := 0
-				for line, times := range chords {
-					c, d := line[0], line[1]
-					if (a == c && b == d) || (a != c && b != d && (c < a && a < d) != (c < b && b < d)) {
-						count += times
-					}
-				}
-				if count > total {
-					total = count
-				}
-			}
+	if part != 3 {
+		return helpers.Itoa(total)
+	}
+
+	cumulative := make(map[int]map[int]int)
+	for i := range nails {
+		cumulative[i] = make(map[int]int)
+		for j := i + 2; j < i+nails; j++ {
+			cumulative[i][j%nails] = cumulative[i][(j-1)%nails] + chords[i][j%nails]
 		}
 	}
-	return helpers.Itoa(total)
+
+	most := 0
+	for a := 0; a < nails-1; a++ {
+		for b := a + 1; b < nails; b++ {
+			count := chords[a][b]
+			for i := a + 1; i < b; i++ {
+				count += cumulative[i][(a-1)%nails] - cumulative[i][b]
+			}
+			most = Max(most, count)
+		}
+	}
+	return helpers.Itoa(most)
 }
