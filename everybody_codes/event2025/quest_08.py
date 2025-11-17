6cd7392a172cf302ffafef728f643273c8d7ace9
Isaac Good <github@isaacgood.com>
2025-11-16 21:49:42 -0800

    Everybody Codes: 2025/08 Python optimization for 20x faster p3

--- everybody_codes/event2025/quest_08.py
+++ everybody_codes/event2025/quest_08.py
@@ -19,7 +19,8 @@ def solve(part: int, data: list[list[int]], testing: bool) -> int:
     for a, b in zip(numbers, numbers[1:]):
         if a > b:
             a, b = b, a
-        lines.append((a, b))
+        # Shift 1..256 to 0..255 to make the modulo math simple.
+        lines.append((a - 1, b - 1))
 
     if part == 1:
         return sum(b - a == size // 2 for a, b in lines)
@@ -29,24 +30,28 @@ def solve(part: int, data: list[list[int]], testing: bool) -> int:
             for idx, (a, b) in enumerate(lines)
             for i, j in lines[:idx]
         )
-    lines.sort()
 
+    # Count the occurance of lines between nails, in both directions.
     frequency = collections.defaultdict(lambda: collections.defaultdict(int))
     for a, b in lines:
         frequency[a][b] += 1
         frequency[b][a] += 1
 
+    # Count the cumulative lines from nail a to nails [a + 1..b].
+    # We can count the lines that i-j cuts anchored by nail a
+    # by looking at the difference between difference between lines from a-(j-1) and a-(i).
+    # This gives the total lines from a to `[(i+1)..(j-1)]`.
     cumulative = collections.defaultdict(lambda: collections.defaultdict(int))
     for a in range(1, size):
         for b in range(a + 1, a + size):
-            cumulative[a][((b - 1) % size) + 1] = cumulative[a][((b - 2) % size) + 1] + frequency[a][((b - 1) % size) + 1]
+            cumulative[a][b % size] = cumulative[a][(b - 1) % size] + frequency[a][b % size]
 
     most = 0
-    for a in range(1, size):
-        for b in range(a + 2, size + 1):
+    for a in range(size - 1):
+        for b in range(a + 2, size):
             knots = frequency[a][b]
             for i in range(a + 1, b):
-                knots += cumulative[i][((a - 2) % size) + 1] - cumulative[i][b]
+                knots += cumulative[i][(a - 1) % size] - cumulative[i][b]
             most = max(most, knots)
     return most
 
