5e517ece6ea4048a5f654153638948db31824f19
Isaac Good <github@isaacgood.com>
2025-11-17 20:56:13 -0800

    Everybody Codes: tidy 2025/11

--- everybody_codes/event2025/quest_11.py
+++ everybody_codes/event2025/quest_11.py
@@ -1,24 +1,16 @@
 """Everyone Codes Day N."""
 
-import logging
 import time
 from lib import helpers
 from lib import parsers
 
-log = logging.info
 
-def solve(part: int, data: str) -> int:
+def solve(part: int, data: list[int]) -> int:
     """Solve the parts."""
-    ducks = [i[0] for i in data]
+    ducks = data
     num = len(ducks) - 1
 
-    if part == 3:
-        assert ducks == sorted(ducks)
-        average = sum(ducks) // len(ducks)
-        return sum(abs(i - average) for i in ducks) // 2
-
     steps = 0
-
     while any(a > b for a, b in zip(ducks, ducks[1:])):
         steps += 1
         for i in range(num):
@@ -26,19 +18,20 @@ def solve(part: int, data: str) -> int:
                 ducks[i] -= 1
                 ducks[i + 1] += 1
 
-    while any(a < b for a, b in zip(ducks, ducks[1:])):
-        steps += 1
+    if part > 1:
+        assert ducks == sorted(ducks)
+        average = sum(ducks) // len(ducks)
+        return steps + sum(abs(i - average) for i in ducks) // 2
+
+    for _ in range(steps, 10):
         for i in range(num):
             if ducks[i] < ducks[i + 1]:
                 ducks[i] += 1
                 ducks[i + 1] -= 1
-        if part == 1 and steps == 10:
-            return sum(idx * count for idx, count in enumerate(ducks, start=1))
-
-    return steps
+    return sum(idx * count for idx, count in enumerate(ducks, start=1))
 
 
-PARSER = parsers.parse_ints
+PARSER = parsers.parse_one_int_per_line
 TEST_DATA = [
     """\
 9
@@ -80,7 +73,7 @@ if __name__ == "__main__":
     day = int(__file__.split("_", maxsplit=-1)[-1].split(".")[0])
     for _part in range(1, 4):
         with open(f"inputs/{day:02}.{_part}.txt", encoding="utf-8") as f:
-            _input = PARSER.parse(f.read())  # type: list[list[int]]
+            _input = PARSER.parse(f.read())  # type: list[int]
             start = time.perf_counter_ns()
             got = solve(_part, _input)
             end = time.perf_counter_ns()
