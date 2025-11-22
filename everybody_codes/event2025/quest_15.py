7ddf56fd0f623747559ff39bb0324b99c2c864de
Isaac Good <github@isaacgood.com>
2025-11-21 22:48:31 -0800

    Everybody Codes: tidy 2025/15

--- everybody_codes/event2025/quest_15.py
+++ everybody_codes/event2025/quest_15.py
@@ -1,29 +1,37 @@
 """Everyone Codes Day N."""
 
-import collections
-import logging
 import operator
 import queue
 from lib import helpers
 from lib import parsers
 
-log = logging.info
 HEADINGS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
 
-def solve(part: int, data: str) -> int:
-    """Solve the parts."""
+def parse_walls(data: str) -> tuple[list[set[tuple[int, int, int]]], int, tuple[int, int]]:
+    """Parse the data into a set of vertical and horizontal walls.
+
+    Since walls are straight vertical or horizontal,
+    either the start-x, end-x or the start-y, end-y will match.
+    We only need to start start-x, end-x, y for walls with a constant y.
+
+    The start, end values can be sorted for easy of checking start <= i <= end.
+
+    Since every wall segment is 90 degrees from the prior,
+    the walls alternate between horizontal and vertical.
+    """
     instructions = data.split(",")
     x, y = 0, 0
     heading = 0
-    walls = [set(), set()]
+    initial_heading, end = 0, (0, 0)
+    walls: list[set[tuple[int, int, int]]] = [set(), set()]
     vertical = True
 
     for idx, instruction in enumerate(data.split(",")):
         direction, distance = instruction[0], int(instruction[1:])
         # Rotate right/left
-        vertical = not vertical
         heading = (heading + (3 if direction == "L" else 1)) % 4
+        vertical = not vertical
         if idx == 0:
             # The initial wall starts one over from the starting point.
             x, y = x + HEADINGS[heading][0], y + HEADINGS[heading][1]
@@ -39,41 +47,29 @@ def solve(part: int, data: str) -> int:
         # Sort segements.
         ax, bx = sorted([x, nx])
         ay, by = sorted([y, ny])
-        # Walls are either horizontal or vertical.
-        # print(f"{idx=}, {instruction=}, {vertical=}, {(x,y)=}, {(nx,ny)=}, {heading=}")
+        # Walls are either horizontal or vertical. Store those separately.
         if vertical:
             walls[heading % 2].add((ay, by, ax))
-            # assert ax == bx, f"{idx=}, {vertical=}, {(x,y)=}, {(nx,ny)=}, {heading=}"
         else:
             walls[heading % 2].add((ax, bx, ay))
-            # assert ay == by, f"{idx=}, {vertical=}, {(x,y)=}, {(nx,ny)=}, {heading=}"
         x, y = nx, ny
+    return walls, initial_heading, end
 
-    # Note the x/y of the vertical/horizontal walls. These indicate points of interest.
-    edges = [{i for wall in half for i in wall[:2]} for half in walls]
 
-    log(f"{end=}")
-    log(f"{walls=}")
-    log(f"{edges=}")
+def solve(part: int, data: str) -> int:
+    """Solve the parts."""
+    _ = part
+    walls, initial_heading, end = parse_walls(data)
 
-    def dist(pos):
-        return abs(end[0] - pos[0]) + abs(end[1] - pos[1])
+    seen = {(0, 0), }
+    q: queue.PriorityQueue[tuple[int, tuple[int, int], int]] = queue.PriorityQueue()
+    q.put((0, (0, 0), initial_heading))
 
-    def is_wall(pos):
-        if pos == end:
-            return False
-        x, y = pos
-        for ax, ay, bx, by in walls:
-            if ax <= x <= bx and ay <= y <= by:
-                return True
-        return False
-
-    seen = {(0, 0): 0}
-    q = queue.PriorityQueue()
-    q.put((0, 0, (0, 0), initial_heading))
-
+    # Start at the begining. Use Djiksta to explore moves.
+    # At any point, we can rotate right or left then move forward to either
+    # (1) go just past a wall or (2) line up with the end.
     while q:
-        _, steps, pos, heading = q.get()
+        steps, pos, heading = q.get()
         x, y = pos
         if pos == end:
             return steps
@@ -84,47 +80,48 @@ def solve(part: int, data: str) -> int:
             headings = [i for i in range(4) if i != heading]
         else:
             headings = [(heading + i) % 4 for i in [1, 3]]
-        log(f"Popped {steps=}, {pos=}; {heading=} = {HEADINGS[heading]}; try {headings=}")
 
         for heading in headings:
             dx, dy = HEADINGS[heading]
-            if pos == (0,1) and heading == 1:
-                log("===")
-            log(f"{pos=} try {heading=} ={HEADINGS[heading]}, {dx+dy=}, {heading % 2=}")
-            f = max if dx + dy == -1 else min
-            g = operator.ge if dx + dy == 1 else operator.le
-            h = operator.gt if dx + dy == 1 else operator.lt
-            a, b = (x, y) if heading % 2 else (y, x)
-            end_a = end[0 if heading % 2 else 1]
+            # Helper functions depending on the direction/orientation.
+            minmax = max if dx + dy == -1 else min
+            ge_le = operator.ge if dx + dy == 1 else operator.le
+            gt_lt = operator.gt if dx + dy == 1 else operator.lt
+            # cur_pos is the current position on the "number line" used to
+            # check which walls and "forward" of this position.
+            # cur_line is the position orthoganol of the movement.
+            # This is used to determine if we would be blocked by a wall or go around it.
+            cur_pos, cur_line = (x, y) if heading % 2 else (y, x)
+            # end_pos is the x- or y-part of the end that is used to determine if we should
+            # stop moving forward at this position, ie when the cur_pos matches it.
+            end_pos = end[0 if heading % 2 else 1]
 
-            blocker = f(
+            # Find the first wall we would hit when moving forward.
+            # This limits which walls we can "move past" as candidate positions.
+            blocker = minmax(
                 (
                     cross
                     for start, end, cross in walls[(heading + 1) % 2]
-                    if g(cross, a) and start <= b <= end
+                    if ge_le(cross, cur_pos) and start <= cur_line <= end
                 ), default=None
             )
+            # distances is how far forward we can move to "move past" a wall (prior to hitting the blocker).
             distances = {
-                abs(cross - a) + 1
+                abs(cross - cur_pos) + 1
                 for start, end, cross in walls[(heading + 1) % 2]
-                if g(cross, a) and (blocker is None or h(blocker, cross))
+                if ge_le(cross, cur_pos) and (blocker is None or gt_lt(blocker, cross))
             }
-            if g(end_a, a) and (blocker is None or h(blocker, end_a)):
-                log(f"Add end {abs(end_a-a)} to distances")
-                distances.add(abs(end_a - a))
-            log(f"{blocker=}, {distances=}")
+            # Also consider stopping when we line up with the end position.
+            if ge_le(end_pos, cur_pos) and (blocker is None or gt_lt(blocker, end_pos)):
+                distances.add(abs(end_pos - cur_pos))
 
-            candidates = []
             for distance in distances:
                 new = x + distance * dx, y + distance * dy
-                n_steps = steps + distance
-                if new in seen and n_steps >= seen[new]:
+                if new in seen:
                     continue
-                seen[new] = n_steps
-                candidates.append((n_steps + dist(new), n_steps, new, heading))
-            log(f"{candidates=}")
-            for i in candidates:
-                q.put(i)
+                seen.add(new)
+                q.put((steps + distance, new, heading))
+    raise RuntimeError("Not solved.")
 
 
 PARSER = parsers.parse_one_str
