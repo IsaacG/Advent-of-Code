2ef67594d2f031adb35f4d2d106ff90035a2bc17
Isaac Good <github@isaacgood.com>
2025-11-24 22:14:20 -0800

    Everybody Codes: solve 2025/16 (rough)

---
+++ everybody_codes/event2025/quest_16.py
@@ -0,0 +1,57 @@
+"""Everyone Codes Day N."""
+
+import logging
+import math
+from lib import helpers
+from lib import parsers
+
+log = logging.info
+
+def solve(part: int, data: str) -> int:
+    """Solve the parts."""
+    if part == 1:
+        # return sum(sum(1 for _ in range(1, 91, i)) for i in data[0])
+        return sum(90 // i for i in data[0])
+
+    spell = []
+    cols = data[0]
+    while any(cols):
+        i = next(i for i, v in enumerate(cols) if v)
+        for j in range(i, len(cols), i + 1):
+            assert cols[j]
+            cols[j] -= 1
+        spell.append(i + 1)
+    if part == 2:
+        return math.prod(spell)
+
+    target = 202520252025000
+    high = 100
+    while sum(high // i for i in spell) < target:
+        high *= 2
+    low = high // 2
+    while low + 1 < high:
+        mid = (high + low) // 2
+        got = sum(mid // i for i in spell)
+        if got == target:
+            return mid
+        elif got > target:
+            high = mid - 1
+        else:
+            low = mid
+    return high
+
+            
+
+
+
+PARSER = parsers.parse_ints
+TEST_DATA = [
+]
+TESTS = [
+    (1, "1,2,3,5,9", 193),
+    (2, "1,2,2,2,2,3,1,2,3,3,1,3,1,2,3,2,1,4,1,3,2,2,1,3,2,2", 270),
+    (3, "1,2,2,2,2,3,1,2,3,3,1,3,1,2,3,2,1,4,1,3,2,2,1,3,2,2", 94439495762954),
+]
+
+if __name__ == "__main__":
+    helpers.run_solution(globals())
