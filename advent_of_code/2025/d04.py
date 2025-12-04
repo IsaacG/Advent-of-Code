0b9c47f2622da50045876b6d3a20f2b5630e3348
Isaac Good <github@isaacgood.com>
2025-12-03 21:27:04 -0800

    Advent of Code: tidy 2025/04

--- advent_of_code/2025/d04.py
+++ advent_of_code/2025/d04.py
@@ -1,18 +1,9 @@
 #!/bin/python
 """Advent of Code, Day 4: Printing Department."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import logging
-import math
-import re
 
 from lib import aoc
 
-SAMPLE = [
-    """\
+SAMPLE = """\
 ..@@.@@@@.
 @@@.@.@.@@
 @@@@@.@.@@
@@ -22,87 +13,39 @@ SAMPLE = [
 .@.@.@.@@@
 @.@@@.@@@@
 .@@@@@@@@.
-@.@.@@@.@.""",  # 1
-]
-
-LineType = int
-InputType = list[LineType]
-log = logging.info
+@.@.@@@.@."""
 
 
 class Day04(aoc.Challenge):
     """Day 4: Printing Department."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
-        aoc.TestCase(part=1, inputs=SAMPLE[0], want=13),
-        aoc.TestCase(part=2, inputs=SAMPLE[0], want=43),
-        # aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
+        aoc.TestCase(part=1, inputs=SAMPLE, want=13),
+        aoc.TestCase(part=2, inputs=SAMPLE, want=43),
     ]
+    INPUT_PARSER = aoc.CoordinatesParser(chars=None, origin_top_left=True)
 
-    # INPUT_PARSER = aoc.parse_ints
-    # INPUT_PARSER = aoc.parse_one_str
-    # INPUT_PARSER = aoc.parse_one_str_per_line
-    # INPUT_PARSER = aoc.parse_multi_str_per_line
-    # INPUT_PARSER = aoc.parse_one_int
-    # INPUT_PARSER = aoc.parse_one_int_per_line
-    # INPUT_PARSER = aoc.parse_ints_one_line
-    # INPUT_PARSER = aoc.parse_ints_per_line
-    # INPUT_PARSER = aoc.parse_re_group_str(r"(a) .* (b) .* (c)")
-    # INPUT_PARSER = aoc.parse_re_findall_str(r"(a|b|c)")
-    # INPUT_PARSER = aoc.parse_multi_mixed_per_line
-    # INPUT_PARSER = aoc.parse_re_group_mixed(r"(foo) .* (\d+)")
-    # INPUT_PARSER = aoc.parse_re_findall_mixed(r"\d+|foo|bar")
-    # INPUT_PARSER = aoc.ParseBlocks([aoc.parse_one_str_per_line, aoc.parse_re_findall_int(r"\d+")])
-    # INPUT_PARSER = aoc.ParseOneWord(aoc.Board.from_int_block)
-    INPUT_PARSER = aoc.CoordinatesParserC(chars=None, origin_top_left=True)
-    # ---
-    # (width, height), start, garden, rocks = puzzle_input
-    # max_x, max_y = width - 1, height - 1
+    def solver(self, puzzle_input: aoc.Map, part_one: bool) -> int:
+        papers = puzzle_input.coords["@"]
 
-    def part1(self, puzzle_input: InputType) -> int:
-        d = puzzle_input.coords["@"]
-        return sum(1 for i in d if sum(n in d for n in puzzle_input.neighbors(i, aoc.EIGHT_DIRECTIONS)) < 4)
+        def moveable():
+            return {
+                paper
+                for paper in papers
+                if sum(
+                    neighbor in papers
+                    for neighbor in puzzle_input.neighbors(paper, aoc.ALL_NEIGHBORS_T)
+                ) < 4
+            }
 
-    def part2(self, puzzle_input: InputType) -> int:
-        d = puzzle_input.coords["@"]
-        changed = True
-        total = 0
+        if part_one:
+            return len(moveable())
+
+        initial = len(papers)
         while True:
-            changed = False
-            can_drop = {i for i in d if sum(n in d for n in puzzle_input.neighbors(i, aoc.EIGHT_DIRECTIONS)) < 4}
-            if not can_drop:
-                return total
-            total += len(can_drop)
-            d -= can_drop
-
-    def input_parser(self, puzzle_input: str) -> InputType:
-        """Parse the input data."""
-        return super().input_parser(puzzle_input)
-        return puzzle_input.splitlines()
-        return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
-        # Words: mixed str and int
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in line.split()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-        # Regex splitting
-        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in patt.match(line).groups()
-            )
-            for line in puzzle_input.splitlines()
-        ]
+            can_move = moveable()
+            if not can_move:
+                return initial - len(papers)
+            papers -= can_move
 
 # vim:expandtab:sw=4:ts=4
