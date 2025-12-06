c813a5672b488924a83532a345da151ad0eaf964
Isaac Good <github@isaacgood.com>
2025-12-05 23:24:47 -0800

    Advent of Code: fix 2022/08

--- advent_of_code/2022/d08.py
+++ advent_of_code/2022/d08.py
@@ -30,13 +30,13 @@ class Day08(aoc.Challenge):
         """
         board = puzzle_input.chars
 
-        visible: set[complex] = set()
+        visible: set[tuple[int, int]] = set()
 
-        edge_location: complex = 0
-        walk_direction: complex = 1
+        edge_location: tuple[int, int] = (0, 0)
+        walk_direction: tuple[int, int] = (0, 1)
 
         for _ in range(4):
-            look_direction = walk_direction * complex(0, 1)
+            look_direction = aoc.rotate_clockwise(*walk_direction)
             while True:
                 # Look into the forest and record what can be seen.
                 viewing = edge_location
@@ -45,13 +45,14 @@ class Day08(aoc.Challenge):
                     if board[viewing] > max_height:
                         max_height = board[viewing]
                         visible.add(viewing)
-                    viewing += look_direction
+                    viewing = viewing[0] + look_direction[0], viewing[1] + look_direction[1]
                 # Stop walking before exiting the forest.
-                if edge_location + walk_direction not in board:
+                next_location = edge_location[0] + walk_direction[0], edge_location[1] + walk_direction[1]
+                if next_location not in board:
                     break
-                edge_location += walk_direction
+                edge_location = next_location
             # Rotate 90 degrees.
-            walk_direction *= complex(0, 1)
+            walk_direction = aoc.rotate_clockwise(*walk_direction)
 
         return len(visible)
 
@@ -65,14 +66,14 @@ class Day08(aoc.Challenge):
             # Walk in every direction until the edge of the board or blocked.
             # Count steps. Multiply.
             visible = []
-            for direction in aoc.FOUR_DIRECTIONS:
-                cur = tree + direction
+            for dx, dy in aoc.FOUR_DIRECTIONS_T:
+                cur = tree[0] + dx, tree[1] + dy
                 num = 0
                 while cur in board:
                     num += 1
                     if board[cur] >= board[tree]:
                         break
-                    cur += direction
+                    cur = cur[0] + dx, cur[1] + dy
                 visible.append(num)
             scores.append(math.prod(visible))
 
