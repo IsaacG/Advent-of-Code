4cfc10b9c265d07b349db3ea4ea825d5b428554b
Isaac Good <github@isaacgood.com>
2025-12-07 22:25:45 -0800

    Advent of Code: tidy 2025/08

--- advent_of_code/2025/d08.py
+++ advent_of_code/2025/d08.py
@@ -1,18 +1,9 @@
 #!/bin/python
 """Advent of Code, Day 8: Playground."""
-from __future__ import annotations
-
-import collections
-import functools
-import itertools
-import logging
 import math
-import re
-
 from lib import aoc
 
-SAMPLE = [
-    """\
+SAMPLE = """\
 162,817,812
 57,618,57
 906,360,560
@@ -32,125 +23,46 @@ SAMPLE = [
 941,993,340
 862,61,35
 984,92,344
-425,690,689""",  # 0
-]
-
-LineType = int
-InputType = list[LineType]
-log = logging.info
+425,690,689"""
 
 
 class Day08(aoc.Challenge):
     """Day 8: Playground."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
-        aoc.TestCase(part=1, inputs=SAMPLE[0], want=40),
-        aoc.TestCase(part=2, inputs=SAMPLE[0], want=25272),
+        aoc.TestCase(part=1, inputs=SAMPLE, want=40),
+        aoc.TestCase(part=2, inputs=SAMPLE, want=25272),
     ]
 
-    # INPUT_PARSER = aoc.parse_ints
-    # INPUT_PARSER = aoc.parse_one_str
-    # INPUT_PARSER = aoc.parse_one_str_per_line
-    # INPUT_PARSER = aoc.parse_multi_str_per_line
-    # INPUT_PARSER = aoc.parse_one_int
-    # INPUT_PARSER = aoc.parse_one_int_per_line
-    # INPUT_PARSER = aoc.parse_ints_one_line
-    # INPUT_PARSER = aoc.parse_ints_per_line
-    # INPUT_PARSER = aoc.parse_re_group_str(r"(a) .* (b) .* (c)")
-    # INPUT_PARSER = aoc.parse_re_findall_str(r"(a|b|c)")
-    # INPUT_PARSER = aoc.parse_multi_mixed_per_line
-    # INPUT_PARSER = aoc.parse_re_group_mixed(r"(foo) .* (\d+)")
-    # INPUT_PARSER = aoc.parse_re_findall_mixed(r"\d+|foo|bar")
-    # INPUT_PARSER = aoc.ParseBlocks([aoc.parse_one_str_per_line, aoc.parse_re_findall_int(r"\d+")])
-    # INPUT_PARSER = aoc.ParseOneWord(aoc.Board.from_int_block)
-    # INPUT_PARSER = aoc.CoordinatesParser(chars=None, origin_top_left=True)
-    # ---
-    # (width, height), start, garden, rocks = puzzle_input
-    # max_x, max_y = width - 1, height - 1
-
-    def part1(self, puzzle_input: InputType) -> int:
+    def solver(self, puzzle_input: list[list[int]], part_one: bool) -> int:
+        """Return stats from connecting junction boxes into circuits using minimum distances."""
         boxes = set(tuple(i) for i in puzzle_input)
-        circuits = []
-        closest = 0
-        distances = []
+        distances = sorted(
+            (math.dist(a, b), a, b)
+            for a in boxes
+            for b in boxes
+            if a < b
+        )
 
-        def delta(a, b):
-            return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
+        circuits = list[set[tuple[int, ...]]]()
+        total_boxes = len(boxes)
+        p1_stop = 10 if self.testing else 1000
 
-        for a in boxes:
-            for b in boxes:
-                if a >= b:
-                    continue
-                distances.append((delta(a, b), a, b))
-        distances.sort()
-        it = iter(distances)
-
-        connections = 0
-        for _, a, b in distances[:10 if self.testing else 1000]:
-            # print("Consider", a, b)
-            ca = next((i for i in circuits if a in i), {a,})
-            cb = next((i for i in circuits if b in i), {b,})
-            if ca == cb:
-                connections += 1
+        for idx, (_, box_a, box_b) in enumerate(distances):
+            if part_one and idx == p1_stop:
+                return math.prod(sorted((len(i) for i in circuits), reverse=True)[:3])
+            circuit_a = next((i for i in circuits if box_a in i), {box_a, })
+            circuit_b = next((i for i in circuits if box_b in i), {box_b, })
+            if circuit_a == circuit_b:
                 continue
-            if len(ca) > 1:
-                circuits.remove(ca)
-            if len(cb) > 1:
-                circuits.remove(cb)
-            cc = ca | cb
-            circuits.append(cc)
-            connections += 1
-            # print(connections, a, b, len(cc))
-
-        sizes = [len(i) for i in circuits]
-        print(sizes)
-        sizes.sort(reverse=1)
-        # assert len(sizes) >= 3
-        return math.prod(sizes[:3])
-
-    def part2(self, puzzle_input: InputType) -> int:
-        boxes = set(tuple(i) for i in puzzle_input)
-        circuits = []
-        closest = 0
-        distances = []
-
-        def delta(a, b):
-            return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2
-
-        for a in boxes:
-            for b in boxes:
-                if a >= b:
-                    continue
-                distances.append((delta(a, b), a, b))
-        distances.sort()
-        it = iter(distances)
-        count = len(boxes)
-
-        connections = 0
-        for _, a, b in distances:
-            # print("Consider", a, b)
-            ca = next((i for i in circuits if a in i), {a,})
-            cb = next((i for i in circuits if b in i), {b,})
-            if ca == cb:
-                continue
-            if len(ca) > 1:
-                circuits.remove(ca)
-            if len(cb) > 1:
-                circuits.remove(cb)
-            cc = ca | cb
-            circuits.append(cc)
-            if len(cc) == count:
-                print(len(circuits))
-                return a[0] * b[0]
-
-        sizes = [len(i) for i in circuits]
-        print(sizes)
-        sizes.sort(reverse=1)
-        # assert len(sizes) >= 3
-        return math.prod(sizes[:3])
+            if len(circuit_a) > 1:
+                circuits.remove(circuit_a)
+            if len(circuit_b) > 1:
+                circuits.remove(circuit_b)
+            combined = circuit_a | circuit_b
+            circuits.append(combined)
+            if len(combined) == total_boxes:
+                return box_a[0] * box_b[0]
+        raise RuntimeError("Not solved")
 
 # vim:expandtab:sw=4:ts=4
