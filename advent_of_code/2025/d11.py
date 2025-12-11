5a85de3fad4b29b2dfbbfb5c5fd1b86efa3630f9
Isaac Good <github@isaacgood.com>
2025-12-10 21:53:24 -0800

    Advent of Code: tidy 2025/11

--- advent_of_code/2025/d11.py
+++ advent_of_code/2025/d11.py
@@ -1,14 +1,6 @@
 #!/bin/python
 """Advent of Code, Day 11: Reactor."""
-from __future__ import annotations
-
-import collections
 import functools
-import itertools
-import logging
-import math
-import re
-
 from lib import aoc
 
 SAMPLE = [
@@ -22,7 +14,7 @@ eee: out
 fff: out
 ggg: out
 hhh: ccc fff iii
-iii: out""",  # 0
+iii: out""",
     """\
 svr: aaa bbb
 aaa: fft
@@ -39,130 +31,37 @@ ggg: out
 hhh: out""",
 ]
 
-LineType = int
-InputType = list[LineType]
-log = logging.info
-
 
 class Day11(aoc.Challenge):
     """Day 11: Reactor."""
 
-    DEBUG = True
-    # Default is True. On live solve, submit one tests pass.
-    # SUBMIT = {1: False, 2: False}
-
     TESTS = [
         aoc.TestCase(part=1, inputs=SAMPLE[0], want=5),
         aoc.TestCase(part=2, inputs=SAMPLE[1], want=2),
-        # aoc.TestCase(part=2, inputs=SAMPLE[0], want=aoc.TEST_SKIP),
     ]
 
-    def part1(self, puzzle_input: InputType) -> int:
-        g = {}
-        for src, *dsts in puzzle_input:
-            g[src.strip(":")] = dsts
-
-        q = collections.deque()
-        q.append("you")
-        total = 0
-        while q:
-            node = q.popleft()
-            if node == "out":
-                total += 1
-            else:
-                for i in g[node]:
-                    q.append(i)
-        print("\n".join(["==="] + [f"{k}={v!r}" for k, v in locals().items()] + ["==="]))
-        return total
-
-    def part2(self, puzzle_input: InputType) -> int:
-        names = set()
-        for src, *dsts in puzzle_input:
-            names.add(src.strip(":"))
-            names.update(dsts)
-        nums = dict((n, i) for i, n in enumerate(names))
-
-        g = {}
-        for src, *dsts in puzzle_input:
-            g[nums[src.strip(":")]] = [nums[d] for d in dsts]
-        svr = nums["svr"]
-        out = nums["out"]
-        dac = nums["dac"]
-        fft = nums["fft"]
+    def solver(self, puzzle_input: dict[str, list[str]], part_one: bool) -> int:
+        """Return the total paths from a source to output."""
 
         @functools.cache
-        def paths_via(node):
-            if node == out:
-                return {0: 1, 1: 0, 2: 0, 3: 0}
-            paths = {i: 0 for i in range(4)}
-            for i in g[node]:
-                p = paths_via(i)
-                for i, j in p.items():
-                    paths[i] += j
-            if node == dac:
-                paths[1] = paths[0]
-                paths[3] = paths[2]
-                paths[0] = 0
-                paths[2] = 0
-            elif node == fft:
-                paths[2] = paths[0]
-                paths[3] = paths[1]
-                paths[0] = 0
-                paths[1] = 0
-            return paths
+        def paths_via(node: str, seen: int) -> int:
+            """Return how many ways from node to out. Seen indicates if we saw the needed nodes."""
+            if node == "out":
+                return 1 if seen == 2 else 0
+            if node in ["dac", "fft"]:
+                seen += 1
+            return sum(paths_via(i, seen) for i in puzzle_input[node])
 
-        return paths_via(svr)[3]
+        if part_one:
+            return paths_via("you", 2)
+        return paths_via("svr", 0)
 
-
-        q = collections.deque()
-        q.append((nums["svr"], frozenset(), False, False))
-        total = 0
-        while q:
-            node, seen, x, y = q.popleft()
-            if node == out and x and y:
-                total += 1
-                continue
-            if node == out:
-                continue
-            if node == dac:
-                x = True
-            if node == fft:
-                y = True
-            for i in g[node]:
-                if i in seen:
-                    continue
-                seen = frozenset(seen | {i})
-                q.append((i, seen, x, y))
-        print("\n".join(["==="] + [f"{k}={v!r}" for k, v in locals().items()] + ["==="]))
-        return total
-
-
-    # def solver(self, puzzle_input: InputType, part_one: bool) -> int | str:
-
-    def input_parser(self, puzzle_input: str) -> InputType:
+    def input_parser(self, puzzle_input: str) -> dict[str, list[str]]:
         """Parse the input data."""
-        return super().input_parser(puzzle_input)
-        return puzzle_input.splitlines()
-        return puzzle_input
-        return [int(i) for i in puzzle_input.splitlines()]
-        mutate = lambda x: (x[0], int(x[1])) 
-        return [mutate(line.split()) for line in puzzle_input.splitlines()]
-        # Words: mixed str and int
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in line.split()
-            )
-            for line in puzzle_input.splitlines()
-        ]
-        # Regex splitting
-        patt = re.compile(r"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-        return [
-            tuple(
-                int(i) if i.isdigit() else i
-                for i in patt.match(line).groups()
-            )
-            for line in puzzle_input.splitlines()
-        ]
+        out = {}
+        for line in puzzle_input.splitlines():
+            src, *dst = line.replace(":", "").split()
+            out[src] = dst
+        return out
 
 # vim:expandtab:sw=4:ts=4
