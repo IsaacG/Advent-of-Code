2fa779bc8880d94e4e4140f5df3c11cfb33ecb68
Isaac Good <github@isaacgood.com>
2025-12-17 00:36:58 -0800

    FlipFlop Codes, 2025/02: refactor so all parts use common code

--- flipflop.slome/2025/d02.py
+++ flipflop.slome/2025/d02.py
@@ -16,17 +16,10 @@ def solve(part: int, data: str) -> int:
     """Solve the parts."""
     highest = 0
     cur = 0
-    if part == 1:
-        for i in data:
-            cur += 1 if i == "^" else -1
-            highest = max(highest, cur)
-    else:
-        for direction, amount in more_itertools.run_length.encode(data):
-            if part == 2:
-                cur += (1 if direction == "^" else -1) * amount * (amount + 1) // 2
-            else:
-                cur += (1 if direction == "^" else -1) * fib(amount)
-            highest = max(highest, cur)
+    transform = [lambda x: x, lambda x: (x * (x + 1) // 2), fib][part - 1]
+    for direction, amount in more_itertools.run_length.encode(data):
+        cur += (1 if direction == "^" else -1) * transform(amount)
+        highest = max(highest, cur)
     return highest
 
 
