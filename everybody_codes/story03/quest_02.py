0d640cfcd07493a4e5233092576b026d539687bf
Isaac Good <github@isaacgood.com>
2026-03-04 20:11:27 -0800

    Tidy Everybody.Codes Story 3 Quest 2

--- everybody_codes/story03/quest_02.py
+++ everybody_codes/story03/quest_02.py
@@ -1,140 +1,92 @@
 """Everyone Codes Day N."""
 
-import logging
+import collections.abc
 import itertools
 from lib import helpers
 from lib import parsers
 
-log = logging.info
 
 def solve(part: int, data: str) -> int:
     """Solve the parts."""
     pos = data.coords["@"].copy().pop()
+    starts = data.coords["@"]
+    bones = data.coords["#"]
+    seen = {pos}
     count = 0
-    dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-    movements = dirs
-    if part == 3:
-        movements = [(0, -1)] * 3 + [(1, 0)] * 3 + [(0, 1)] * 3 + [(-1, 0)] * 3
+    dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]  # up, right, down, left
 
-    moves = itertools.cycle(enumerate(movements))
-    seen = {pos,}
+    def neighbors(pos: tuple[int, int]) -> collections.abc.Iterable[tuple[int, int]]:
+        """Get the neighbors of a position."""
+        return {(pos[0] + move[0], pos[1] + move[1]) for move in dirs}
+
+    def next_position() -> collections.abc.Iterable[tuple[int, int]]:
+        """Generate next positions. Closure over pos and seen."""
+        # Turns UP, RIGHT, ... in UP, UP, UP, RIGHT, RIGHT, RIGHT, ...
+        repeats = 1 if part < 3 else 3
+        for move in itertools.cycle(i for j in dirs for i in [j] * repeats):
+            next_pos = pos[0] + move[0], pos[1] + move[1]
+            if next_pos not in seen:
+                yield next_pos
+
+    positions = next_position()
+
     if part == 1:
-        end = data.coords["#"].pop()
+        end = bones.pop()
+        # Count steps to get to the end bone.
         while pos != end:
-            _, d = next(moves)
-            np = pos[0] + d[0], pos[1] + d[1]
-            if np in seen:
-                continue
-            seen.add(np)
-            pos = np
-            count += 1
-        return count
-
-    if part == 2:
-        history = []
-        seen.update(data.coords["#"])
-        want = {(end[0] + d[0], end[1] + d[1]) for d in movements for end in data.coords["#"]} - data.coords["#"]
-        while not seen >= want:
-            if all((pos[0] + d[0], pos[1] + d[1]) in seen for d in dirs):
-                while all((pos[0] + d[0], pos[1] + d[1]) in seen for d in dirs):
-                    pos, i = history.pop()
-                    count -= 1
-                    while i != next(moves)[0]:
-                        True
-                    for _ in range(len(movements) - 1):
-                        next(moves)
-            i, d = next(moves)
-            np = pos[0] + d[0], pos[1] + d[1]
-            if np in seen:
-                continue
-            seen.add(np)
-            history.append((pos, i))
-            pos = np
-            count += 1
-        return count
-
-    if part == 3:
-        max_x = max(i[0] for i in data.coords["#"] | data.coords["@"])
-        min_x = min(i[0] for i in data.coords["#"] | data.coords["@"])
-        max_y = max(i[1] for i in data.coords["#"] | data.coords["@"])
-        min_y = min(i[1] for i in data.coords["#"] | data.coords["@"])
-
-        bones = data.coords["#"]
-        seen.update(data.coords["#"])
-        want = {(end[0] + d[0], end[1] + d[1]) for d in dirs for end in data.coords["#"]} - data.coords["#"]
-        print((35,7) in want)
-        print(data.chars[(35, 7)])
-        print(data.chars[(34, 7)])
-        print(data.chars[(36, 7)])
-        print(data.chars[(35, 6)])
-        print(data.chars[(35, 8)])
-
-        def floodfill(pos):
-            s = pos
-            todo = [pos]
-            area = set()
-            done = {pos}
-            while todo:
-                p = todo.pop()
-                if not (min_x <= p[0] <= max_x and min_y <= p[1] <= max_y):
-                    return []
-                if p in seen or p in area:
-                    continue
-                area.add(p)
-                cands = {(p[0] + d[0], p[1] + d[1]) for d in dirs}
-                assert len(cands) == 4
-                for np in cands:
-                    if np in done or np in seen:
-                        continue
-                    done.add(np)
-                    todo.append(np)
-            return area
-
-        filled = set()
-        for w in want:
-            filled.update(floodfill(w))
-        print((35,7) in want)
-        print((35,7) in filled)
-        print((35,7) in seen)
-        print(floodfill((35,7)))
-        seen |= filled
-
-        def display():
-            x0 = min(p[0] for p in seen)
-            x1 = max(p[0] for p in seen)
-            y0 = min(p[1] for p in seen)
-            y1 = max(p[1] for p in seen)
-            print(f"Step: {count}")
-            for y in range(y0, y1+1):
-                print("".join(" " if (x,y) == pos else "#" if (x,y) in bones else " " if (x,y) in seen else " " for x in range(x0, x1+1)))
-            print()
-
-        while not seen >= want:
-            if count > 1200:
-                display()
-            assert not all((pos[0] + d[0], pos[1] + d[1]) in seen for d in dirs), count
-            _, d = next(moves)
-            np = pos[0] + d[0], pos[1] + d[1]
-            if np in seen:
-                continue
-            pos = np
+            pos = next(positions)
             seen.add(pos)
-            max_x = max(max_x, pos[0])
-            min_x = min(min_x, pos[0])
-            max_y = max(max_y, pos[1])
-            min_y = min(min_y, pos[1])
             count += 1
-
-            for d in dirs:
-                flood = set()
-                np = pos[0] + d[0], pos[1] + d[1]
-                if np not in seen:
-                    flood.update(floodfill(np))
-                if flood:
-                    # print(f"{count=} FF {len(flood)}: {flood}")
-                    seen.update(flood)
-
         return count
+
+    min_x, max_x = data.min_x, data.max_x
+    min_y, max_y = data.min_y, data.max_y
+
+    # Parts 2, 3: do not move into locations with bone.
+    seen.update(bones)
+
+    def floodfill(pos: tuple[int, int]) -> set[tuple[int, int]]:
+        """Return the set of positions which are entirely surrounded."""
+        todo = [pos]
+        flooded = set()
+        done = {pos}
+        while todo:
+            pos = todo.pop()
+            # Stop if we get move further than the explored board, ie this is not surrounded.
+            if not (min_x <= pos[0] <= max_x and min_y <= pos[1] <= max_y):
+                return set()
+            flooded.add(pos)
+            for next_pos in neighbors(pos):
+                if next_pos in done or next_pos in seen:
+                    continue
+                done.add(next_pos)
+                todo.append(next_pos)
+        return flooded
+
+    # Want to surround the bones.
+    want = {n for bone in bones for n in neighbors(bone)}
+    # Exclude positions entirely surrounded by bones. We cannot reach there.
+    filled = set()
+    for w in want:
+        filled.update(floodfill(w))
+    want -= filled
+
+    # Expand the sound until we've seen everything we want.
+    while not seen >= want:
+        pos = next(positions)
+        seen.add(pos)
+        max_x = max(max_x, pos[0])
+        min_x = min(min_x, pos[0])
+        max_y = max(max_y, pos[1])
+        min_y = min(min_y, pos[1])
+        count += 1
+
+        # Flood fill when we close off an area.
+        for next_pos in neighbors(pos):
+            if next_pos not in seen:
+                seen.update(floodfill(next_pos))
+
+    return count
 
 
 PARSER = parsers.CoordinatesParser()
@@ -200,10 +152,11 @@ TEST_DATA = [
 TESTS = [
     (1, TEST_DATA[0], 12),
     (2, TEST_DATA[0], 47),
-    # (3, TEST_DATA[0], 87),
-    # (3, TEST_DATA[1], 239),
+    (3, TEST_DATA[0], 87),
+    (3, TEST_DATA[1], 239),
     (3, TEST_DATA[2], 1539),
 ]
+
 
 if __name__ == "__main__":
     helpers.run_solution(globals())
