d004a3b259bd2c8d59fec009234ec60d2247fad5
Isaac Good <github@isaacgood.com>
2026-07-05 21:33:46 -0700

    Solve FlipFlop 2026/01

---
+++ flipflop.slome/2026/d01.py
@@ -0,0 +1,31 @@
+"""FlipFlop Codes, Puzzle 1: Coffee Brewing."""
+
+import logging
+from lib import helpers
+from lib import parsers
+
+log = logging.info
+
+def solve(part: int, data: str) -> int:
+    """Solve the parts."""
+    if part < 3:
+        temps_targets = zip(data, [60] * len(data))
+    else:
+        temps_targets = zip(data[:len(data) // 2], data[len(data) // 2:])
+
+    return sum(
+        (target - temp) * (1 if target > temp else 0 if part == 1 else -5)
+        for temp, target in temps_targets
+    )
+
+
+PARSER = parsers.parse_one_int_per_line
+TEST_DATA = "41 87 93 104 46 102 65 105 81 36 66 46 60 65 64 64 61 73 55 69".replace(" ", "\n")
+TESTS = [
+    (1, TEST_DATA, 76),
+    (2, TEST_DATA, 1371),
+    (3, TEST_DATA, 1141),
+]
+
+if __name__ == "__main__":
+    helpers.run_solution(globals())
